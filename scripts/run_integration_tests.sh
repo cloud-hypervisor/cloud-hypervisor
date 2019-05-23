ddce3df8260b85976aa894d67758848d75571ea4
Rob Bradford <robert.bradford@intel.com>
2019-05-23 16:45:13 +0100

    tests: Add basic integration testing
    Add basic integration testing of the hypervisor using a cloud-init to
    configure the VM at boot and SSH to control it at runtime.
    
    Initial test just boots the VM up checks some basic resources and
    reboots. With a second test that calls into the first to check that
    subsequent tests work correctly.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

---
+++ scripts/run_integration_tests.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -x
+
+source $HOME/.cargo/env
+
+WORKLOADS_DIR="$HOME/workloads"
+mkdir -p "$WORKLOADS_DIR"
+
+FW_URL=$(curl --silent https://api.github.com/repos/intel/rust-hypervisor-firmware/releases/latest | grep "browser_download_url" | grep -o 'https://.*[^ "]')
+FW="$WORKLOADS_DIR/hypervisor-fw"
+if [ ! -f "$FW" ]; then
+    pushd $WORKLOADS_DIR
+    wget --quiet $FW_URL
+    popd
+fi
+
+OS_IMAGE_NAME="clear-29620-cloud.img"
+OS_IMAGE_URL="https://download.clearlinux.org/releases/29620/clear/clear-29620-cloud.img.xz"
+OS_IMAGE="$WORKLOADS_DIR/$OS_IMAGE_NAME"
+if [ ! -f "$OS_IMAGE" ]; then
+    pushd $WORKLOADS_DIR
+    wget --quiet $OS_IMAGE_URL
+    unxz $OS_IMAGE_NAME.xz
+popd
+fi
+
+rm /tmp/cloudinit.img
+mkdosfs -n config-2 -C /tmp/cloudinit.img 8192
+mcopy -oi /tmp/cloudinit.img -s test_data/cloud-init/openstack ::
+
+cargo test --features "integration_tests" --no-run
+find target/debug/deps/ -name cloud_hypervisor* | xargs -n1 sudo setcap cap_net_admin+ep
+
+# Tests must be executed serially for now as they have a hardcoded IP address
+cargo test --features "integration_tests" -- --test-threads=1
