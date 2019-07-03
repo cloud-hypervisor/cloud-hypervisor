c431f1a40dd245ee596252e32e026b8bc2e169c9
Chao Peng <chao.p.peng@linux.intel.com>
2019-07-03 19:40:04 +0000

    vm-allocator: Support allocate-time alignment
    There is alignment support for AddressAllocator but there are occations
    that the alignment is known only when we call allocate(). One example
    is PCI BAR which is natually aligned, means for which we have to align
    the base address to its size.
    
    Signed-off-by: Chao Peng <chao.p.peng@linux.intel.com>

--- vfio/src/vfio_pci.rs
+++ vfio/src/vfio_pci.rs
@@ -320,7 +320,7 @@ impl PciDevice for VfioPciDevice {
                 region_size = 2u64.pow(first_bit);
                 // We need to allocate a guest PIO address range for that BAR.
                 bar_addr = allocator
-                    .allocate_io_addresses(None, region_size)
+                    .allocate_io_addresses(None, region_size, Some(region_size))
                     .ok_or_else(|| PciDeviceError::IoAllocationFailed(region_size))?;
             } else {
                 if is_64bit_bar {
@@ -356,7 +356,7 @@ impl PciDevice for VfioPciDevice {
 
                 // We need to allocate a guest MMIO address range for that BAR.
                 bar_addr = allocator
-                    .allocate_mmio_addresses(None, region_size)
+                    .allocate_mmio_addresses(None, region_size, Some(region_size))
                     .ok_or_else(|| PciDeviceError::IoAllocationFailed(region_size))?;
             }
 
