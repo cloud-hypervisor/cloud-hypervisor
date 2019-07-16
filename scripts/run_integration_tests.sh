fd65223e2d0a75f44d878f9b87cbea468424bb01
Rob Bradford <robert.bradford@intel.com>
2019-07-16 12:04:14 +0100

    tests: Add "time" before most interesting commands
    Get an idea of where we're spending time on the integration tests.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -10,7 +10,7 @@ FW_URL=$(curl --silent https://api.github.com/repos/intel/rust-hypervisor-firmwa
 FW="$WORKLOADS_DIR/hypervisor-fw"
 if [ ! -f "$FW" ]; then
     pushd $WORKLOADS_DIR
-    wget --quiet $FW_URL
+    time wget --quiet $FW_URL
     popd
 fi
 
@@ -19,7 +19,7 @@ OS_IMAGE_URL="https://cloudhypervisorstorage.blob.core.windows.net/images/$OS_IM
 OS_IMAGE="$WORKLOADS_DIR/$OS_IMAGE_NAME"
 if [ ! -f "$OS_IMAGE" ]; then
     pushd $WORKLOADS_DIR
-    wget --quiet $OS_IMAGE_URL
+    time wget --quiet $OS_IMAGE_URL
     unxz $OS_IMAGE_NAME.xz
     popd
 fi
@@ -28,7 +28,7 @@ OS_RAW_IMAGE_NAME="clear-29810-cloud-raw.img"
 OS_RAW_IMAGE="$WORKLOADS_DIR/$OS_RAW_IMAGE_NAME"
 if [ ! -f "$OS_RAW_IMAGE" ]; then
     pushd $WORKLOADS_DIR
-    qemu-img convert -p -f qcow2 -O raw $OS_IMAGE_NAME $OS_RAW_IMAGE_NAME
+    time qemu-img convert -p -f qcow2 -O raw $OS_IMAGE_NAME $OS_RAW_IMAGE_NAME
     popd
 fi
 
@@ -39,11 +39,11 @@ BZIMAGE_IMAGE="$WORKLOADS_DIR/bzImage"
 if [ ! -f "$VMLINUX_IMAGE" ]; then
     SRCDIR=$PWD
     pushd $WORKLOADS_DIR
-    wget --quiet https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.0.21.tar.xz
-    tar xf linux-5.0.21.tar.xz
+    time wget --quiet https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.0.21.tar.xz
+    time tar xf linux-5.0.21.tar.xz
     pushd linux-5.0.21
     cp $SRCDIR/resources/linux-5.0-config .config
-    make bzImage -j `nproc`
+    time make bzImage -j `nproc`
     cp vmlinux $VMLINUX_IMAGE
     cp arch/x86/boot/bzImage $BZIMAGE_IMAGE
     popd
@@ -59,10 +59,10 @@ LINUX_CUSTOM_DIR="linux-custom"
 if [ ! -f "$VMLINUX_CUSTOM_IMAGE" ]; then
     SRCDIR=$PWD
     pushd $WORKLOADS_DIR
-    git clone --depth 1 "https://github.com/sboeuf/linux.git" -b "virtio-pmem_and_virtio-fs" $LINUX_CUSTOM_DIR
+    time git clone --depth 1 "https://github.com/sboeuf/linux.git" -b "virtio-pmem_and_virtio-fs" $LINUX_CUSTOM_DIR
     pushd $LINUX_CUSTOM_DIR
     cp $SRCDIR/resources/linux-virtio-pmem-and-virtio-fs-config .config
-    make bzImage -j `nproc`
+    time make bzImage -j `nproc`
     cp vmlinux $VMLINUX_CUSTOM_IMAGE
     popd
     rm -r $LINUX_CUSTOM_DIR
@@ -73,7 +73,7 @@ VIRTIOFSD_URL="$(curl --silent https://api.github.com/repos/intel/nemu/releases/
 VIRTIOFSD="$WORKLOADS_DIR/virtiofsd"
 if [ ! -f "$VIRTIOFSD" ]; then
     pushd $WORKLOADS_DIR
-    wget --quiet $VIRTIOFSD_URL -O "virtiofsd"
+    time wget --quiet $VIRTIOFSD_URL -O "virtiofsd"
     chmod +x "virtiofsd"
     sudo setcap cap_sys_admin+epi "virtiofsd"
     popd
@@ -90,8 +90,8 @@ rm /tmp/cloudinit.img
 mkdosfs -n config-2 -C /tmp/cloudinit.img 8192
 mcopy -oi /tmp/cloudinit.img -s test_data/cloud-init/openstack ::
 
-cargo build
+time cargo build
 sudo setcap cap_net_admin+ep target/debug/cloud-hypervisor
 
 # Tests must be executed serially for now as they have a hardcoded IP address
-cargo test --features "integration_tests" -- --test-threads=1
+time cargo test --features "integration_tests" -- --test-threads=1
