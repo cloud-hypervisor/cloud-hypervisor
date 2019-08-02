google/crosvm

qcow: Fix invalid_cluster_bits test
Start with a valid header so the invalid cluster bits field is tested in
isolation. Before this change the test would pass even if the cluster
bits check was removed from the code because the header was invalid for
other reasons.

BUG=none
TEST=this is a test

Change-Id: I5c09417ae3f974522652a50cb0fdc5dc0e10dd44
Signed-off-by: Dylan Reid <dgreid@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/1660889
Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
Tested-by: kokoro <noreply+kokoro@google.com>

qcow/src/qcow.rs
@@ -1793,9 +1793,9 @@ mod tests {
 
     #[test]
     fn invalid_cluster_bits() {
-        let mut header = test_huge_header();
+        let mut header = valid_header();
         header[23] = 3;
-        with_basic_file(&test_huge_header(), |disk_file: File| {
+        with_basic_file(&header, |disk_file: File| {
             QcowFile::from(disk_file).expect_err("Failed to create file.");
         });
     }

cloud-hypervisor/cloud-hypervisor

(cherry picked from crosvm commit c9f254b1921335231b32550b5ae6b8416e1ca7aa)
Signed-off-by: Rob Bradford <robert.bradford@intel.com>

qcow/src/qcow.rs
@@ -1817,9 +1817,9 @@ mod tests {
 
     #[test]
     fn invalid_cluster_bits() {
-        let mut header = test_huge_header();
+        let mut header = valid_header_v3();
         header[23] = 3;
-        with_basic_file(&test_huge_header(), |disk_file: File| {
+        with_basic_file(&header, |disk_file: File| {
             QcowFile::from(disk_file).expect_err("Failed to create file.");
         });
     }