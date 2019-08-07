f272390de85f4dd591cb7cad39bb245133010028
Rob Bradford <robert.bradford@intel.com>
2019-08-07 17:53:37 +0100

    acpi_tables: Add initial ACPI tables support
    Add a revision 2 RSDP table only supporting an XSDT along with support
    for creating generic SDT based tables.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

---
+++ acpi_tables/src/lib.rs
@@ -0,0 +1,11 @@
+// Copyright © 2019 Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+pub mod rsdp;
+pub mod sdt;
+
+fn generate_checksum(data: &[u8]) -> u8 {
+    (255 - data.iter().fold(0u8, |acc, x| acc.wrapping_add(*x))).wrapping_add(1)
+}
