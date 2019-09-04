22f91ab3a2a9982c050ee1b0b68e40bb79d85b82
Sebastien Boeuf <sebastien.boeuf@intel.com>
2019-09-04 12:49:55 -0700

    vm-virtio: Move vsock to its own module
    There is a lot of code related to this virtio-vsock hybrid
    implementation, that's why it's better to keep it under its
    own module.
    
    Signed-off-by: Sebastien Boeuf <sebastien.boeuf@intel.com>

---
+++ vm-virtio/src/vsock/mod.rs
@@ -0,0 +1,13 @@
+// Copyright 2019 Intel Corporation. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Portions Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Portions Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the THIRD-PARTY file.
+
+mod device;
+
+pub use self::device::Vsock;
