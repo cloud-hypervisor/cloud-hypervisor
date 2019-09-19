6e854a924f6a10f9b3241d8d53ceb4522e99ea54
Samuel Ortiz <sameo@linux.intel.com>
2019-09-19 15:22:53 +0200

    vmm: Start the API server thread
    The API server thread is unconditionally started. Since we can now start
    cloud-hypervisor without arguments (As it will then only start the API
    server), we need to verify if the built VM configuration is both valid
    and provided by the user explicitly. If not, then we don't attempt to
    stay a guest VM.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- vmm/src/lib.rs
+++ vmm/src/lib.rs
@@ -9,7 +9,7 @@ extern crate log;
 
 use kvm_ioctls::*;
 use std::fmt::{self, Display};
-use std::result;
+use std::{result, thread};
 
 pub mod config;
 pub mod device_manager;
@@ -21,6 +21,9 @@ use self::vm::{ExitBehaviour, Vm};
 /// Errors associated with VM management
 #[derive(Debug)]
 pub enum Error {
+    /// Cannot start API server socket
+    ApiServerStart,
+
     /// Cannot create a new VM.
     VmNew(vm::Error),
 
@@ -37,6 +40,7 @@ impl Display for Error {
         use self::Error::*;
 
         match self {
+            ApiServerStart => write!(f, "Can not start the API server"),
             VmNew(e) => write!(f, "Can not create a new virtual machine: {:?}", e),
             VmStart(e) => write!(f, "Can not start a new virtual machine: {:?}", e),
             LoadKernel(e) => write!(f, "Can not load a guest kernel: {:?}", e),
@@ -55,18 +59,32 @@ impl Vmm {
     }
 
     pub fn run(&self, vmm_config: VmmConfig, vm_config: VmConfig) -> Result<()> {
-        loop {
-            let mut vm = Vm::new(&self.kvm, &vm_config).map_err(Error::VmNew)?;
+        let socket_path = vmm_config.api_server.socket_path.to_string();
 
-            let entry = vm.load_kernel().map_err(Error::LoadKernel)?;
+        // First we spawn the API server
+        let api_thread = thread::spawn(move || {
+            panic!("API server at {} UNSUPPORTED", &socket_path);
+        });
 
-            if vm.start(entry).map_err(Error::VmStart)? == ExitBehaviour::Shutdown {
+        // If we have a valid VM configuration, we need to verify that it
+        // has been explicitly set by the user.
+        if vm_config.user_defined && vm_config.valid() {
+            loop {
+                let mut vm = Vm::new(&self.kvm, &vm_config).map_err(Error::VmNew)?;
+
+                let entry = vm.load_kernel().map_err(Error::LoadKernel)?;
+
+                if vm.start(entry).map_err(Error::VmStart)? == ExitBehaviour::Shutdown {
+                    break;
+                }
+
+                #[cfg(not(feature = "acpi"))]
                 break;
             }
-
-            #[cfg(not(feature = "acpi"))]
-            break;
         }
+
+        api_thread.join().map_err(|_| Error::ApiServerStart)?;
+
         Ok(())
     }
 }
