a99d9302d76e9c96894932c8856d92d54b32b049
Samuel Ortiz <sameo@linux.intel.com>
2019-11-18 15:30:54 +0100

    vm-device: Initial Migratable trait
    The Migratable trait groups all expected capabilities of devices and
    components that can be migrated.
    
    For a component to be migrated, it must be able to pause and resume.
    Once paused, it should be able to provide a snapshot of itself. It
    should also be able to restore itself from a snaphot.
    
    As a consequence, the Migratable trait will be split between the
    Pausable and the Snapshotable traits. This commit only adds the
    Pausable one.
    
    All migratable devices will be tracked from the DeviceManager.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- vm-device/src/lib.rs
+++ vm-device/src/lib.rs
@@ -1,3 +1,8 @@
+extern crate serde;
+extern crate thiserror;
+
+use thiserror::Error;
+
 /// Trait meant for triggering the DMA mapping update related to an external
 /// device not managed fully through virtio. It is dedicated to virtio-iommu
 /// in order to trigger the map update anytime the mapping is updated from the
@@ -9,3 +14,31 @@ pub trait ExternalDmaMapping: Send + Sync {
     /// Unmap a memory range
     fn unmap(&self, iova: u64, size: u64) -> std::result::Result<(), std::io::Error>;
 }
+
+#[derive(Error, Debug)]
+pub enum MigratableError {
+    #[error("Failed to pause migratable component: {0}")]
+    Pause(#[source] anyhow::Error),
+
+    #[error("Failed to resume migratable component: {0}")]
+    Resume(#[source] anyhow::Error),
+}
+
+/// A Pausable component can be paused and resumed.
+pub trait Pausable {
+    /// Pause the component.
+    fn pause(&mut self) -> std::result::Result<(), MigratableError>;
+
+    /// Resume the component.
+    fn resume(&mut self) -> std::result::Result<(), MigratableError>;
+}
+
+/// A snapshotable component can be snapshoted.
+pub trait Snapshotable {}
+
+/// Trait to be implemented by any component (device, CPU, RAM, etc) that
+/// can be migrated.
+/// All migratable components are paused before being snapshotted, and then
+/// eventually resumed. Thus any Migratable component must be both Pausable
+/// and Snapshotable.
+pub trait Migratable: Pausable + Snapshotable {}
