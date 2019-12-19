880906a6bf3f8516e09ba834726c733720b2c91c
Rob Bradford <robert.bradford@intel.com>
2019-12-19 16:55:45 +0000

    arch_gen: Remove unused bootparam module
    We use the version that's included in the linux-loader crate. This old
    version was also generating build errors after updating to the new
    clippy:
    
    e.g.:
    
    error: unsafe function's docs miss `# Safety` section
      --> arch_gen/src/x86/bootparam.rs:23:5
       |
    23 | /     pub unsafe fn as_ptr(&self) -> *const T {
    24 | |         ::std::mem::transmute(self)
    25 | |     }
       | |_____^
       |
       = note: `-D clippy::missing-safety-doc` implied by `-D warnings`
       = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#missing_safety_doc
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- arch_gen/src/x86/mod.rs
+++ arch_gen/src/x86/mod.rs
@@ -16,7 +16,6 @@
     clippy::should_implement_trait,
     clippy::transmute_ptr_to_ptr
 )]
-pub mod bootparam;
 #[allow(non_camel_case_types)]
 #[allow(non_upper_case_globals)]
 #[allow(clippy::unreadable_literal, clippy::redundant_static_lifetimes)]
