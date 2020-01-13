01a65195aae94401b5b5d6090a3af6d403bdf9d5
Sebastien Boeuf <sebastien.boeuf@intel.com>
2020-01-13 16:25:57 +0100

    ci: Move to personal branch for virtiofsd
    The current virtio-fs-dev branch from the official repo is breaking the
    build on our Azure VMs because the glibc wrapper for renameat2() syscall
    does not exist. This branch fixes the problem while the maintainers are
    fixing the issue upstream.
    
    Signed-off-by: Sebastien Boeuf <sebastien.boeuf@intel.com>

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -91,7 +91,7 @@ VIRTIOFSD="$WORKLOADS_DIR/virtiofsd"
 QEMU_DIR="qemu_build"
 if [ ! -f "$VIRTIOFSD" ]; then
     pushd $WORKLOADS_DIR
-    git clone --depth 1 "https://gitlab.com/virtio-fs/qemu.git" -b "virtio-fs-dev" $QEMU_DIR
+    git clone --depth 1 "https://github.com/sboeuf/qemu.git" -b "virtio-fs" $QEMU_DIR
     pushd $QEMU_DIR
     time ./configure --prefix=$PWD --target-list=x86_64-softmmu
     time make virtiofsd -j `nproc`
