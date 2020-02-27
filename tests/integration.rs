ecd0564a34071fbc8e059eff4fa2437612ce4dd3
Rob Bradford <robert.bradford@intel.com>
2020-02-27 13:54:05 +0000

    tests: Test rebooting works for block self spawn test
    Being able to reboot the VM is an important aspect of the self spawning
    behaviour.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- tests/integration.rs
+++ tests/integration.rs
@@ -1380,8 +1380,25 @@ mod tests {
 
             thread::sleep(std::time::Duration::new(20, 0));
 
-            guest.ssh_command("sudo shutdown -h now")?;
-            thread::sleep(std::time::Duration::new(5, 0));
+            let reboot_count = guest
+                .ssh_command("sudo journalctl | grep -c -- \"-- Reboot --\"")
+                .unwrap_or_default()
+                .trim()
+                .parse::<u32>()
+                .unwrap_or(1);
+
+            aver_eq!(tb, reboot_count, 0);
+            guest.ssh_command("sudo reboot").unwrap_or_default();
+
+            thread::sleep(std::time::Duration::new(20, 0));
+            let reboot_count = guest
+                .ssh_command("sudo journalctl | grep -c -- \"-- Reboot --\"")
+                .unwrap_or_default()
+                .trim()
+                .parse::<u32>()
+                .unwrap_or_default();
+            aver_eq!(tb, reboot_count, 1);
+
             let _ = cloud_child.kill();
             let _ = cloud_child.wait();
 
