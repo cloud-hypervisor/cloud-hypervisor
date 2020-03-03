1c4f92bdfb96de6314544deb3777077d1e714f3e
Rob Bradford <robert.bradford@intel.com>
2020-03-03 17:39:34 +0000

    tests: Test virtio-fs with virtio-mmio
    Now that virtio-mmio transport has support for SHM regions we should be
    able to test virtio-fs against it.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- tests/integration.rs
+++ tests/integration.rs
@@ -1754,27 +1754,22 @@ mod tests {
         });
     }
 
-    #[cfg_attr(not(feature = "mmio"), test)]
     fn test_virtio_fs_dax_on_default_cache_size() {
         test_virtio_fs(true, None, "none", &prepare_virtiofsd)
     }
 
-    #[cfg_attr(not(feature = "mmio"), test)]
     fn test_virtio_fs_dax_on_cache_size_1_gib() {
         test_virtio_fs(true, Some(0x4000_0000), "none", &prepare_virtiofsd)
     }
 
-    #[cfg_attr(not(feature = "mmio"), test)]
     fn test_virtio_fs_dax_off() {
         test_virtio_fs(false, None, "none", &prepare_virtiofsd)
     }
 
-    #[cfg_attr(not(feature = "mmio"), test)]
     fn test_virtio_fs_dax_on_default_cache_size_w_vhost_user_fs_daemon() {
         test_virtio_fs(true, None, "none", &prepare_vhost_user_fs_daemon)
     }
 
-    #[cfg_attr(not(feature = "mmio"), test)]
     fn test_virtio_fs_dax_on_cache_size_1_gib_w_vhost_user_fs_daemon() {
         test_virtio_fs(
             true,
@@ -1784,7 +1779,6 @@ mod tests {
         )
     }
 
-    #[cfg_attr(not(feature = "mmio"), test)]
     fn test_virtio_fs_dax_off_w_vhost_user_fs_daemon() {
         test_virtio_fs(false, None, "none", &prepare_vhost_user_fs_daemon)
     }
