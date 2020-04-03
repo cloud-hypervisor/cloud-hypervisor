2cae3a38acb4115aacd7aa980c10c86bfc44eebc
Hui Zhu <teawater@antfin.com>
2020-04-03 17:27:20 +0800

    vmm: api: ch-remote: Add balloon to VmResizeData
    Signed-off-by: Hui Zhu <teawater@antfin.com>

--- vmm/src/lib.rs
+++ vmm/src/lib.rs
@@ -490,9 +490,10 @@ impl Vmm {
         &mut self,
         desired_vcpus: Option<u8>,
         desired_ram: Option<u64>,
+        desired_ram_w_balloon: Option<u64>,
     ) -> result::Result<(), VmError> {
         if let Some(ref mut vm) = self.vm {
-            if let Err(e) = vm.resize(desired_vcpus, desired_ram) {
+            if let Err(e) = vm.resize(desired_vcpus, desired_ram, desired_ram_w_balloon) {
                 error!("Error when resizing VM: {:?}", e);
                 Err(e)
             } else {
@@ -766,6 +767,7 @@ impl Vmm {
                                         .vm_resize(
                                             resize_data.desired_vcpus,
                                             resize_data.desired_ram,
+                                            resize_data.desired_ram_w_balloon,
                                         )
                                         .map_err(ApiError::VmResize)
                                         .map(|_| ApiResponsePayload::Empty);
