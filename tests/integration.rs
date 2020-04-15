4c32d2dc1601d1d91882b6fe7db8c0dd7c25652d
Rob Bradford <robert.bradford@intel.com>
2020-04-15 18:17:51 +0100

    tests: Enhance test_disk_hotplug to also unplug device
    Ensure that the device is removed and the removed device stays away
    after a reboot.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- tests/integration.rs
+++ tests/integration.rs
@@ -3668,7 +3668,10 @@ mod tests {
                 remote_command(
                     &api_socket,
                     "add-disk",
-                    Some(&format!("path={}", blk_file_path.to_str().unwrap()))
+                    Some(&format!(
+                        "path={},id=test0",
+                        blk_file_path.to_str().unwrap()
+                    ))
                 )
             );
 
@@ -3705,6 +3708,48 @@ mod tests {
                     .parse::<u32>()
                     .unwrap_or_default(),
                 1
+            );
+
+            aver!(
+                tb,
+                remote_command(&api_socket, "remove-device", Some("test0"))
+            );
+
+            thread::sleep(std::time::Duration::new(20, 0));
+
+            // Check device has gone away
+            aver_eq!(
+                tb,
+                guest
+                    .ssh_command("lsblk | grep vdc | grep -c 16M")
+                    .unwrap_or_default()
+                    .trim()
+                    .parse::<u32>()
+                    .unwrap_or(1),
+                0
+            );
+
+            guest.ssh_command("sudo reboot").unwrap_or_default();
+
+            thread::sleep(std::time::Duration::new(20, 0));
+            let reboot_count = guest
+                .ssh_command("sudo journalctl | grep -c -- \"-- Reboot --\"")
+                .unwrap_or_default()
+                .trim()
+                .parse::<u32>()
+                .unwrap_or_default();
+            aver_eq!(tb, reboot_count, 2);
+
+            // Check device still absent
+            aver_eq!(
+                tb,
+                guest
+                    .ssh_command("lsblk | grep vdc | grep -c 16M")
+                    .unwrap_or_default()
+                    .trim()
+                    .parse::<u32>()
+                    .unwrap_or(1),
+                0
             );
 
             let _ = child.kill();
