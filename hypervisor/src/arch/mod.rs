72e39a34967c198782d3d19f7b765e4b5c8a23a9
Muminul Islam <muislam@microsoft.com>
2020-06-03 13:09:39 -0700

    hypervisor: Add KVM implementation
    For each of the traits we are defining kvm related structures
    and add the trait implementation to the structs. For more information
    please see the kvm-ioctls and kvm-bindings crate.
    
    This is a standalone implementation that does not include the switch of
    the Cloud-Hypervisor vmm and arch crates to it.
    
    Signed-off-by: Muminul Islam <muislam@microsoft.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

---
+++ hypervisor/src/arch/mod.rs
@@ -0,0 +1,12 @@
+// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Portions Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE-BSD-3-Clause file.
+//
+// Copyright © 2019 Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0 AND BSD-3-Clause
+
+#[cfg(target_arch = "x86_64")]
+pub mod x86;
---
+++ hypervisor/src/kvm/aarch64/mod.rs
@@ -0,0 +1,20 @@
+// Copyright © 2019 Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright © 2020, Microsoft  Corporation
+//
+
+///
+/// Export generically-named wrappers of kvm-bindings for Unix-based platforms
+///
+use crate::kvm::{KvmError, KvmResult};
+use serde_derive::{Deserialize, Serialize};
+pub use {kvm_ioctls::Cap, kvm_ioctls::Kvm};
+
+pub fn check_required_kvm_extensions(kvm: &Kvm) -> KvmResult<()> {
+    if !kvm.check_extension(Cap::SignalMsi) {
+        return Err(KvmError::CapabilityMissing(Cap::SignalMsi));
+    }
+    Ok(())
+}
