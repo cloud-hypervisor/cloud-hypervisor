f5afc288d8394caef8c7f95acdd06f62b92d2c93
Muminul Islam <muislam@microsoft.com>
2020-06-03 12:58:00 -0700

    hypervisor: Add Vm trait
    This VM trait should be implemented by each underlying hypervisor.
    
    Previously created hypervisor object should create the VM based on
    already selected hypervisor. This is just the trait definition. For each
    of supported hypervisor we need to implement the trait. Later we will
    implement this trait for KVM and then Microsoft Hyper-V.
    
    Signed-off-by: Muminul Islam <muislam@microsoft.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- hypervisor/src/kvm/mod.rs
+++ hypervisor/src/kvm/mod.rs
@@ -18,7 +18,10 @@ pub use x86_64::{CpuId, ExtendedControlRegisters, LapicState, MsrEntries, Xsave}
 /// Export generically-named wrappers of kvm-bindings for Unix-based platforms
 ///
 pub use {
-    kvm_bindings::kvm_fpu as FpuState, kvm_bindings::kvm_mp_state as MpState,
+    kvm_bindings::kvm_create_device as CreateDevice, kvm_bindings::kvm_fpu as FpuState,
+    kvm_bindings::kvm_irq_routing as IrqRouting, kvm_bindings::kvm_mp_state as MpState,
     kvm_bindings::kvm_regs as StandardRegisters, kvm_bindings::kvm_sregs as SpecialRegisters,
-    kvm_bindings::kvm_vcpu_events as VcpuEvents, kvm_ioctls::VcpuExit,
+    kvm_bindings::kvm_userspace_memory_region as MemoryRegion,
+    kvm_bindings::kvm_vcpu_events as VcpuEvents, kvm_ioctls::DeviceFd, kvm_ioctls::IoEventAddress,
+    kvm_ioctls::VcpuExit,
 };
