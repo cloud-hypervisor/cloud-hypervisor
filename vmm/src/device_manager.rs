cloud-hypervisor/cloud-hypervisor

commit e4dee57e813696439af0cf08bf08ec02038537b6

arch, pci, vmm: Initial switch to the hypervisor crate
Start moving the vmm, arch and pci crates to being hypervisor agnostic
by using the hypervisor trait and abstractions. This is not a complete
switch and there are still some remaining KVM dependencies.

Signed-off-by: Muminul Islam <muislam@microsoft.com>
Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

vmm/src/device_manager.rs
@@ -40,7 +40,10 @@ use devices::{
     interrupt_controller, interrupt_controller::InterruptController, legacy::Serial, BusDevice,
     HotPlugNotificationFlags,
 };
-use kvm_ioctls::*;
+use hypervisor::kvm_ioctls;
+use hypervisor::kvm_ioctls::*;
+#[cfg(feature = "mmio_support")]
+use hypervisor::vm::DataMatch;
 use libc::TIOCGWINSZ;
 use libc::{MAP_NORESERVE, MAP_PRIVATE, MAP_SHARED, O_TMPFILE, PROT_READ, PROT_WRITE};
 #[cfg(feature = "pci_support")]
@@ -186,10 +189,10 @@ pub enum DeviceManagerError {
     FreePciBars(pci::PciDeviceError),
 
     /// Cannot register ioevent.
-    RegisterIoevent(kvm_ioctls::Error),
+    RegisterIoevent(anyhow::Error),
 
     /// Cannot unregister ioevent.
-    UnRegisterIoevent(kvm_ioctls::Error),
+    UnRegisterIoevent(anyhow::Error),
 
     /// Cannot create virtio device
     VirtioDevice(vmm_sys_util::errno::Error),
@@ -229,7 +232,7 @@ pub enum DeviceManagerError {
     VfioMapRegion(pci::VfioPciError),
 
     /// Failed to create the KVM device.
-    CreateKvmDevice(kvm_ioctls::Error),
+    CreateKvmDevice(anyhow::Error),
 
     /// Failed to memory map.
     Mmap(io::Error),
@@ -430,7 +433,7 @@ struct AddressManager {
     #[cfg(target_arch = "x86_64")]
     io_bus: Arc<devices::Bus>,
     mmio_bus: Arc<devices::Bus>,
-    vm_fd: Arc<VmFd>,
+    vm_fd: Arc<dyn hypervisor::Vm>,
     #[cfg(feature = "pci_support")]
     device_tree: Arc<Mutex<DeviceTree>>,
 }
@@ -561,15 +564,16 @@ impl DeviceRelocation for AddressManager {
             if bar_addr == new_base {
                 for (event, addr) in virtio_pci_dev.ioeventfds(old_base) {
                     let io_addr = IoEventAddress::Mmio(addr);
-                    self.vm_fd
-                        .unregister_ioevent(event, &io_addr)
-                        .map_err(|e| io::Error::from_raw_os_error(e.errno()))?;
+                    self.vm_fd.unregister_ioevent(event, &io_addr).unwrap();
+                    /*
+                    map_err(io::Error::from_raw_os_error(
+                        hypervisor::HypervisorVmError::UnregisterIoEvent,
+                    ))?; */
                 }
                 for (event, addr) in virtio_pci_dev.ioeventfds(new_base) {
                     let io_addr = IoEventAddress::Mmio(addr);
-                    self.vm_fd
-                        .register_ioevent(event, &io_addr, NoDatamatch)
-                        .map_err(|e| io::Error::from_raw_os_error(e.errno()))?;
+                    self.vm_fd.register_ioevent(event, &io_addr, None).unwrap();
+                    //.map_err(|e| io::Error::from_raw_os_error(e.errno()))?;
                 }
             } else {
                 let virtio_dev = virtio_pci_dev.virtio_device();
@@ -577,7 +581,7 @@ impl DeviceRelocation for AddressManager {
                 if let Some(mut shm_regions) = virtio_dev.get_shm_regions() {
                     if shm_regions.addr.raw_value() == old_base {
                         // Remove old region from KVM by passing a size of 0.
-                        let mut mem_region = kvm_bindings::kvm_userspace_memory_region {
+                        let mut mem_region = hypervisor::kvm::kvm_userspace_memory_region {
                             slot: shm_regions.mem_slot,
                             guest_phys_addr: old_base,
                             memory_size: 0,
@@ -585,23 +589,19 @@ impl DeviceRelocation for AddressManager {
                             flags: 0,
                         };
 
-                        // Safe because removing an existing guest region.
-                        unsafe {
-                            self.vm_fd
-                                .set_user_memory_region(mem_region)
-                                .map_err(|e| io::Error::from_raw_os_error(e.errno()))?;
-                        }
+                        self.vm_fd.set_user_memory_region(mem_region).unwrap();
+                        //.map_err(|e| io::Error::from_raw_os_error(e.errno()))?;
 
                         // Create new mapping by inserting new region to KVM.
                         mem_region.guest_phys_addr = new_base;
                         mem_region.memory_size = shm_regions.len;
 
-                        // Safe because the guest regions are guaranteed not to overlap.
-                        unsafe {
-                            self.vm_fd
-                                .set_user_memory_region(mem_region)
-                                .map_err(|e| io::Error::from_raw_os_error(e.errno()))?;
-                        }
+                        self.vm_fd.set_user_memory_region(mem_region).map_err(|e| {
+                            io::Error::new(
+                                io::ErrorKind::Other,
+                                format!("failed to set user memory regions: {:?}", e),
+                            )
+                        })?;
 
                         // Update shared memory regions to reflect the new mapping.
                         shm_regions.addr = GuestAddress(new_base);
@@ -753,7 +753,7 @@ pub struct DeviceManager {
 
 impl DeviceManager {
     pub fn new(
-        vm_fd: Arc<VmFd>,
+        vm_fd: Arc<dyn hypervisor::Vm>,
         config: Arc<Mutex<VmConfig>>,
         memory_manager: Arc<Mutex<MemoryManager>>,
         _exit_evt: &EventFd,
@@ -2291,15 +2291,15 @@ impl DeviceManager {
     }
 
     #[cfg(feature = "pci_support")]
-    fn create_kvm_device(vm: &Arc<VmFd>) -> DeviceManagerResult<DeviceFd> {
-        let mut vfio_dev = kvm_bindings::kvm_create_device {
-            type_: kvm_bindings::kvm_device_type_KVM_DEV_TYPE_VFIO,
+    fn create_kvm_device(vm: &Arc<dyn hypervisor::Vm>) -> DeviceManagerResult<DeviceFd> {
+        let mut vfio_dev = hypervisor::kvm::kvm_create_device {
+            type_: hypervisor::kvm::kvm_device_type_KVM_DEV_TYPE_VFIO,
             fd: 0,
             flags: 0,
         };
 
         vm.create_device(&mut vfio_dev)
-            .map_err(DeviceManagerError::CreateKvmDevice)
+            .map_err(|e| DeviceManagerError::CreateKvmDevice(e.into()))
     }
 
     #[cfg(not(feature = "pci_support"))]
@@ -2582,8 +2582,8 @@ impl DeviceManager {
             let io_addr = IoEventAddress::Mmio(addr);
             self.address_manager
                 .vm_fd
-                .register_ioevent(event, &io_addr, NoDatamatch)
-                .map_err(DeviceManagerError::RegisterIoevent)?;
+                .register_ioevent(event, &io_addr, None)
+                .map_err(|e| DeviceManagerError::RegisterIoevent(e.into()))?;
         }
 
         let virtio_pci_device = Arc::new(Mutex::new(virtio_pci_device));
@@ -2739,8 +2739,8 @@ impl DeviceManager {
             let io_addr = IoEventAddress::Mmio(*addr);
             self.address_manager
                 .vm_fd
-                .register_ioevent(event, &io_addr, i as u32)
-                .map_err(DeviceManagerError::RegisterIoevent)?;
+                .register_ioevent(event, &io_addr, Some(DataMatch::DataMatch32(i as u32)))
+                .map_err(|e| DeviceManagerError::RegisterIoevent(e.into()))?;
         }
 
         let interrupt_group = interrupt_manager
@@ -2979,7 +2979,7 @@ impl DeviceManager {
                     self.address_manager
                         .vm_fd
                         .unregister_ioevent(event, &io_addr)
-                        .map_err(DeviceManagerError::UnRegisterIoevent)?;
+                        .map_err(|e| DeviceManagerError::UnRegisterIoevent(e.into()))?;
                 }
 
                 (

.github/workflows/cross-build.yaml
@@ -25,7 +25,7 @@ jobs:
       - name: Install arm64 libfdt
         run: wget http://ftp.us.debian.org/debian/pool/main/d/device-tree-compiler/libfdt-dev_1.6.0-1_arm64.deb && dpkg-deb -xv libfdt-dev_1.6.0-1_arm64.deb ./tlibfdtdev && sudo mkdir /tmmmp && mkdir target && mkdir target/debug && mkdir target/debug/deps && sudo cp ./tlibfdtdev/usr/lib/aarch64-linux-gnu/libfdt.a target/debug/deps/libfdt.a && echo "libfdt installed"
       - name: Disable "with-serde" in kvm-bindings
-        run: sed -i 's/"with-serde",\ //g' vmm/Cargo.toml
+        run: sed -i 's/"with-serde",\ //g' vmm/Cargo.toml && sed -i 's/"with-serde",\ //g' hypervisor/Cargo.toml
       - name: Build
         uses: actions-rs/cargo@v1
         with:

arch/Cargo.toml
@@ -8,7 +8,9 @@ default = []
 acpi = ["acpi_tables"]
 
 [dependencies]
+anyhow = "1.0"
 byteorder = "1.3.4"
+hypervisor = { path = "../hypervisor" }
 kvm-bindings = { git = "https://github.com/cloud-hypervisor/kvm-bindings", branch = "ch" }
 kvm-ioctls = { git = "https://github.com/cloud-hypervisor/kvm-ioctls", branch = "ch" }
 libc = "0.2.71"

arch/src/aarch64/gic.rs
@@ -1,9 +1,9 @@
 // Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-use std::{boxed::Box, result};
-
 use kvm_ioctls::{DeviceFd, VmFd};
+use std::sync::Arc;
+use std::{boxed::Box, result};
 
 use super::gicv2::GICv2;
 use super::gicv3::GICv3;
@@ -12,7 +12,7 @@ use super::gicv3::GICv3;
 #[derive(Debug)]
 pub enum Error {
     /// Error while calling KVM ioctl for setting up the global interrupt controller.
-    CreateGIC(kvm_ioctls::Error),
+    CreateGIC(hypervisor::HypervisorVmError),
     /// Error while setting device attributes for the GIC.
     SetDeviceAttribute(kvm_ioctls::Error),
 }
@@ -51,7 +51,7 @@ pub trait GICDevice: Send + Sync {
         Self: Sized;
 
     /// Initialize a GIC device
-    fn init_device(vm: &VmFd) -> Result<DeviceFd>
+    fn init_device(vm: &Arc<dyn hypervisor::Vm>) -> Result<DeviceFd>
     where
         Self: Sized,
     {
@@ -120,7 +120,7 @@ pub trait GICDevice: Send + Sync {
     }
 
     /// Method to initialize the GIC device
-    fn new(vm: &VmFd, vcpu_count: u64) -> Result<Box<dyn GICDevice>>
+    fn new(vm: &Arc<dyn hypervisor::Vm>, vcpu_count: u64) -> Result<Box<dyn GICDevice>>
     where
         Self: Sized,
     {
@@ -140,7 +140,7 @@ pub trait GICDevice: Send + Sync {
 ///
 /// It will try to create by default a GICv3 device. If that fails it will try
 /// to fall-back to a GICv2 device.
-pub fn create_gic(vm: &VmFd, vcpu_count: u64) -> Result<Box<dyn GICDevice>> {
+pub fn create_gic(vm: &Arc<dyn hypervisor::Vm>, vcpu_count: u64) -> Result<Box<dyn GICDevice>> {
     GICv3::new(vm, vcpu_count).or_else(|_| GICv2::new(vm, vcpu_count))
 }
 

arch/src/aarch64/mod.rs
@@ -20,6 +20,7 @@ use kvm_ioctls::*;
 use std::collections::HashMap;
 use std::ffi::CStr;
 use std::fmt::Debug;
+use std::sync::Arc;
 use vm_memory::{
     Address, GuestAddress, GuestAddressSpace, GuestMemory, GuestMemoryAtomic, GuestMemoryMmap,
     GuestUsize,
@@ -41,10 +42,10 @@ pub enum Error {
     REGSConfiguration(regs::Error),
 
     /// Error fetching prefered target
-    VcpuArmPreferredTarget(kvm_ioctls::Error),
+    VcpuArmPreferredTarget(hypervisor::HypervisorVmError),
 
     /// Error doing Vcpu Init on Arm.
-    VcpuArmInit(kvm_ioctls::Error),
+    VcpuArmInit(hypervisor::HypervisorCpuError),
 }
 
 impl From<Error> for super::Error {
@@ -63,9 +64,9 @@ pub struct EntryPoint {
 
 /// Configure the specified VCPU, and return its MPIDR.
 pub fn configure_vcpu(
-    fd: &VcpuFd,
+    fd: &Arc<dyn hypervisor::Vcpu>,
     id: u8,
-    vm_fd: &VmFd,
+    vm_fd: &Arc<dyn hypervisor::Vm>,
     kernel_entry_point: Option<EntryPoint>,
     vm_memory: &GuestMemoryAtomic<GuestMemoryMmap>,
 ) -> super::Result<u64> {
@@ -138,7 +139,7 @@ pub fn arch_memory_regions(size: GuestUsize) -> Vec<(GuestAddress, usize, Region
 #[allow(clippy::too_many_arguments)]
 #[allow(unused_variables)]
 pub fn configure_system<T: DeviceInfoForFDT + Clone + Debug>(
-    vm_fd: &VmFd,
+    vm_fd: &Arc<dyn hypervisor::Vm>,
     guest_mem: &GuestMemoryMmap,
     cmdline_cstring: &CStr,
     vcpu_count: u64,
@@ -198,13 +199,6 @@ pub fn get_host_cpu_phys_bits() -> u8 {
     // It will be replace once rust-vmm/kvm-ioctls is ready.
     //
     40
-}
-
-pub fn check_required_kvm_extensions(kvm: &Kvm) -> super::Result<()> {
-    if !kvm.check_extension(Cap::SignalMsi) {
-        return Err(super::Error::CapabilityMissing(Cap::SignalMsi));
-    }
-    Ok(())
 }
 
 #[cfg(test)]

arch/src/aarch64/regs.rs
@@ -5,8 +5,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the THIRD-PARTY file.
 
-use std::{mem, result};
-
 use super::get_fdt_addr;
 use kvm_bindings::{
     user_pt_regs, KVM_REG_ARM64, KVM_REG_ARM64_SYSREG, KVM_REG_ARM64_SYSREG_CRM_MASK,
@@ -16,16 +14,17 @@ use kvm_bindings::{
     KVM_REG_ARM_CORE, KVM_REG_SIZE_U64,
 };
 use kvm_ioctls::VcpuFd;
-
+use std::sync::Arc;
+use std::{mem, result};
 use vm_memory::GuestMemoryMmap;
 
 /// Errors thrown while setting aarch64 registers.
 #[derive(Debug)]
 pub enum Error {
     /// Failed to set core register (PC, PSTATE or general purpose ones).
-    SetCoreRegister(kvm_ioctls::Error),
+    SetCoreRegister(hypervisor::HypervisorCpuError),
     /// Failed to get a system register.
-    GetSysRegister(kvm_ioctls::Error),
+    GetSysRegister(hypervisor::HypervisorCpuError),
 }
 type Result<T> = result::Result<T, Error>;
 
@@ -122,7 +121,12 @@ arm64_sys_reg!(MPIDR_EL1, 3, 0, 0, 0, 5);
 /// * `cpu_id` - Index of current vcpu.
 /// * `boot_ip` - Starting instruction pointer.
 /// * `mem` - Reserved DRAM for current VM.
-pub fn setup_regs(vcpu: &VcpuFd, cpu_id: u8, boot_ip: u64, mem: &GuestMemoryMmap) -> Result<()> {
+pub fn setup_regs(
+    vcpu: &Arc<dyn hypervisor::Vcpu>,
+    cpu_id: u8,
+    boot_ip: u64,
+    mem: &GuestMemoryMmap,
+) -> Result<()> {
     // Get the register index of the PSTATE (Processor State) register.
     vcpu.set_one_reg(arm64_core_reg!(pstate), PSTATE_FAULT_BITS_64)
         .map_err(Error::SetCoreRegister)?;
@@ -148,7 +152,7 @@ pub fn setup_regs(vcpu: &VcpuFd, cpu_id: u8, boot_ip: u64, mem: &GuestMemoryMmap
 /// # Arguments
 ///
 /// * `vcpu` - Structure for the VCPU that holds the VCPU's fd.
-pub fn read_mpidr(vcpu: &VcpuFd) -> Result<u64> {
+pub fn read_mpidr(vcpu: &Arc<dyn hypervisor::Vcpu>) -> Result<u64> {
     vcpu.get_one_reg(MPIDR_EL1).map_err(Error::GetSysRegister)
 }
 

arch/src/lib.rs
@@ -15,6 +15,7 @@
 )]
 
 extern crate byteorder;
+extern crate hypervisor;
 extern crate kvm_bindings;
 extern crate kvm_ioctls;
 extern crate libc;
@@ -26,7 +27,6 @@ extern crate acpi_tables;
 extern crate arch_gen;
 extern crate linux_loader;
 
-use kvm_ioctls::*;
 use std::fmt;
 use std::result;
 
@@ -57,8 +57,6 @@ pub enum Error {
     ModlistSetup(vm_memory::GuestMemoryError),
     /// RSDP Beyond Guest Memory
     RSDPPastRamEnd,
-    /// Capability missing
-    CapabilityMissing(Cap),
 }
 
 /// Type for returning public functions outcome.
@@ -89,9 +87,9 @@ pub mod aarch64;
 
 #[cfg(target_arch = "aarch64")]
 pub use aarch64::{
-    arch_memory_regions, check_required_kvm_extensions, configure_system, configure_vcpu,
-    fdt::DeviceInfoForFDT, get_host_cpu_phys_bits, get_kernel_start, layout,
-    layout::CMDLINE_MAX_SIZE, layout::IRQ_BASE, layout::IRQ_MAX, EntryPoint,
+    arch_memory_regions, configure_system, configure_vcpu, fdt::DeviceInfoForFDT,
+    get_host_cpu_phys_bits, get_kernel_start, layout, layout::CMDLINE_MAX_SIZE, layout::IRQ_BASE,
+    layout::IRQ_MAX, EntryPoint,
 };
 
 #[cfg(target_arch = "x86_64")]
@@ -99,9 +97,9 @@ pub mod x86_64;
 
 #[cfg(target_arch = "x86_64")]
 pub use x86_64::{
-    arch_memory_regions, check_required_kvm_extensions, configure_system, configure_vcpu,
-    get_host_cpu_phys_bits, initramfs_load_addr, layout, layout::CMDLINE_MAX_SIZE,
-    layout::CMDLINE_START, regs, BootProtocol, CpuidPatch, CpuidReg, EntryPoint,
+    arch_memory_regions, configure_system, configure_vcpu, get_host_cpu_phys_bits,
+    initramfs_load_addr, layout, layout::CMDLINE_MAX_SIZE, layout::CMDLINE_START, regs,
+    BootProtocol, CpuidPatch, CpuidReg, EntryPoint,
 };
 
 /// Safe wrapper for `sysconf(_SC_PAGESIZE)`.

arch/src/x86_64/gdt.rs
@@ -8,8 +8,7 @@
 // found in the LICENSE-BSD-3-Clause file.
 
 // For GDT details see arch/x86/include/asm/segment.h
-
-use kvm_bindings::kvm_segment;
+use hypervisor::x86_64::SegmentRegister;
 
 /// Constructor for a conventional segment GDT (or LDT) entry. Derived from the kernel's segment.h.
 pub fn gdt_entry(flags: u16, base: u32, limit: u32) -> u64 {
@@ -88,14 +87,14 @@ fn get_type(entry: u64) -> u8 {
     ((entry & 0x00000F0000000000) >> 40) as u8
 }
 
-/// Automatically build the kvm struct for SET_SREGS from the kernel bit fields.
+/// Automatically build the struct for SET_SREGS from the kernel bit fields.
 ///
 /// # Arguments
 ///
 /// * `entry` - The gdt entry.
 /// * `table_index` - Index of the entry in the gdt table.
-pub fn kvm_segment_from_gdt(entry: u64, table_index: u8) -> kvm_segment {
-    kvm_segment {
+pub fn segment_from_gdt(entry: u64, table_index: u8) -> SegmentRegister {
+    SegmentRegister {
         base: get_base(entry),
         limit: get_limit(entry),
         selector: (table_index * 8) as u16,
@@ -122,7 +121,7 @@ mod tests {
     #[test]
     fn field_parse() {
         let gdt = gdt_entry(0xA09B, 0x100000, 0xfffff);
-        let seg = kvm_segment_from_gdt(gdt, 0);
+        let seg = segment_from_gdt(gdt, 0);
         // 0xA09B
         // 'A'
         assert_eq!(0x1, seg.g);

commit 7101dee614ea59077da556d21a26e58eaef6d5e8

vmm: device_manager: restore error handling
When the hypervisor crate was introduced, a few places that handled
errors were commented out in favor of unwrap, but that's bad practice.

Restore proper error handling in those places in this patch.

We cannot use from_raw_os_error anymore because it is wrapped deep under
hypervisor crate. Create new custom errors instead.

Fixes: e4dee57e81369 ("arch, pci, vmm: Initial switch to the hypervisor crate")
Signed-off-by: Wei Liu <liuwe@microsoft.com>

vmm/src/device_manager.rs
@@ -564,16 +564,25 @@ impl DeviceRelocation for AddressManager {
             if bar_addr == new_base {
                 for (event, addr) in virtio_pci_dev.ioeventfds(old_base) {
                     let io_addr = IoEventAddress::Mmio(addr);
-                    self.vm_fd.unregister_ioevent(event, &io_addr).unwrap();
-                    /*
-                    map_err(io::Error::from_raw_os_error(
-                        hypervisor::HypervisorVmError::UnregisterIoEvent,
-                    ))?; */
+                    self.vm_fd
+                        .unregister_ioevent(event, &io_addr)
+                        .map_err(|e| {
+                            io::Error::new(
+                                io::ErrorKind::Other,
+                                format!("failed to unregister ioevent: {:?}", e),
+                            )
+                        })?;
                 }
                 for (event, addr) in virtio_pci_dev.ioeventfds(new_base) {
                     let io_addr = IoEventAddress::Mmio(addr);
-                    self.vm_fd.register_ioevent(event, &io_addr, None).unwrap();
-                    //.map_err(|e| io::Error::from_raw_os_error(e.errno()))?;
+                    self.vm_fd
+                        .register_ioevent(event, &io_addr, None)
+                        .map_err(|e| {
+                            io::Error::new(
+                                io::ErrorKind::Other,
+                                format!("failed to register ioevent: {:?}", e),
+                            )
+                        })?;
                 }
             } else {
                 let virtio_dev = virtio_pci_dev.virtio_device();
@@ -589,8 +598,12 @@ impl DeviceRelocation for AddressManager {
                             flags: 0,
                         };
 
-                        self.vm_fd.set_user_memory_region(mem_region).unwrap();
-                        //.map_err(|e| io::Error::from_raw_os_error(e.errno()))?;
+                        self.vm_fd.set_user_memory_region(mem_region).map_err(|e| {
+                            io::Error::new(
+                                io::ErrorKind::Other,
+                                format!("failed to set user memory region: {:?}", e),
+                            )
+                        })?;
 
                         // Create new mapping by inserting new region to KVM.
                         mem_region.guest_phys_addr = new_base;