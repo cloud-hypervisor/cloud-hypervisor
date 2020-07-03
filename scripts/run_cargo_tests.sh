c9f0005828a17c71ebe712dcc45b30be47389c5f
Rob Bradford <robert.bradford@intel.com>
2020-07-03 15:14:36 +0100

    vhost_user_net: Move binary into vhost_user_net crate
    The binary is still built in the same location but the source code and
    the dependencies for it come from the vhost_user_net crate itself.
    
    The binary will be built with:
    
    `cargo build --all --bin vhost_user_net` or just `cargo build --all`
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- scripts/run_cargo_tests.sh
+++ scripts/run_cargo_tests.sh
@@ -12,17 +12,17 @@ time which cargo-audit || cargo install cargo-audit
 # Run cargo builds and checks
 time cargo clippy --all-targets --all-features -- -D warnings
 time cargo rustc --bin cloud-hypervisor -- -D warnings
-time cargo rustc --bin vhost_user_net -- -D warnings
+time cargo rustc -p vhost_user_net --bin vhost_user_net -- -D warnings
 time cargo test
 time cargo audit
 time cargo clippy --all-targets --no-default-features --features "pci,acpi" -- -D warnings
 time cargo rustc --bin cloud-hypervisor --no-default-features --features "pci,acpi"  -- -D warnings
-time cargo rustc --bin vhost_user_net --no-default-features --features "pci,acpi"  -- -D warnings
+time cargo rustc -p vhost_user_net --bin vhost_user_net --no-default-features --features "pci,acpi"  -- -D warnings
 time cargo clippy --all-targets --no-default-features --features "pci" -- -D warnings
 time cargo rustc --bin cloud-hypervisor --no-default-features --features "pci"  -- -D warnings
-time cargo rustc --bin vhost_user_net --no-default-features --features "pci"  -- -D warnings
+time cargo rustc -p vhost_user_net --bin vhost_user_net --no-default-features --features "pci"  -- -D warnings
 time cargo clippy --all-targets --no-default-features --features "mmio" -- -D warnings
 time cargo rustc --bin cloud-hypervisor --no-default-features --features "mmio"  -- -D warnings
-time cargo rustc --bin vhost_user_net --no-default-features --features "mmio"  -- -D warnings
+time cargo rustc -p vhost_user_net --bin vhost_user_net --no-default-features --features "mmio"  -- -D warnings
 time cargo fmt -- --check
 time cargo build --all --release
