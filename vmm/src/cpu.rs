da58c1c9201a433d96b5127cf71923fe9270084c
Henry Wang <Henry.Wang@arm.com>
2020-09-04 23:37:52 +0800

    vmm: AArch64: add methods to get saved vCPU states
    The construction of `GICR_TYPER` register will need vCPU states.
    Therefore this commit adds methods to extract saved vCPU states
    from the cpu manager.
    
    Signed-off-by: Henry Wang <Henry.Wang@arm.com>

--- vmm/src/cpu.rs
+++ vmm/src/cpu.rs
@@ -328,6 +328,12 @@ impl Vcpu {
         self.mpidr
     }
 
+    /// Gets the saved vCPU state.
+    #[cfg(target_arch = "aarch64")]
+    pub fn get_saved_state(&self) -> Option<CpuState> {
+        self.saved_state.clone()
+    }
+
     /// Runs the VCPU until it exits, returning the reason.
     ///
     /// Note that the state of the VCPU and associated VM must be setup first for this to do
@@ -1038,6 +1044,14 @@ impl CpuManager {
         self.vcpus
             .iter()
             .map(|cpu| cpu.lock().unwrap().get_mpidr())
+            .collect()
+    }
+
+    #[cfg(target_arch = "aarch64")]
+    pub fn get_saved_states(&self) -> Vec<CpuState> {
+        self.vcpus
+            .iter()
+            .map(|cpu| cpu.lock().unwrap().get_saved_state().unwrap())
             .collect()
     }
 
