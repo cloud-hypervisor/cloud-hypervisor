f71a6a0d1cd8f1c1fc750cfafb027f1b09e592d2
Sebastien Boeuf <sebastien.boeuf@intel.com>
2020-09-23 17:08:45 +0200

    ci: Fix virtiofsd build by staying on older branch
    While we figure out the details on how to correctly build virtiofsd from
    the latest rebase from the branch "virtio-fs-dev" (which now relies on
    QEMU's new build system), let's fix the CI by relying on an older branch
    which still relies on the previous build system.
    
    Signed-off-by: Sebastien Boeuf <sebastien.boeuf@intel.com>

--- scripts/run_integration_tests_aarch64.sh
+++ scripts/run_integration_tests_aarch64.sh
@@ -126,7 +126,7 @@ update_workloads() {
 
     if [ ! -f "$VIRTIOFSD" ]; then
         pushd $WORKLOADS_DIR
-        git clone --depth 1 "https://gitlab.com/virtio-fs/qemu.git" -b "virtio-fs-dev" $QEMU_DIR
+        git clone --depth 1 "https://gitlab.com/virtio-fs/qemu.git" -b "qemu5.0-virtiofs-dax" $QEMU_DIR
         pushd $QEMU_DIR
         time ./configure --prefix=$PWD --target-list=aarch64-softmmu
         time make virtiofsd -j `nproc`
