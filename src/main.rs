7fedf3c0bcd50e7537879487444a2180f339c333
Rob Bradford <robert.bradford@intel.com>
2020-09-30 16:53:23 +0100

    main: Improve documentation for --kernel
    Further mark this argument as required as it it necessary.
    
    Fixes: #1712
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- src/main.rs
+++ src/main.rs
@@ -133,8 +133,10 @@ fn create_app<'a, 'b>(
         .arg(
             Arg::with_name("kernel")
                 .long("kernel")
-                .help("Path to kernel image (vmlinux)")
+                .help("Path to loaded kernel. This may be a kernel or firmware that supports a PVH \
+                entry point, a vmlinux ELF file or a Linux bzImage or achitecture equivalent")
                 .takes_value(true)
+                .required(true)
                 .group("vm-config"),
         )
         .arg(
