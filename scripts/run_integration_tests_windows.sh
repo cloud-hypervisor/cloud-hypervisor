3e6be1d82bce919366bcc19747058395b7d5eeba
Rob Bradford <robert.bradford@intel.com>
2020-10-08 15:11:18 +0100

    scripts: Add wrapper script to run Windows guest integration tests
    Adding a dedicated script to avoid confusion with other Linux
    integration tests scripts.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

---
+++ scripts/run_integration_tests_windows.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+
+source $HOME/.cargo/env
+
+BUILD_TARGET="$(uname -m)-unknown-linux-${CH_LIBC}"
+CFLAGS=""
+TARGET_CC=""
+if [[ "${BUILD_TARGET}" == "x86_64-unknown-linux-musl" ]]; then
+TARGET_CC="musl-gcc"
+CFLAGS="-I /usr/include/x86_64-linux-musl/ -idirafter /usr/include/"
+fi
+
+cargo build --all --release --target $BUILD_TARGET
+strip target/$BUILD_TARGET/release/cloud-hypervisor
+
+export RUST_BACKTRACE=1
+
+time cargo test --features "integration_tests" "tests::windows::$@"
+RES=$?
+
+exit $RES
