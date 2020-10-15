c7e5fea0a408448a6799bb3462fa90c5f7ec62b9
Sebastien Boeuf <sebastien.boeuf@intel.com>
2020-10-15 17:54:56 +0200

    virtio-devices: Remove mmio and pci differentiation
    Signed-off-by: Sebastien Boeuf <sebastien.boeuf@intel.com>

--- virtio-devices/src/transport/mod.rs
+++ virtio-devices/src/transport/mod.rs
@@ -3,21 +3,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use vmm_sys_util::eventfd::EventFd;
-#[cfg(feature = "pci_support")]
 mod pci_common_config;
-#[cfg(feature = "pci_support")]
 mod pci_device;
-#[cfg(feature = "pci_support")]
 pub use pci_common_config::VirtioPciCommonConfig;
-#[cfg(feature = "pci_support")]
 pub use pci_device::VirtioPciDevice;
-
-#[cfg(feature = "mmio_support")]
-mod mmio;
-#[cfg(feature = "mmio_support")]
-pub use mmio::MmioDevice;
-#[cfg(feature = "mmio_support")]
-pub const NOTIFY_REG_OFFSET: u32 = 0x50;
 
 pub trait VirtioTransport {
     fn ioeventfds(&self, base_addr: u64) -> Vec<(&EventFd, u64)>;
