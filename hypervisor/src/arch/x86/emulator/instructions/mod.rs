b8f7e759400539798d11a03f5ec67ba4a4ea1202
Samuel Ortiz <sameo@linux.intel.com>
2020-11-19 20:20:23 +0100

    hypervisor: x86: Emulate MOV
    And add a few unit tests based on a Mock platform.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- hypervisor/src/arch/x86/emulator/instructions/mod.rs
+++ hypervisor/src/arch/x86/emulator/instructions/mod.rs
@@ -6,12 +6,42 @@
 
 extern crate iced_x86;
 
-use crate::arch::emulator::{EmulationError, PlatformEmulator};
+use crate::arch::emulator::{EmulationError, PlatformEmulator, PlatformError};
 use crate::arch::x86::emulator::CpuStateManager;
 use crate::arch::x86::Exception;
 use iced_x86::*;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
+
+pub mod mov;
+
+// Returns the linear a.k.a. virtual address for a memory operand.
+fn memory_operand_address<T: CpuStateManager>(
+    insn: &Instruction,
+    state: &T,
+) -> Result<u64, PlatformError> {
+    let mut address: u64 = 0;
+
+    // Get the DS or override segment base first
+    let segment_base = state.read_segment(insn.memory_segment())?.base;
+    address += segment_base;
+
+    if insn.memory_base() != iced_x86::Register::None {
+        let base: u64 = state.read_reg(insn.memory_base())?;
+        address += base;
+    }
+
+    if insn.memory_index() != iced_x86::Register::None {
+        let mut index: u64 = state.read_reg(insn.memory_index())?;
+        index *= insn.memory_index_scale() as u64;
+
+        address += index;
+    }
+
+    address += insn.memory_displacement() as u64;
+
+    Ok(address)
+}
 
 pub trait InstructionHandler<T: CpuStateManager> {
     fn emulate(
