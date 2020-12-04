c0700dfacb304de12b0d293100bd293f2ae44421
Rob Bradford <robert.bradford@intel.com>
2020-12-04 09:19:03 +0000

    devices: acpi: Remove empty BusDevice::write implementation
    The trait already includes an empty implementation of this function.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- devices/src/acpi.rs
+++ devices/src/acpi.rs
@@ -93,8 +93,6 @@ impl BusDevice for AcpiGEDDevice {
         data[0] = self.notification_type.bits();
         self.notification_type = HotPlugNotificationFlags::NO_DEVICES_CHANGED;
     }
-
-    fn write(&mut self, _base: u64, _offset: u64, _data: &[u8]) {}
 }
 
 #[cfg(feature = "acpi")]
@@ -183,6 +181,4 @@ impl BusDevice for AcpiPMTimerDevice {
 
         data.copy_from_slice(&counter.to_le_bytes());
     }
-
-    fn write(&mut self, _base: u64, _offset: u64, _data: &[u8]) {}
 }
