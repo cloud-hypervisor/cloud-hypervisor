539708e45ec55fbbdd00d2383725edc686efdde7
Rob Bradford <robert.bradford@intel.com>
2020-12-17 16:56:39 +0000

    scripts, resources: x86_64: Update to Linux kernel v5.10 base
    This allows the removal of most of the custom patches in particular all
    the virtio-fs ones have been integrated.
    
    Partially fixes: #2070
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- scripts/run_integration_tests_x86_64.sh
+++ scripts/run_integration_tests_x86_64.sh
@@ -105,7 +105,7 @@ LINUX_CUSTOM_DIR="$WORKLOADS_DIR/linux-custom"
 if [ ! -f "$VMLINUX_IMAGE" ] || [ ! -f "$VMLINUX_PVH_IMAGE" ]; then
     SRCDIR=$PWD
     pushd $WORKLOADS_DIR
-    time git clone --depth 1 "https://github.com/cloud-hypervisor/linux.git" -b "virtio-fs-virtio-iommu-5.8-rc4" $LINUX_CUSTOM_DIR
+    time git clone --depth 1 "https://github.com/cloud-hypervisor/linux.git" -b "ch-5.10" $LINUX_CUSTOM_DIR
     cp $SRCDIR/resources/linux-config-x86_64 $LINUX_CUSTOM_DIR/.config
     popd
 fi
