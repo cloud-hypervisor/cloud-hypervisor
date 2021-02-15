a330a1569a2a7ff1f409c1feba9a960e0b4f7ffa
Rob Bradford <robert.bradford@intel.com>
2021-02-15 16:12:26 +0000

    arch, arch_gen, hypervisor: Remove some unnecessary clippy attributes
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- hypervisor/src/arch/x86/mod.rs
+++ hypervisor/src/arch/x86/mod.rs
@@ -11,26 +11,13 @@
 // Copyright © 2020, Microsoft Corporation
 //
 
+pub mod emulator;
 pub mod gdt;
-
 #[allow(non_upper_case_globals)]
 #[allow(non_camel_case_types)]
 #[allow(non_snake_case)]
 #[allow(non_upper_case_globals)]
-#[allow(unused)]
-#[allow(
-    clippy::unreadable_literal,
-    clippy::redundant_static_lifetimes,
-    clippy::trivially_copy_pass_by_ref,
-    clippy::useless_transmute,
-    clippy::should_implement_trait,
-    clippy::transmute_ptr_to_ptr,
-    clippy::unreadable_literal,
-    clippy::redundant_static_lifetimes
-)]
 pub mod msr_index;
-
-pub mod emulator;
 
 // MTRR constants
 pub const MTRR_ENABLE: u64 = 0x800; // IA32_MTRR_DEF_TYPE MSR: E (MTRRs enabled) flag, bit 11
