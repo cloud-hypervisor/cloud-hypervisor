88e21a9a01ab3de2b20d5416f8d1ec17da5c1e7c
Rob Bradford <robert.bradford@intel.com>
2021-02-18 15:10:51 +0000

    virtio-devices: Report events for virtio device activation and reset
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- virtio-devices/src/pmem.rs
+++ virtio-devices/src/pmem.rs
@@ -428,13 +428,16 @@ impl VirtioDevice for Pmem {
 
             self.common.epoll_threads = Some(epoll_threads);
 
+            event!("virtio-device", "activated", "id", &self.id);
             return Ok(());
         }
         Err(ActivateError::BadActivate)
     }
 
     fn reset(&mut self) -> Option<Arc<dyn VirtioInterrupt>> {
-        self.common.reset()
+        let result = self.common.reset();
+        event!("virtio-device", "reset", "id", &self.id);
+        result
     }
 
     fn userspace_mappings(&self) -> Vec<UserspaceMapping> {
--- virtio-devices/src/watchdog.rs
+++ virtio-devices/src/watchdog.rs
@@ -355,11 +355,14 @@ impl VirtioDevice for Watchdog {
 
         self.common.epoll_threads = Some(epoll_threads);
 
+        event!("virtio-device", "activated", "id", &self.id);
         Ok(())
     }
 
     fn reset(&mut self) -> Option<Arc<dyn VirtioInterrupt>> {
-        self.common.reset()
+        let result = self.common.reset();
+        event!("virtio-device", "reset", "id", &self.id);
+        result
     }
 }
 
