de02f293fea4bfad21103feb937a6b07b5242a86
Rob Bradford <robert.bradford@intel.com>
2021-05-19 13:12:54 +0000

    hypervisor: Unconditional build  Hypervisor::create_vm_with_type()
    Mark it as unreachable for now in the default implementation as this is
    currently only used on tdx code path which is KVM only.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- hypervisor/src/hypervisor.rs
+++ hypervisor/src/hypervisor.rs
@@ -72,12 +72,13 @@ pub trait Hypervisor: Send + Sync {
     /// Return a hypervisor-agnostic Vm trait object
     ///
     fn create_vm(&self) -> Result<Arc<dyn Vm>>;
-    #[cfg(feature = "kvm")]
     ///
     /// Create a Vm of a specific type using the underlying hypervisor
     /// Return a hypervisor-agnostic Vm trait object
     ///
-    fn create_vm_with_type(&self, vm_type: u64) -> Result<Arc<dyn Vm>>;
+    fn create_vm_with_type(&self, _vm_type: u64) -> Result<Arc<dyn Vm>> {
+        unreachable!()
+    }
     #[cfg(target_arch = "x86_64")]
     ///
     /// Get the supported CpuID
