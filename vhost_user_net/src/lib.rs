e63436568a5a59dedd0af8c235cc5870f17d684e
Sebastien Boeuf <sebastien.boeuf@intel.com>
2021-05-20 23:37:26 +0200

    vhost_user_net: Extend list of supported virtio features
    Extend the current list of available virtio features in order to make it
    work correctly with the VMM side.
    
    Signed-off-by: Sebastien Boeuf <sebastien.boeuf@intel.com>

--- vhost_user_net/src/lib.rs
+++ vhost_user_net/src/lib.rs
@@ -263,10 +263,23 @@ impl VhostUserBackend for VhostUserNetBackend {
     }
 
     fn features(&self) -> u64 {
-        1 << VIRTIO_F_VERSION_1
+        1 << VIRTIO_NET_F_GUEST_CSUM
+            | 1 << VIRTIO_NET_F_CSUM
+            | 1 << VIRTIO_NET_F_GUEST_TSO4
+            | 1 << VIRTIO_NET_F_GUEST_TSO6
+            | 1 << VIRTIO_NET_F_GUEST_ECN
+            | 1 << VIRTIO_NET_F_GUEST_UFO
+            | 1 << VIRTIO_NET_F_HOST_TSO4
+            | 1 << VIRTIO_NET_F_HOST_TSO6
+            | 1 << VIRTIO_NET_F_HOST_ECN
+            | 1 << VIRTIO_NET_F_HOST_UFO
+            | 1 << VIRTIO_NET_F_MRG_RXBUF
+            | 1 << VIRTIO_F_NOTIFY_ON_EMPTY
+            | 1 << VIRTIO_F_VERSION_1
             | 1 << VIRTIO_RING_F_EVENT_IDX
             | 1 << VIRTIO_NET_F_CTRL_VQ
             | 1 << VIRTIO_NET_F_MQ
+            | 1 << VIRTIO_NET_F_MAC
             | VhostUserVirtioFeatures::PROTOCOL_FEATURES.bits()
     }
 
