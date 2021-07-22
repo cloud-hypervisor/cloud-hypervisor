cade7a48a5bad13fc613a7e444b23f76131ca801
Bo Chen <chen.bo@intel.com>
2021-07-22 09:43:19 -0700

    vmm: Create guest memory regions without dirty-pages-log by default
    With the support of dynamically turning on/off dirty-pages-log during
    live-migration (only for guest RAM regions), we now can create guest
    memory regions without dirty-pages-log by default both for guest RAM
    regions and other regions backed by file/device.
    
    Signed-off-by: Bo Chen <chen.bo@intel.com>

--- vmm/src/vm.rs
+++ vmm/src/vm.rs
@@ -765,8 +765,6 @@ impl Vm {
             &config.lock().unwrap().memory.clone(),
             false,
             phys_bits,
-            #[cfg(feature = "tdx")]
-            tdx_enabled,
         )
         .map_err(Error::MemoryManager)?;
 
@@ -887,8 +885,6 @@ impl Vm {
             &config.lock().unwrap().memory.clone(),
             false,
             phys_bits,
-            #[cfg(feature = "tdx")]
-            false,
         )
         .map_err(Error::MemoryManager)?;
 
