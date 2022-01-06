9c5544c73eb3455fc2f02e0cb7be632b17c43083
Bo Chen <chen.bo@intel.com>
2022-01-06 15:59:50 -0800

    tests: Add crate-level attribute to silence "dead-code" warnings
    When enabling the `mshv` feature, we skip quite some tests and
    hence have known dead-code. This annotation silences dead-code
    related warnings for our quality workflow to pass.
    
    Signed-off-by: Bo Chen <chen.bo@intel.com>

--- tests/integration.rs
+++ tests/integration.rs
@@ -3,6 +3,11 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+// When enabling the `mshv` feature, we skip quite some tests and
+// hence have known dead-code. This annotation silences dead-code
+// related warnings for our quality workflow to pass.
+#![allow(dead_code)]
+
 #[macro_use]
 extern crate lazy_static;
 
