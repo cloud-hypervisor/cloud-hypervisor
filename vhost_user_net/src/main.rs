0ee8ead53d14abdd46144e2a8dc2c8274e7de80b
Bo Chen <chen.bo@intel.com>
2022-02-16 17:23:45 -0800

    ch-remote, main, vhost_user: Fix deprecated APIs for clap v3.1.0
    Signed-off-by: Bo Chen <chen.bo@intel.com>

--- vhost_user_net/src/main.rs
+++ vhost_user_net/src/main.rs
@@ -9,13 +9,13 @@
 #[macro_use(crate_version, crate_authors)]
 extern crate clap;
 
-use clap::{App, Arg};
+use clap::{Arg, Command};
 use vhost_user_net::start_net_backend;
 
 fn main() {
     env_logger::init();
 
-    let cmd_arguments = App::new("vhost-user-net backend")
+    let cmd_arguments = Command::new("vhost-user-net backend")
         .version(crate_version!())
         .author(crate_authors!())
         .about("Launch a vhost-user-net backend.")
