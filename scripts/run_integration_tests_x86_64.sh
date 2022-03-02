2c3908eed945bc4e670d631d615c36f780d5335b
Michael Zhao <michael.zhao@arm.com>
2022-03-02 20:25:12 +0800

    scripts: Remove some obselete settings for musl
    Additional settings on `TARGET_CC` and `CFLAGS` for `musl` target are no
    longer needed on X86_64.
    
    Signed-off-by: Michael Zhao <michael.zhao@arm.com>

--- scripts/run_integration_tests_x86_64.sh
+++ scripts/run_integration_tests_x86_64.sh
@@ -174,12 +174,6 @@ cp $FW $VFIO_DIR
 cp $VMLINUX_IMAGE $VFIO_DIR || exit 1
 
 BUILD_TARGET="$(uname -m)-unknown-linux-${CH_LIBC}"
-CFLAGS=""
-TARGET_CC=""
-if [[ "${BUILD_TARGET}" == "x86_64-unknown-linux-musl" ]]; then
-TARGET_CC="musl-gcc"
-CFLAGS="-I /usr/include/x86_64-linux-musl/ -idirafter /usr/include/"
-fi
 
 cargo build --all  --release $features --target $BUILD_TARGET
 strip target/$BUILD_TARGET/release/cloud-hypervisor
