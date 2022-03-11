603ba8808ea3f0b20ae76affb0c6fe61bbf5a946
Jianyong Wu <jianyong.wu@arm.com>
2022-03-11 10:26:30 +0800

    performance-metrics: prepare image and toolchain for AArch64
    Prepare disk image, kernel image and toolchain for AArch64 before run
    performance-metrics tests.
    
    Signed-off-by: Jianyong Wu <jianyong.wu@arm.com>

--- scripts/run_metrics.sh
+++ scripts/run_metrics.sh
@@ -4,7 +4,9 @@ set -x
 source $HOME/.cargo/env
 source $(dirname "$0")/test-util.sh
 
-export BUILD_TARGET=${BUILD_TARGET-x86_64-unknown-linux-gnu}
+export TEST_ARCH=$(uname -m)
+export BUILD_TARGET=${BUILD_TARGET-${TEST_ARCH}-unknown-linux-gnu}
+
 
 WORKLOADS_DIR="$HOME/workloads"
 mkdir -p "$WORKLOADS_DIR"
@@ -18,9 +20,14 @@ if [ "$hypervisor" = "mshv" ]; then
     features="--no-default-features --features mshv,common"
 fi
 
-cp scripts/sha1sums-x86_64 $WORKLOADS_DIR
+cp scripts/sha1sums-${TEST_ARCH} $WORKLOADS_DIR
 
-FOCAL_OS_IMAGE_NAME="focal-server-cloudimg-amd64-custom-20210609-0.qcow2"
+if [ ${TEST_ARCH} == "aarch64" ]; then
+     FOCAL_OS_IMAGE_NAME="focal-server-cloudimg-arm64-custom-20210929-0.qcow2"
+else
+     FOCAL_OS_IMAGE_NAME="focal-server-cloudimg-amd64-custom-20210609-0.qcow2"
+fi
+
 FOCAL_OS_IMAGE_URL="https://cloud-hypervisor.azureedge.net/$FOCAL_OS_IMAGE_NAME"
 FOCAL_OS_IMAGE="$WORKLOADS_DIR/$FOCAL_OS_IMAGE_NAME"
 if [ ! -f "$FOCAL_OS_IMAGE" ]; then
@@ -29,7 +36,12 @@ if [ ! -f "$FOCAL_OS_IMAGE" ]; then
     popd
 fi
 
-FOCAL_OS_RAW_IMAGE_NAME="focal-server-cloudimg-amd64-custom-20210609-0.raw"
+if [ ${TEST_ARCH} == "aarch64" ]; then
+    FOCAL_OS_RAW_IMAGE_NAME="focal-server-cloudimg-arm64-custom-20210929-0.raw"
+else
+    FOCAL_OS_RAW_IMAGE_NAME="focal-server-cloudimg-amd64-custom-20210609-0.raw"
+fi
+
 FOCAL_OS_RAW_IMAGE="$WORKLOADS_DIR/$FOCAL_OS_RAW_IMAGE_NAME"
 if [ ! -f "$FOCAL_OS_RAW_IMAGE" ]; then
     pushd $WORKLOADS_DIR
@@ -38,7 +50,7 @@ if [ ! -f "$FOCAL_OS_RAW_IMAGE" ]; then
 fi
 
 pushd $WORKLOADS_DIR
-grep focal sha1sums-x86_64 | sha1sum --check
+grep focal sha1sums-${TEST_ARCH} | sha1sum --check
 if [ $? -ne 0 ]; then
     echo "sha1sum validation of images failed, remove invalid images to fix the issue."
     exit 1
@@ -46,7 +58,11 @@ fi
 popd
 
 # Build custom kernel based on virtio-pmem and virtio-fs upstream patches
-VMLINUX_IMAGE="$WORKLOADS_DIR/vmlinux"
+if [ ${TEST_ARCH} == "aarch64" ]; then
+       VMLINUX_IMAGE="$WORKLOADS_DIR/Image"
+else
+       VMLINUX_IMAGE="$WORKLOADS_DIR/vmlinux"
+fi
 
 LINUX_CUSTOM_DIR="$WORKLOADS_DIR/linux-custom"
 
@@ -54,14 +70,19 @@ if [ ! -f "$VMLINUX_IMAGE" ]; then
     SRCDIR=$PWD
     pushd $WORKLOADS_DIR
     time git clone --depth 1 "https://github.com/cloud-hypervisor/linux.git" -b "ch-5.15.12" $LINUX_CUSTOM_DIR
-    cp $SRCDIR/resources/linux-config-x86_64 $LINUX_CUSTOM_DIR/.config
+    cp $SRCDIR/resources/linux-config-${TEST_ARCH} $LINUX_CUSTOM_DIR/.config
     popd
 fi
 
 if [ ! -f "$VMLINUX_IMAGE" ]; then
     pushd $LINUX_CUSTOM_DIR
-    time make bzImage -j $(nproc)
-    cp vmlinux $VMLINUX_IMAGE || exit 1
+    if [ ${TEST_ARCH} == "x86_64" ]; then
+       make bzImage -j `nproc`
+       cp vmlinux $VMLINUX_IMAGE || exit 1
+    elif [ ${TEST_ARCH} == "aarch64" ]; then
+       make Image -j `nproc`
+       cp arch/arm64/boot/Image $VMLINUX_IMAGE || exit 1
+    fi
     popd
 fi
 
@@ -69,12 +90,12 @@ if [ -d "$LINUX_CUSTOM_DIR" ]; then
     rm -rf $LINUX_CUSTOM_DIR
 fi
 
-BUILD_TARGET="$(uname -m)-unknown-linux-${CH_LIBC}"
+BUILD_TARGET="${TEST_ARCH}-unknown-linux-${CH_LIBC}"
 CFLAGS=""
 TARGET_CC=""
-if [[ "${BUILD_TARGET}" == "x86_64-unknown-linux-musl" ]]; then
+if [[ "${BUILD_TARGET}" == "${TEST_ARCH}-unknown-linux-musl" ]]; then
     TARGET_CC="musl-gcc"
-    CFLAGS="-I /usr/include/x86_64-linux-musl/ -idirafter /usr/include/"
+    CFLAGS="-I /usr/include/${TEST_ARCH}-linux-musl/ -idirafter /usr/include/"
 fi
 
 cargo build --all --release $features --target $BUILD_TARGET
