8ab3aaee6aa03c5ab126ef125911037c3ec3ded8
Rob Bradford <robert.bradford@intel.com>
2022-05-10 14:00:11 +0100

    hypervisor: emulator: Remove some glob use imports
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- hypervisor/src/arch/x86/emulator/mod.rs
+++ hypervisor/src/arch/x86/emulator/mod.rs
@@ -6,9 +6,8 @@
 
 use crate::arch::emulator::{EmulationError, EmulationResult, PlatformEmulator, PlatformError};
 use crate::arch::x86::emulator::instructions::*;
-use crate::arch::x86::regs::*;
-use crate::arch::x86::*;
-use crate::arch::x86::{Exception, SegmentRegisterOps};
+use crate::arch::x86::regs::{CR0_PE, EFER_LMA};
+use crate::arch::x86::{segment_type_expand_down, segment_type_ro, Exception, SegmentRegisterOps};
 use crate::x86_64::{SegmentRegister, SpecialRegisters, StandardRegisters};
 use anyhow::Context;
 use iced_x86::*;
