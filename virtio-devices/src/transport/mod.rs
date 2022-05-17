657fdde92759b6a23507183990555ba669064fcf
Rob Bradford <robert.bradford@intel.com>
2022-05-17 15:33:33 +0100

    virtio-devices, vmm: Optimised async virtio device activation
    In order to ensure that the virtio device thread is spawned from the vmm
    thread we use an asynchronous activation mechanism for the virtio
    devices. This change optimises that code so that we do not need to
    iterate through all virtio devices on the platform in order to find the
    one that requires activation. We solve this by creating a separate short
    lived VirtioPciDeviceActivator that holds the required state for the
    activation (e.g. the clones of the queues) this can then be stored onto
    the device manager ready for asynchronous activation.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- virtio-devices/src/transport/mod.rs
+++ virtio-devices/src/transport/mod.rs
@@ -6,7 +6,7 @@ use vmm_sys_util::eventfd::EventFd;
 mod pci_common_config;
 mod pci_device;
 pub use pci_common_config::VirtioPciCommonConfig;
-pub use pci_device::VirtioPciDevice;
+pub use pci_device::{VirtioPciDevice, VirtioPciDeviceActivator};
 
 pub trait VirtioTransport {
     fn ioeventfds(&self, base_addr: u64) -> Vec<(&EventFd, u64)>;
