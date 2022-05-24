952275e1aabec5d017f1cd4cb7895bd4e7c3e803
Praveen K Paladugu <prapal@microsoft.com>
2022-05-24 19:19:53 +0000

    vtpm: Add error Handling
    Scrub unused code

--- vtpm/src/char.rs
+++ vtpm/src/char.rs
@@ -1,23 +1,42 @@
-use std::sync::{Arc, Mutex};
-use std::cmp;
-use std::io;
-use std::io::{BufRead, BufReader};
-use std::os::unix::net::{UnixStream,UnixListener};
-use std::thread;
-use std::time::{Duration, Instant};
-use std::thread::sleep;
-use std::str;
+// Copyright © 2022, Microsoft Corporation
+//
+// SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
+//
+
+use anyhow::anyhow;
+use nix::sys::socket::{recv, recvfrom, sendmsg, ControlMessage, MsgFlags};
+use nix::sys::uio::IoVec;
 use std::io::prelude::*;
 use std::io::Write;
-use std::os::unix::io::{RawFd, AsRawFd};
-use nix::unistd::{read, write};
-use nix::sys::socket::{socketpair, AddressFamily, SockType, SockFlag, recv, sendmsg, recvfrom, ControlMessage, MsgFlags };
-use nix::sys::uio::IoVec;
-use libc;
+use std::os::unix::io::{AsRawFd, RawFd};
+use std::os::unix::net::UnixStream;
+use std::str;
+//use std::sync::{Arc, Mutex};
+use std::thread::sleep;
+use std::time::{Duration, Instant};
+use thiserror::Error;
 
 const TPM_TIS_BUFFER_MAX: usize = 4096;
 
-type Result<T> = std::result::Result<T, Error>;
+#[derive(Error, Debug)]
+pub enum TPMCharError {
+    #[error("Cannot connect to TPM Socket even after retrying for 1 min")]
+    TPMCharCannotConnect(#[source] anyhow::Error),
+    #[error("Failed to read from TPM socket")]
+    TPMCharFailedRead(#[source] anyhow::Error),
+    #[error("Failed to write to TPM socket")]
+    TPMCharFailedWrite(#[source] anyhow::Error),
+    #[error("Failed to configure TPM Char backend")]
+    CharBackendConfigureFailed(#[source] anyhow::Error)
+}
+type Result<T> = anyhow::Result<T, TPMCharError>;
+
+#[derive(PartialEq)]
+enum ChardevState {
+    ChardevStateDisconnected,
+    ChardevStateConnecting,
+    ChardevStateConnected,
+}
 
 /// Copy data in `from` into `to`, until the shortest
 /// of the two slices.
@@ -27,22 +46,16 @@ fn byte_copy(from: &[u8], mut to: &mut [u8]) -> usize {
     to.write(from).unwrap()
 }
 
-#[derive(PartialEq)]
-enum ChardevState {
-    ChardevStateDisconnected,
-    ChardevStateConnecting,
-    ChardevStateConnected,
-}
-
 pub struct SocketCharDev {
     state: ChardevState,
     stream: Option<UnixStream>,
+    /// Fd sent to swtpm process for Data
     write_msgfd: RawFd,
-    /* Control Channel */
+    /// Control Channel
     ctrl_fd: RawFd,
-    /* Command Channel */
-    data_ioc: RawFd,
-    chr_write_lock: Arc<Mutex<usize>>,
+    /// Data Channel
+    data_fd: RawFd,
+   // chr_write_lock: Arc<Mutex<usize>>,
 }
 
 impl SocketCharDev {
@@ -52,138 +65,124 @@ impl SocketCharDev {
             stream: None,
             write_msgfd: -1,
             ctrl_fd: -1,
-            data_ioc: -1,
-            chr_write_lock: Arc::new(Mutex::new(0))
+            data_fd: -1,
+            //chr_write_lock: Arc::new(Mutex::new(0)),
         }
     }
 
-    pub fn debugmessage(&mut self) {
-        {
-            let mut buf = (44 as u32).to_be_bytes();
-            let iov = &[IoVec::from_slice(&buf)];
-            let res = sendmsg(self.ctrl_fd, iov, &[ControlMessage::ScmRights(&[])], MsgFlags::empty(), None).expect("Error sending");
-            let mut buf2: &mut [u8] = &mut [1 as u8; 4];
-            println!("before: {:?}", buf2);
-            let (n, s) = recvfrom(self.ctrl_fd, buf2).expect("Hello");
-        }
-    }
-
-    pub fn connect(&mut self, socket_path: &str) -> isize {
+    pub fn connect(&mut self, socket_path: &str) -> Result<isize> {
         self.state = ChardevState::ChardevStateConnecting;
 
         let now = Instant::now();
 
         // Retry connecting for a full minute
-        let err = loop {
-            let err = match UnixStream::connect(socket_path) {
+        loop {
+            match UnixStream::connect(socket_path) {
                 Ok(s) => {
                     let fd = s.as_raw_fd();
                     self.ctrl_fd = fd;
                     self.stream = Some(s);
                     self.state = ChardevState::ChardevStateConnected;
-             warn!("PPK: Connected to socket path : {:?}\n", socket_path);
-
-                    return 0
+                    debug!("Connected to vTPM socket path : {:?}\n", socket_path);
+                    return Ok(0);
                 }
-                Err(e) => e,
+                Err(_e) => {}
             };
             sleep(Duration::from_millis(100));
 
             if now.elapsed().as_secs() >= 60 {
-                break err;
+                break;
             }
-        };
-        
-        // error!(
-        //     "Failed connecting the backend after trying for 1 minute: {:?}",
-        //     err
-        // );
-        -1
+        }
+        Err(TPMCharError::TPMCharCannotConnect(anyhow!(
+            "Failed to connect to vTPM socket path"
+        )))
     }
 
-    pub fn set_dataioc(&mut self, fd: RawFd) {
-        self.data_ioc = fd;
+    pub fn set_datafd(&mut self, fd: RawFd) {
+        self.data_fd = fd;
     }
 
-    pub fn set_msgfd(&mut self, fd: RawFd){
+    pub fn set_msgfd(&mut self, fd: RawFd) {
         self.write_msgfd = fd;
     }
 
-    pub fn chr_sync_read(&self, buf: &mut [u8], len: usize) -> isize {
+    pub fn chr_sync_read(&self, buf: &mut [u8], _len: usize) -> Result<usize> {
         if self.state != ChardevState::ChardevStateConnected {
-            return 0
+            return Ok(0);
         }
-        //SET BLOCKING
-        warn!("sync read state connected");
-        let size = recv(self.ctrl_fd, buf, MsgFlags::empty()).expect("char.rs: sync_read recvmsg error");
-        warn!("success recv");
-        size as isize
+        debug!("synchronous read from vTPM Socket");
+        let size = recv(self.ctrl_fd, buf, MsgFlags::empty()).map_err(|e| {
+            TPMCharError::TPMCharFailedRead(anyhow!(
+                "Failed to read from vTPM socket. Error Code {:?}",
+                e
+            ))
+        })?;
+        debug!("sync read completed");
+        Ok(size as usize)
     }
 
-    pub fn send_full(&self, buf: &mut [u8], len: usize) -> isize {
+    pub fn send_full(&self, buf: &mut [u8], _len: usize) -> Result<usize> {
         let iov = &[IoVec::from_slice(buf)];
         let write_fd = self.write_msgfd;
         let write_vec = &[write_fd];
         let cmsgs = &[ControlMessage::ScmRights(write_vec)];
-        warn!("send full message");
+        debug!("send full message");
 
-        sendmsg(self.ctrl_fd, iov, cmsgs, MsgFlags::empty(), None).expect("char.rs: ERROR ON send_full sendmsg") as isize
+        // Send Ancillary data, along with cmds and data
+        let size = sendmsg(self.ctrl_fd, iov, cmsgs, MsgFlags::empty(), None).map_err(|e| {
+            TPMCharError::TPMCharFailedWrite(anyhow!(
+                "Failed to write to vTPM Socket. Error Code {:?}",
+                e
+            ))
+        })?;
+        Ok(size as usize)
     }
 
-    pub fn chr_write(&mut self, buf: &mut [u8], len:usize) -> isize {
-        let mut res = 0;
-        warn!("CHR_WRITE HAPPENED");
+    pub fn chr_write(&mut self, buf: &mut [u8], len:usize) -> Result<usize> {
+        debug!("chr_write initialized");
 
-        if let Some(ref mut sock) = self.stream {
-            // let guard = self.chr_write_lock.lock().unwrap();
-            {
-                res = match self.state {
+        if let Some(ref mut _sock) = self.stream {
+               let res = match self.state {
                     ChardevState::ChardevStateConnected => {
                         warn!("State Connected");
-                        let ret = self.send_full(buf, len);
-                        /* free the written msgfds in any cases
-                        * other than ret < 0 */
-                        if ret >= 0 {
-                            self.write_msgfd = 0;
-                        }
-
-                        // if (ret < 0 && errno != EAGAIN) {
-                        //     if (tcp_chr_read_poll(chr) <= 0) {
-                        //         /* Perform disconnect and return error. */
-                        //         tcp_chr_disconnect_locked(chr);
-                        //     } /* else let the read handler finish it properly */
-                        // }
-
+                        let ret = self.send_full(buf, len)?;
+                        // swtpm will receive data Fd after a successful send
+                        // reset write_msgfd after a successful send
+                        self.write_msgfd = 0;
                         ret
-                    }
-                    _ => -1,
-                };
-            }
-            warn!("WRITE SUCCESS");
-            // std::mem::drop(guard);
+                    },
+                    _ => return Err(TPMCharError::TPMCharFailedWrite(anyhow!(
+                        "TPM Socket was not in Connected State"))),
+            };
+            debug!("chr_write succeeded");
 
-            res
+            Ok(res)
         } else {
-            -1
+            return Err(TPMCharError::TPMCharFailedWrite(anyhow!("Stream for TPM Socket was not initialized")))
         }
     }
 
-    pub fn chr_read(&mut self, buf: &mut [u8], len: usize) -> isize {
+    pub fn chr_read(&mut self, buf: &mut [u8], _len: usize) -> Result<usize> {
         //Grab all response bytes so none is left behind
-        warn!("CHR_READ HAPPENED");
+        debug!("chr_read initialized");
 
         let mut newbuf: &mut [u8] = &mut [0; TPM_TIS_BUFFER_MAX];
-        
+
         if let Some(ref mut sock) = self.stream {
-            sock.read(&mut newbuf);
+            let size:usize = sock.read(&mut newbuf).map_err(|e| TPMCharError::TPMCharFailedRead(anyhow!(
+                "Failed to read from vTPM Socket. Error Code {:?}",
+                e
+            )))?;
             byte_copy(&newbuf, buf);
-            0
+            Ok(size)
         } else {
-            -1
+            return Err(TPMCharError::TPMCharFailedRead(anyhow!("Stream for TPM Socket was not initialized")))
         }
     }
 }
-
+/// This is the backend seen by frontend
+/// Actual Backend is SocketCharDev
 pub struct CharBackend {
     pub chr: Option<SocketCharDev>,
     fe_open: bool,
@@ -197,39 +196,35 @@ impl CharBackend {
         }
     }
 
-    pub fn chr_fe_init(&mut self) -> isize {
+    pub fn chr_be_init(&mut self, path: String) -> Result<()> {
         let mut sockdev = SocketCharDev::new();
-        warn!("PPK: chr_fe_init invoked\n");
-        let res = sockdev.connect("/tmp/swtpm/swtpm-sock");
+        let _ = sockdev.connect(&path)?;
+
         self.chr = Some(sockdev);
-        if res < 0 {
-            return -1
-        }
-        
         self.fe_open = true;
-        0
+        Ok(())
     }
 
-    pub fn chr_fe_set_msgfd(&mut self, fd: RawFd) -> isize {
+    pub fn chr_be_set_msgfd(&mut self, fd: RawFd) -> Result<()> {
         if let Some(ref mut dev) = self.chr {
             dev.set_msgfd(fd);
-            0
+            Ok(())
         } else {
-            -1
+            return Err(TPMCharError::CharBackendConfigureFailed(anyhow!("SocketCharDev was not initialized")))
         }
     }
 
-    pub fn chr_fe_set_dataioc(&mut self, fd: RawFd) -> isize {
+    pub fn chr_be_set_datafd(&mut self, fd: RawFd) -> Result<isize> {
         if let Some(ref mut dev) = self.chr {
-            dev.set_dataioc(fd);
-            0
+            dev.set_datafd(fd);
+            Ok(0)
         } else {
-            -1
+            return Err(TPMCharError::CharBackendConfigureFailed(anyhow!("SocketCharDev was not initialized")))
         }
     }
 
     /**
-     * qemu_chr_fe_write_all:
+     * chr_be_write_all:
      * @buf: the data
      * @len: the number of bytes to send
      *
@@ -240,17 +235,16 @@ impl CharBackend {
      *
      * Returns: the number of bytes consumed (0 if no associated Chardev)
      */
-    pub fn chr_fe_write_all(&mut self, buf: &mut [u8], len: usize) -> isize {
+    pub fn chr_be_write_all(&mut self, buf: &mut [u8], len: usize) -> Result<usize> {
         if let Some(ref mut dev) = self.chr {
             dev.chr_write(buf, len)
         } else {
-            -1
+            return Err(TPMCharError::CharBackendConfigureFailed(anyhow!("SocketCharDev was not initialized")))
         }
     }
 
-
     /**
-     * chr_fe_read_all:
+     * chr_be_read_all:
      * @buf: the data buffer
      * @len: the number of bytes to read
      *
@@ -258,20 +252,16 @@ impl CharBackend {
      *
      * Returns: the number of bytes read (0 if no associated Chardev)
      */
-    pub fn chr_fe_read_all(&mut self, mut buf: &mut [u8], len: usize) -> isize {
+    pub fn chr_be_read_all(&mut self, mut buf: &mut [u8]) -> Result<usize> {
         if let Some(ref mut dev) = self.chr {
-            warn!("Made it to sync");
-            let (s,_) = recvfrom(dev.ctrl_fd, &mut buf).expect("char.rs: sync_read recvmsg error");
-            s as isize
-            // dev.chr_sync_read(&mut buf, len)
+            let (s, _) = recvfrom(dev.ctrl_fd, &mut buf).map_err(|e| TPMCharError::TPMCharFailedRead(anyhow!(
+                "Failed to read from vTPM Socket. Error Code {:?}",
+                e
+            )))?;
+            //expect("char.rs: sync_read recvmsg error");
+            Ok(s)
         } else {
-            -1
+            return Err(TPMCharError::CharBackendConfigureFailed(anyhow!("SocketCharDev was not initialized")))
         }
     }
-
-
-}
-
-pub enum Error {
-    BindSocket()
 }
---
+++ vtpm/src/lib.rs
@@ -0,0 +1,6 @@
+#[macro_use]
+extern crate log;
+
+//pub mod tpm_backend;
+pub mod char;
+//pub mod tpm_ioctl;
