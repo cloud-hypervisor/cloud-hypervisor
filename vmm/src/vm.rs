3edaa8adb69056b4480274bc3d5ee25d997d6dcc
Sebastien Boeuf <sebastien.boeuf@intel.com>
2022-06-02 18:02:36 +0200

    vmm: Ensure restore matches boot sequence
    The vCPU is created and set after all the devices on a VM's boot.
    There's no reason to follow a different order on the restore codepath as
    this could cause some unexpected behaviors.
    
    Signed-off-by: Sebastien Boeuf <sebastien.boeuf@intel.com>

--- vmm/src/vm.rs
+++ vmm/src/vm.rs
@@ -2753,17 +2753,6 @@ impl Snapshottable for Vm {
             )));
         }
 
-        if let Some(cpu_manager_snapshot) = snapshot.snapshots.get(CPU_MANAGER_SNAPSHOT_ID) {
-            self.cpu_manager
-                .lock()
-                .unwrap()
-                .restore(*cpu_manager_snapshot.clone())?;
-        } else {
-            return Err(MigratableError::Restore(anyhow!(
-                "Missing CPU manager snapshot"
-            )));
-        }
-
         if let Some(device_manager_snapshot) = snapshot.snapshots.get(DEVICE_MANAGER_SNAPSHOT_ID) {
             self.device_manager
                 .lock()
@@ -2772,6 +2761,17 @@ impl Snapshottable for Vm {
         } else {
             return Err(MigratableError::Restore(anyhow!(
                 "Missing device manager snapshot"
+            )));
+        }
+
+        if let Some(cpu_manager_snapshot) = snapshot.snapshots.get(CPU_MANAGER_SNAPSHOT_ID) {
+            self.cpu_manager
+                .lock()
+                .unwrap()
+                .restore(*cpu_manager_snapshot.clone())?;
+        } else {
+            return Err(MigratableError::Restore(anyhow!(
+                "Missing CPU manager snapshot"
             )));
         }
 
