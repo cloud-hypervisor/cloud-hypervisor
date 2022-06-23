b00f987c3c8bf022107aacbd1c09bdb5a1475f2f
Dev Rajput <t-devrajput@microsoft.com>
2022-06-23 16:59:15 +0000

    hypervisor: Add default implementation for set_immediate_exit
    Signed-off-by: Dev Rajput <t-devrajput@microsoft.com>

--- hypervisor/src/cpu.rs
+++ hypervisor/src/cpu.rs
@@ -474,11 +474,12 @@ pub trait Vcpu: Send + Sync {
     fn get_suspend_regs(&self) -> Result<SuspendRegisters> {
         Result::Err(HypervisorCpuError::GetSuspendRegs(anyhow!("Not implemented")))
     }
-    #[cfg(feature = "kvm")]
     ///
     /// Set the "immediate_exit" state
     ///
-    fn set_immediate_exit(&self, exit: bool);
+    fn set_immediate_exit(&self, _exit: bool) {
+        error!("Not implemented");
+    }
     #[cfg(feature = "tdx")]
     ///
     /// Returns the details about TDX exit reason
