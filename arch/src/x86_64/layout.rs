d12534f0b8f000c5ff9005d2429fa0ca18e19d83
Praveen K Paladugu <prapal@microsoft.com>
2022-08-15 15:33:41 +0000

    arch: Add vTPM Address range to x86_64 layout
    The address range added in this commit is specific to CRB Interface.
    
    Signed-off-by: Praveen K Paladugu <prapal@linux.microsoft.com>
    Co-authored-by: Sean Yoo <t-seanyoo@microsoft.com>

--- arch/src/x86_64/layout.rs
+++ arch/src/x86_64/layout.rs
@@ -107,6 +107,10 @@ pub const KVM_TSS_SIZE: u64 = (3 * 4) << 10;
 pub const KVM_IDENTITY_MAP_START: GuestAddress = GuestAddress(KVM_TSS_START.0 + KVM_TSS_SIZE);
 pub const KVM_IDENTITY_MAP_SIZE: u64 = 4 << 10;
 
+/// VTPM Address Space Range
+pub const VTPM_START: GuestAddress = GuestAddress(0xfed4_0000);
+pub const VTPM_SIZE: u64 = 0x1000;
+
 // IOAPIC
 pub const IOAPIC_START: GuestAddress = GuestAddress(0xfec0_0000);
 pub const IOAPIC_SIZE: u64 = 0x20;
