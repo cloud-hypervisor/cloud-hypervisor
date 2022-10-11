5fd79571b7f017e45f141fea009f47152282c202
Jinank Jain <jinankjain@microsoft.com>
2022-10-11 09:58:00 +0000

    vmm: Add a feature flag for SEV-SNP support
    This feature flag gates the development for SEV-SNP enabled guest.
    
    Also add a helper function to identify if SNP should be enabled for the
    guest.
    
    Signed-off-by: Jinank Jain <jinankjain@microsoft.com>

--- vmm/src/vm_config.rs
+++ vmm/src/vm_config.rs
@@ -89,6 +89,9 @@ pub struct PlatformConfig {
     #[cfg(feature = "tdx")]
     #[serde(default)]
     pub tdx: bool,
+    #[cfg(feature = "sev_snp")]
+    #[serde(default)]
+    pub sev_snp: bool,
 }
 
 impl Default for PlatformConfig {
@@ -101,6 +104,8 @@ impl Default for PlatformConfig {
             oem_strings: None,
             #[cfg(feature = "tdx")]
             tdx: false,
+            #[cfg(feature = "sev_snp")]
+            sev_snp: false,
         }
     }
 }
