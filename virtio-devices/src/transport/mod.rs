a50b3784fe9bbd4eeb231e4524b93c00672e5179
Sebastien Boeuf <sebastien.boeuf@intel.com>
2022-11-23 15:01:16 +0100

    virtio-devices: Create a proper result type for VirtioPciDevice
    Creating a dedicated Result type for VirtioPciDevice, associated with
    the new VirtioPciDeviceError enum. This allows for a clearer handling of
    the errors generated through VirtioPciDevice::new().
    
    Signed-off-by: Sebastien Boeuf <sebastien.boeuf@intel.com>

--- virtio-devices/src/transport/mod.rs
+++ virtio-devices/src/transport/mod.rs
@@ -6,7 +6,7 @@ use vmm_sys_util::eventfd::EventFd;
 mod pci_common_config;
 mod pci_device;
 pub use pci_common_config::{VirtioPciCommonConfig, VIRTIO_PCI_COMMON_CONFIG_ID};
-pub use pci_device::{VirtioPciDevice, VirtioPciDeviceActivator};
+pub use pci_device::{VirtioPciDevice, VirtioPciDeviceActivator, VirtioPciDeviceError};
 
 pub trait VirtioTransport {
     fn ioeventfds(&self, base_addr: u64) -> Vec<(&EventFd, u64)>;
