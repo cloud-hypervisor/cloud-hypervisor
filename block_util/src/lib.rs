e2ea9e2821ef5166b125c7a7989229e4725dedfb
Yong He <alexyonghe@tencent.com>
2022-12-27 15:43:07 +0800

    block_util: add timestamp in request
    Add timestamp per request, so that we could account
    block process latency base request.
    
    Signed-off-by: Yong He <alexyonghe@tencent.com>

--- block_util/src/lib.rs
+++ block_util/src/lib.rs
@@ -34,6 +34,7 @@ use std::path::Path;
 use std::result;
 use std::sync::Arc;
 use std::sync::MutexGuard;
+use std::time::Instant;
 use thiserror::Error;
 use versionize::{VersionMap, Versionize, VersionizeResult};
 use versionize_derive::Versionize;
@@ -201,6 +202,7 @@ pub struct Request {
     pub status_addr: GuestAddress,
     pub writeback: bool,
     pub aligned_operations: Vec<AlignedOperation>,
+    pub start: Instant,
 }
 
 impl Request {
@@ -232,6 +234,7 @@ impl Request {
             status_addr: GuestAddress(0),
             writeback: true,
             aligned_operations: Vec::new(),
+            start: Instant::now(),
         };
 
         let status_desc;
