a08aa85db7b7ed6519e71ede3aad55d49e7de552
Rob Bradford <robert.bradford@intel.com>
2023-01-13 10:52:33 +0000

    Revert "build: drop clap from dev-dependencies"
    This reverts commit 998fb48ff276821f3b3ced6d07d492130eef7fec.
    
    This is breaking the release build process (cross build for aarch64
    musl) so temporarily reverting until we can identify the cause to allow
    the release proceeed.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- performance-metrics/build.rs
+++ performance-metrics/build.rs
@@ -3,11 +3,13 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#[macro_use(crate_version)]
+extern crate clap;
+
 use std::process::Command;
 
 fn main() {
-    let mut git_human_readable = "v".to_owned() + env!("CARGO_PKG_VERSION");
-
+    let mut git_human_readable = "v".to_owned() + crate_version!();
     if let Ok(git_out) = Command::new("git").args(["describe", "--dirty"]).output() {
         if git_out.status.success() {
             if let Ok(git_out_str) = String::from_utf8(git_out.stdout) {
