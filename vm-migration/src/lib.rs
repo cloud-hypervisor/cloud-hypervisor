3b0d2e796b0810ba24c86aad6ada44381f161138
Bo Chen <chen.bo@intel.com>
2023-01-24 09:57:40 -0800

    build: Release v28.2 (bug fix release)
    Signed-off-by: Bo Chen <chen.bo@intel.com>

--- vm-migration/src/lib.rs
+++ vm-migration/src/lib.rs
@@ -13,7 +13,7 @@ pub mod protocol;
 
 /// Global VMM version for versioning
 const MAJOR_VERSION: u16 = 28;
-const MINOR_VERSION: u16 = 1;
+const MINOR_VERSION: u16 = 2;
 const VMM_VERSION: u16 = MAJOR_VERSION << 12 | MINOR_VERSION & 0b1111;
 
 pub trait VersionMapped {
