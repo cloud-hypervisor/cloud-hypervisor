934b20a77ab78ee901664eee9eea8ff77edf8c67
Anatol Belski <anbelski@linux.microsoft.com>
2023-02-10 16:51:42 +0100

    ci: Switch to Windows Server 2022
    The updated image is configured in a same way as the previously used
    2019, it has same
    
    - Credentials
    - Services configured, like SAC, SSH, RDP
    - Size
    
    All the Windows updates are applied so the state is current to the date.
    Also, the latest stable version 0.1.229 of the VirtIO Windows drivers
    is installed.
    
    Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>

--- tests/integration.rs
+++ tests/integration.rs
@@ -38,7 +38,7 @@ mod x86_64 {
     pub const FOCAL_IMAGE_NAME_VHD: &str = "focal-server-cloudimg-amd64-custom-20210609-0.vhd";
     pub const FOCAL_IMAGE_NAME_VHDX: &str = "focal-server-cloudimg-amd64-custom-20210609-0.vhdx";
     pub const JAMMY_IMAGE_NAME: &str = "jammy-server-cloudimg-amd64-custom-20230119-0.raw";
-    pub const WINDOWS_IMAGE_NAME: &str = "windows-server-2019.raw";
+    pub const WINDOWS_IMAGE_NAME: &str = "windows-server-2022-amd64-2.raw";
     pub const OVMF_NAME: &str = "CLOUDHV.fd";
     pub const GREP_SERIAL_IRQ_CMD: &str = "grep -c 'IO-APIC.*ttyS0' /proc/interrupts || true";
 }
