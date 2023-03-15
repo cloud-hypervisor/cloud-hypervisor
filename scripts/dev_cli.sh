ca96ff601b094b52045c1523a32a70bf09d9045e
Rob Bradford <rbradford@rivosinc.com>
2023-03-15 17:27:51 +0000

    build: Migrate from DockerHub to ghcr
    It is necessary to transition away from DockerHub as the small
    organisation plan in being EOLed.
    
    Signed-off-by: Rob Bradford <rbradford@rivosinc.com>

--- scripts/dev_cli.sh
+++ scripts/dev_cli.sh
@@ -6,8 +6,8 @@
 
 CLI_NAME="Cloud Hypervisor"
 
-CTR_IMAGE_TAG="cloudhypervisor/dev"
-CTR_IMAGE_VERSION="20230301-0"
+CTR_IMAGE_TAG="ghcr.io/cloud-hypervisor/cloud-hypervisor"
+CTR_IMAGE_VERSION="20230315-0"
 CTR_IMAGE="${CTR_IMAGE_TAG}:${CTR_IMAGE_VERSION}"
 
 DOCKER_RUNTIME="docker"
