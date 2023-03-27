3afa8b6a8c5fe6224ea9ce0942114b2394cb120b
Anatol Belski <anbelski@linux.microsoft.com>
2023-03-27 17:44:01 +0200

    seccomp: Add filter entry for MSHV_VP_REGISTER_INTERCEPT_RESULT
    Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>

--- vmm/src/seccomp_filters.rs
+++ vmm/src/seccomp_filters.rs
@@ -168,6 +168,7 @@ mod mshv {
     pub const MSHV_GET_GPA_ACCESS_STATES: u64 = 0xc01c_b812;
     pub const MSHV_VP_TRANSLATE_GVA: u64 = 0xc020_b80e;
     pub const MSHV_CREATE_PARTITION: u64 = 0x4030_b801;
+    pub const MSHV_VP_REGISTER_INTERCEPT_RESULT: u64 = 0x4030_b817;
 }
 #[cfg(feature = "mshv")]
 use mshv::*;
@@ -197,6 +198,12 @@ fn create_vmm_ioctl_seccomp_rule_common_mshv() -> Result<Vec<SeccompRule>, Backe
         and![Cond::new(1, ArgLen::Dword, Eq, MSHV_GET_GPA_ACCESS_STATES)?],
         and![Cond::new(1, ArgLen::Dword, Eq, MSHV_VP_TRANSLATE_GVA)?],
         and![Cond::new(1, ArgLen::Dword, Eq, MSHV_CREATE_PARTITION)?],
+        and![Cond::new(
+            1,
+            ArgLen::Dword,
+            Eq,
+            MSHV_VP_REGISTER_INTERCEPT_RESULT
+        )?],
     ])
 }
 
