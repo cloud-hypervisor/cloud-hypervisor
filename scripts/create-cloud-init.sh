839ee0aff1836e50da9baabf109cf2d1cefbaeaa
Ravi kumar Veeramally <ravikumar.veeramally@intel.com>
2023-06-13 23:42:19 +0300

    test_data: Simplify default configuration for local testing
    Script `create-cloud-init.sh" uses the same cloud init data as the the CI
    but this means it is somewhat overloaded with unhelpful network configuration
    entries and scripts that are only needed for the CI.
    
    For local testing data kept in test_data/cloud-init/ubuntu/local folder.
    This contains minimum configuration data for user to test it locally.
    Script assigns default IP address using "network-config" details
    with --net "tap=" option. The default network interface will be "ens4".
    
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@intel.com>

--- scripts/create-cloud-init.sh
+++ scripts/create-cloud-init.sh
@@ -3,7 +3,7 @@ set -x
 
 rm -f /tmp/ubuntu-cloudinit.img
 mkdosfs -n CIDATA -C /tmp/ubuntu-cloudinit.img 8192
-mcopy -oi /tmp/ubuntu-cloudinit.img -s test_data/cloud-init/ubuntu/user-data ::
-mcopy -oi /tmp/ubuntu-cloudinit.img -s test_data/cloud-init/ubuntu/meta-data ::
-mcopy -oi /tmp/ubuntu-cloudinit.img -s test_data/cloud-init/ubuntu/network-config ::
+mcopy -oi /tmp/ubuntu-cloudinit.img -s test_data/cloud-init/ubuntu/local/user-data ::
+mcopy -oi /tmp/ubuntu-cloudinit.img -s test_data/cloud-init/ubuntu/local/meta-data ::
+mcopy -oi /tmp/ubuntu-cloudinit.img -s test_data/cloud-init/ubuntu/local/network-config ::
 
