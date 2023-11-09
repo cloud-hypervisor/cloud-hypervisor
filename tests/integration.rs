ca9e20c27acc0b6ec595409f716e5df5b2f386e7
Bo Chen <chen.bo@intel.com>
2023-11-08 20:56:58 -0800

    tests: Temporarily disable vhost_user_blk tests on aarch64
    See: #5934
    
    Signed-off-by: Bo Chen <chen.bo@intel.com>
    (cherry picked from commit 62db13ba0ed7c7864c7959edcc26f224305c870d)

--- tests/integration.rs
+++ tests/integration.rs
@@ -3297,16 +3297,19 @@ mod common_parallel {
     }
 
     #[test]
+    #[cfg(not(target_arch = "aarch64"))]
     fn test_vhost_user_blk_default() {
         test_vhost_user_blk(2, false, false, Some(&prepare_vubd))
     }
 
     #[test]
+    #[cfg(not(target_arch = "aarch64"))]
     fn test_vhost_user_blk_readonly() {
         test_vhost_user_blk(1, true, false, Some(&prepare_vubd))
     }
 
     #[test]
+    #[cfg(not(target_arch = "aarch64"))]
     fn test_vhost_user_blk_direct() {
         test_vhost_user_blk(1, false, true, Some(&prepare_vubd))
     }
