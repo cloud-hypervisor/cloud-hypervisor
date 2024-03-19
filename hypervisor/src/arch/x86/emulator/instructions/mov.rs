c721c0d88fa877aee4de60c79fafd8bd29f50b4d
Rob Bradford <rbradford@rivosinc.com>
2024-03-19 17:17:37 +0000

    hypervisor: emulator: Remove unnecessary #![allow(unused_mut)]
    clippy was flagging this up as a mixture of mixed attributes but that
    attribute is no longer necessary
    
    warning: item has both inner and outer attributes
       --> hypervisor/src/arch/x86/emulator/mod.rs:769:1
        |
    769 | / #[cfg(test)]
    770 | | mod tests {
    771 | |     #![allow(unused_mut)]
        | |_________________________^
        |
        = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#mixed_attributes_style
        = note: `#[warn(clippy::mixed_attributes_style)]` on by default
    
    Signed-off-by: Rob Bradford <rbradford@rivosinc.com>

--- hypervisor/src/arch/x86/emulator/instructions/mov.rs
+++ hypervisor/src/arch/x86/emulator/instructions/mov.rs
@@ -269,7 +269,6 @@ impl<T: CpuStateManager> InstructionHandler<T> for Mov_RAX_moffs64 {
 
 #[cfg(test)]
 mod tests {
-    #![allow(unused_mut)]
     use super::*;
     use crate::arch::x86::emulator::mock_vmm::*;
 
