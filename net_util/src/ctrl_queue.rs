87adb5946c8ed36f9fc79f04eecdc65f2085a4f3
KshitijKapoor8 <kshitijkapoor8@gmail.com>
2024-04-13 16:38:43 -0500

    net_util: Changed order of thiserror import
    Changed order of imports in package to match coding style
    
    Signed-off-by: Kshitij Kapoor <kshitijkapoor8@gmail.com>

--- net_util/src/ctrl_queue.rs
+++ net_util/src/ctrl_queue.rs
@@ -6,6 +6,7 @@ use crate::GuestMemoryMmap;
 use crate::Tap;
 use libc::c_uint;
 use std::sync::Arc;
+use thiserror::Error;
 use virtio_bindings::virtio_net::{
     VIRTIO_NET_CTRL_GUEST_OFFLOADS, VIRTIO_NET_CTRL_GUEST_OFFLOADS_SET, VIRTIO_NET_CTRL_MQ,
     VIRTIO_NET_CTRL_MQ_VQ_PAIRS_MAX, VIRTIO_NET_CTRL_MQ_VQ_PAIRS_MIN,
@@ -16,7 +17,6 @@ use virtio_bindings::virtio_net::{
 use virtio_queue::{Queue, QueueT};
 use vm_memory::{ByteValued, Bytes, GuestMemoryError};
 use vm_virtio::{AccessPlatform, Translatable};
-use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum Error {
