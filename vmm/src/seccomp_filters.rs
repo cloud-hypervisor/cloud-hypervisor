2fea515f5e5468ce786218cedaa20e4c36f69c48
Nuno Das Neves <nudasnev@microsoft.com>
2024-04-30 18:39:21 +0000

    hypervisor: mshv: Update mshv crates to v0.3.0
    And modify to code to use the updated interfaces.
    
    Arguments for map_guest_memory, get_dirty_bitmap, vp.run(),
    import_isolated_pages, modify_gpa_host_access have changed.
    Update these to use the new interfaces, including new MSHV_*
    definitions, and remove some redundant arguments.
    
    Update seccomp IOCTLs to reflect interface changes.
    
    Fix irq-related definitions naming.
    
    Bump vfio-ioctls to support mshv v0.3.0.
    
    Signed-off-by: Nuno Das Neves <nudasnev@microsoft.com>

--- vmm/src/seccomp_filters.rs
+++ vmm/src/seccomp_filters.rs
@@ -159,8 +159,13 @@ use hypervisor::mshv::mshv_ioctls::*;
 fn create_vmm_ioctl_seccomp_rule_common_mshv() -> Result<Vec<SeccompRule>, BackendError> {
     Ok(or![
         and![Cond::new(1, ArgLen::Dword, Eq, MSHV_CREATE_PARTITION())?],
-        and![Cond::new(1, ArgLen::Dword, Eq, MSHV_MAP_GUEST_MEMORY())?],
-        and![Cond::new(1, ArgLen::Dword, Eq, MSHV_UNMAP_GUEST_MEMORY())?],
+        and![Cond::new(
+            1,
+            ArgLen::Dword,
+            Eq,
+            MSHV_INITIALIZE_PARTITION()
+        )?],
+        and![Cond::new(1, ArgLen::Dword, Eq, MSHV_SET_GUEST_MEMORY())?],
         and![Cond::new(1, ArgLen::Dword, Eq, MSHV_CREATE_VP())?],
         and![Cond::new(1, ArgLen::Dword, Eq, MSHV_IRQFD())?],
         and![Cond::new(1, ArgLen::Dword, Eq, MSHV_IOEVENTFD())?],
@@ -186,7 +191,7 @@ fn create_vmm_ioctl_seccomp_rule_common_mshv() -> Result<Vec<SeccompRule>, Backe
             1,
             ArgLen::Dword,
             Eq,
-            MSHV_GET_GPA_ACCESS_STATES()
+            MSHV_GET_GPAP_ACCESS_BITMAP()
         )?],
         and![Cond::new(1, ArgLen::Dword, Eq, MSHV_VP_TRANSLATE_GVA())?],
         and![Cond::new(
@@ -700,8 +705,7 @@ fn create_vcpu_ioctl_seccomp_rule_mshv() -> Result<Vec<SeccompRule>, BackendErro
         and![Cond::new(1, ArgLen::Dword, Eq, MSHV_RUN_VP())?],
         and![Cond::new(1, ArgLen::Dword, Eq, MSHV_GET_VP_REGISTERS())?],
         and![Cond::new(1, ArgLen::Dword, Eq, MSHV_SET_VP_REGISTERS())?],
-        and![Cond::new(1, ArgLen::Dword, Eq, MSHV_MAP_GUEST_MEMORY())?],
-        and![Cond::new(1, ArgLen::Dword, Eq, MSHV_UNMAP_GUEST_MEMORY())?],
+        and![Cond::new(1, ArgLen::Dword, Eq, MSHV_SET_GUEST_MEMORY())?],
         and![Cond::new(1, ArgLen::Dword, Eq, MSHV_VP_TRANSLATE_GVA())?],
         and![Cond::new(1, ArgLen::Dword, Eq, MSHV_GET_VP_CPUID_VALUES())?],
         and![Cond::new(
