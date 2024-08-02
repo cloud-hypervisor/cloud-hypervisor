996fbfe0657c3cb2cd409f129fb3df2bd7e52664
Jinank Jain <jinankjain@microsoft.com>
2024-08-02 14:22:48 +0530

    hypervisor: Mark all the field of StandardRegisters as private
    Along with it also create setter/getter functions to access these field.
    This will help us in future once we replace StandardRegisters with enum
    of bindings based StandardRegisters.
    
    Signed-off-by: Jinank Jain <jinankjain@microsoft.com>

--- hypervisor/src/kvm/x86_64/mod.rs
+++ hypervisor/src/kvm/x86_64/mod.rs
@@ -81,50 +81,51 @@ pub struct VcpuKvmState {
 impl From<StandardRegisters> for kvm_regs {
     fn from(regs: StandardRegisters) -> Self {
         Self {
-            rax: regs.rax,
-            rbx: regs.rbx,
-            rcx: regs.rcx,
-            rdx: regs.rdx,
-            rsi: regs.rsi,
-            rdi: regs.rdi,
-            rsp: regs.rsp,
-            rbp: regs.rbp,
-            r8: regs.r8,
-            r9: regs.r9,
-            r10: regs.r10,
-            r11: regs.r11,
-            r12: regs.r12,
-            r13: regs.r13,
-            r14: regs.r14,
-            r15: regs.r15,
-            rip: regs.rip,
-            rflags: regs.rflags,
+            rax: regs.get_rax(),
+            rbx: regs.get_rbx(),
+            rcx: regs.get_rcx(),
+            rdx: regs.get_rdx(),
+            rsi: regs.get_rsi(),
+            rdi: regs.get_rdi(),
+            rsp: regs.get_rsp(),
+            rbp: regs.get_rbp(),
+            r8: regs.get_r8(),
+            r9: regs.get_r9(),
+            r10: regs.get_r10(),
+            r11: regs.get_r11(),
+            r12: regs.get_r12(),
+            r13: regs.get_r13(),
+            r14: regs.get_r14(),
+            r15: regs.get_r15(),
+            rip: regs.get_rip(),
+            rflags: regs.get_rflags(),
         }
     }
 }
 
 impl From<kvm_regs> for StandardRegisters {
     fn from(regs: kvm_regs) -> Self {
-        Self {
-            rax: regs.rax,
-            rbx: regs.rbx,
-            rcx: regs.rcx,
-            rdx: regs.rdx,
-            rsi: regs.rsi,
-            rdi: regs.rdi,
-            rsp: regs.rsp,
-            rbp: regs.rbp,
-            r8: regs.r8,
-            r9: regs.r9,
-            r10: regs.r10,
-            r11: regs.r11,
-            r12: regs.r12,
-            r13: regs.r13,
-            r14: regs.r14,
-            r15: regs.r15,
-            rip: regs.rip,
-            rflags: regs.rflags,
-        }
+        let mut sregs = StandardRegisters::default();
+        sregs.set_rax(regs.rax);
+        sregs.set_rbx(regs.rbx);
+        sregs.set_rcx(regs.rcx);
+        sregs.set_rdx(regs.rdx);
+        sregs.set_rsi(regs.rsi);
+        sregs.set_rdi(regs.rdi);
+        sregs.set_rsp(regs.rsp);
+        sregs.set_rbp(regs.rbp);
+        sregs.set_r8(regs.r8);
+        sregs.set_r9(regs.r9);
+        sregs.set_r10(regs.r10);
+        sregs.set_r11(regs.r11);
+        sregs.set_r12(regs.r12);
+        sregs.set_r13(regs.r13);
+        sregs.set_r14(regs.r14);
+        sregs.set_r15(regs.r15);
+        sregs.set_rip(regs.rip);
+        sregs.set_rflags(regs.rflags);
+
+        sregs
     }
 }
 
