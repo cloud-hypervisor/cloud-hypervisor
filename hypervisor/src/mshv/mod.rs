7dd69cc55891248e01ee9fe3bd96b95458a8ed6d
Muminul Islam <muislam@microsoft.com>
2024-09-05 16:16:02 -0700

    hypervisor: maintain a bitmap for SEV-SNP VM on MSHV
    Add a bitmap to MshvVM struct for caching the pages
    that the VMM got shared access from the guest.
    
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

--- hypervisor/src/mshv/mod.rs
+++ hypervisor/src/mshv/mod.rs
@@ -5,12 +5,16 @@
 
 use std::any::Any;
 use std::collections::HashMap;
+#[cfg(feature = "sev_snp")]
+use std::num::NonZeroUsize;
 use std::sync::{Arc, RwLock};
 
 use mshv_bindings::*;
 use mshv_ioctls::{set_registers_64, InterruptRequest, Mshv, NoDatamatch, VcpuFd, VmFd, VmType};
 use vfio_ioctls::VfioDeviceFd;
 use vm::DataMatch;
+#[cfg(feature = "sev_snp")]
+use vm_memory::bitmap::AtomicBitmap;
 
 use crate::arch::emulator::PlatformEmulator;
 #[cfg(target_arch = "x86_64")]
@@ -55,6 +59,8 @@ use crate::{
 };
 
 pub const PAGE_SHIFT: usize = 12;
+#[cfg(feature = "sev_snp")]
+const ONE_GB: usize = 1024 * 1024 * 1024;
 
 impl From<mshv_user_mem_region> for UserMemoryRegion {
     fn from(region: mshv_user_mem_region) -> Self {
@@ -297,12 +303,27 @@ impl MshvHypervisor {
                 msrs[pos].index = *index;
             }
 
+            #[cfg(feature = "sev_snp")]
+            let mem_size = _mem_size.unwrap_or_default();
+
+            #[cfg(feature = "sev_snp")]
+            let mem_size_for_bitmap = if mem_size as usize > 3 * ONE_GB {
+                mem_size as usize + ONE_GB
+            } else {
+                mem_size as usize
+            };
+
             Ok(Arc::new(MshvVm {
                 fd: vm_fd,
                 msrs,
                 dirty_log_slots: Arc::new(RwLock::new(HashMap::new())),
                 #[cfg(feature = "sev_snp")]
                 sev_snp_enabled: mshv_vm_type == VmType::Snp,
+                #[cfg(feature = "sev_snp")]
+                host_access_pages: Arc::new(AtomicBitmap::new(
+                    mem_size_for_bitmap,
+                    NonZeroUsize::new(HV_PAGE_SIZE).unwrap(),
+                )),
             }))
         }
 
@@ -441,6 +462,8 @@ pub struct MshvVcpu {
     msrs: Vec<MsrEntry>,
     vm_ops: Option<Arc<dyn vm::VmOps>>,
     vm_fd: Arc<VmFd>,
+    #[cfg(feature = "sev_snp")]
+    host_access_pages: Arc<AtomicBitmap>,
 }
 
 /// Implementation of Vcpu trait for Microsoft Hypervisor
@@ -768,6 +791,9 @@ impl cpu::Vcpu for MshvVcpu {
                         .map_err(|e| cpu::HypervisorCpuError::RunVcpu(anyhow!(
                             "Unhandled VCPU exit: attribute intercept - couldn't modify host access {}", e
                         )))?;
+                    // Guest is revoking the shared access, so we need to update the bitmap
+                    self.host_access_pages
+                        .reset_addr_range(gpa_start as usize, gfn_count as usize);
                     Ok(cpu::VmExit::Ignore)
                 }
                 #[cfg(target_arch = "x86_64")]
@@ -1594,6 +1620,8 @@ pub struct MshvVm {
     dirty_log_slots: Arc<RwLock<HashMap<u64, MshvDirtyLogSlot>>>,
     #[cfg(feature = "sev_snp")]
     sev_snp_enabled: bool,
+    #[cfg(feature = "sev_snp")]
+    host_access_pages: Arc<AtomicBitmap>,
 }
 
 impl MshvVm {
@@ -1694,6 +1722,8 @@ impl vm::Vm for MshvVm {
             msrs: self.msrs.clone(),
             vm_ops,
             vm_fd: self.fd.clone(),
+            #[cfg(feature = "sev_snp")]
+            host_access_pages: self.host_access_pages.clone(),
         };
         Ok(Arc::new(vcpu))
     }
@@ -2094,7 +2124,10 @@ impl vm::Vm for MshvVm {
         let start_gpfn: u64 = gpa >> PAGE_SHIFT;
         let end_gpfn: u64 = (gpa + size as u64 - 1) >> PAGE_SHIFT;
 
-        let gpas: Vec<u64> = (start_gpfn..=end_gpfn).map(|x| x << PAGE_SHIFT).collect();
+        let gpas: Vec<u64> = (start_gpfn..=end_gpfn)
+            .filter(|x| !self.host_access_pages.is_bit_set(*x as usize))
+            .map(|x| x << PAGE_SHIFT)
+            .collect();
 
         if !gpas.is_empty() {
             let mut gpa_list = vec_with_array_field::<mshv_modify_gpa_host_access, u64>(gpas.len());
@@ -2117,6 +2150,11 @@ impl vm::Vm for MshvVm {
             self.fd
                 .modify_gpa_host_access(&gpa_list[0])
                 .map_err(|e| vm::HypervisorVmError::ModifyGpaHostAccess(e.into()))?;
+
+            for acquired_gpa in gpas {
+                self.host_access_pages
+                    .set_bit((acquired_gpa >> PAGE_SHIFT) as usize);
+            }
         }
 
         Ok(())
