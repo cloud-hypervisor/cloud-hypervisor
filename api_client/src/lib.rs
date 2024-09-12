cloud-hypervisor/cloud-hypervisor

commit 5d0d56f50ba2b69a6d00379d446792e063da9a4f

api_client: Use thiserror for errors
Added thiserror crate for Error enums to the api_client package

Signed-off-by: SamrutGadde <samrut.gadde@gmail.com>

api_client/src/lib.rs
@@ -3,39 +3,25 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-use std::fmt;
 use std::io::{Read, Write};
 use std::os::unix::io::RawFd;
+use thiserror::Error;
 use vmm_sys_util::sock_ctrl_msg::ScmSocket;
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum Error {
+    #[error("Error writing to or reading from HTTP socket: {0}")]
     Socket(std::io::Error),
+    #[error("Error writing to or reading from HTTP socket: {0}")]
     SocketSendFds(vmm_sys_util::errno::Error),
+    #[error("Error parsing HTTP status code: {0}")]
     StatusCodeParsing(std::num::ParseIntError),
+    #[error("HTTP output is missing protocol statement")]
     MissingProtocol,
+    #[error("Error parsing HTTP Content-Length field: {0}")]
     ContentLengthParsing(std::num::ParseIntError),
+    #[error("Server responded with an error: {0:?}")]
     ServerResponse(StatusCode, Option<String>),
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        use Error::*;
-        match self {
-            Socket(e) => write!(f, "Error writing to or reading from HTTP socket: {e}"),
-            SocketSendFds(e) => write!(f, "Error writing to or reading from HTTP socket: {e}"),
-            StatusCodeParsing(e) => write!(f, "Error parsing HTTP status code: {e}"),
-            MissingProtocol => write!(f, "HTTP output is missing protocol statement"),
-            ContentLengthParsing(e) => write!(f, "Error parsing HTTP Content-Length field: {e}"),
-            ServerResponse(s, o) => {
-                if let Some(o) = o {
-                    write!(f, "Server responded with an error: {s:?}: {o}")
-                } else {
-                    write!(f, "Server responded with an error: {s:?}")
-                }
-            }
-        }
-    }
 }
 
 #[derive(Clone, Copy, Debug)]

api_client/Cargo.toml
@@ -5,4 +5,5 @@ name = "api_client"
 version = "0.1.0"
 
 [dependencies]
+thiserror = "1.0.61"
 vmm-sys-util = "0.12.1"

commit 8995a68bdaa1ed7a9d33b55644919ec1a343c20b

api_client: show response body in ServerResponse
Otherwise, you just see output like "Error running command: Server
responded with an error: InternalServerError", which isn't very
helpful.  The response body used to be include with the message, but
was removed when the Error enum was converted to thiserror.

Fixes: 5d0d56f5 ("api_client: Use thiserror for errors")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

api_client/src/lib.rs
@@ -20,7 +20,7 @@ pub enum Error {
     MissingProtocol,
     #[error("Error parsing HTTP Content-Length field: {0}")]
     ContentLengthParsing(std::num::ParseIntError),
-    #[error("Server responded with an error: {0:?}")]
+    #[error("Server responded with an error: {0:?}: {1:?}")]
     ServerResponse(StatusCode, Option<String>),
 }
 