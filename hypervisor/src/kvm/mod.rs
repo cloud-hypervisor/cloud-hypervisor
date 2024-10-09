7d1ac1b8680ebe06a999a6039e7a789f1b410ddf
Ruoqing He <heruoqing@iscas.ac.cn>
2024-10-09 20:39:39 +0800

    hypervisor: kvm: Introduce riscv64 register g/set
    Implement macros to calculate register ID on riscv64, definition of
    RISC-V `VcpuKvmState`.
    
    Signed-off-by: Ruoqing He <heruoqing@iscas.ac.cn>

--- hypervisor/src/kvm/mod.rs
+++ hypervisor/src/kvm/mod.rs
@@ -65,6 +65,9 @@ use crate::{
 // aarch64 dependencies
 #[cfg(target_arch = "aarch64")]
 pub mod aarch64;
+// riscv64 dependencies
+#[cfg(target_arch = "riscv64")]
+pub mod riscv64;
 #[cfg(target_arch = "aarch64")]
 use std::mem;
 
