86315adb23718885d7c8e24187a5484a43e34151
Ruoqing He <heruoqing@iscas.ac.cn>
2024-10-18 17:36:51 +0800

    misc: Use c"" to construct nul-terminated string
    As clippy of rust-toolchain version 1.83.0-beta.1 suggests, use `c""` to
    construct nul-terminated `CStr`.
    
    Signed-off-by: Ruoqing He <heruoqing@iscas.ac.cn>

--- net_util/src/tap.rs
+++ net_util/src/tap.rs
@@ -124,7 +124,7 @@ impl Tap {
             // Open calls are safe because we give a constant null-terminated
             // string and verify the result.
             libc::open(
-                b"/dev/net/tun\0".as_ptr() as *const c_char,
+                c"/dev/net/tun".as_ptr() as *const c_char,
                 flags.unwrap_or(libc::O_RDWR | libc::O_NONBLOCK | libc::O_CLOEXEC),
             )
         };
