fa686fdfc72e91f7a02f7eb5e99d3811f8865356
Rob Bradford <rbradford@rivosinc.com>
2024-11-23 11:10:18 +0000

    tests: Bump OVMF version
    Signed-off-by: Rob Bradford <rbradford@rivosinc.com>

--- scripts/test-util.sh
+++ scripts/test-util.sh
@@ -170,7 +170,7 @@ prepare_linux() {
 }
 
 download_ovmf() {
-    OVMF_FW_TAG="ch-6624aa331f"
+    OVMF_FW_TAG="ch-a54f262b09"
     OVMF_FW_URL="https://github.com/cloud-hypervisor/edk2/releases/download/$OVMF_FW_TAG/CLOUDHV.fd"
     OVMF_FW="$WORKLOADS_DIR/CLOUDHV.fd"
     pushd "$WORKLOADS_DIR" || exit
