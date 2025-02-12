e0cf9dbd883437cba20c464a9e105189145ed6e4
Kevin Garbe <kgarbe@google.com>
2025-02-12 18:21:25 +0000

    Fix typo, apply formatting
    Fix split "then"
    Apply shellcheck and shfmt
    Apply kokoro config to bazel for cleaner logging.
    
    Change-Id: I9ba64b12826fcfdd3276501264d6881b63059755

--- google_internal/kokoro/kokoro_swarming_build.sh
+++ google_internal/kokoro/kokoro_swarming_build.sh
@@ -13,36 +13,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-## Set for debugging
+# Fail on any error.
 set -e
+
+## Set for debugging
 # set -x
 
 KOKORO_CHV_DIR="${KOKORO_ARTIFACTS_DIR}/git/cloud-hypervisor"
 CHV_SWARMING_DIR="${KOKORO_CHV_DIR}/google_internal/swarming"
 
-# Install latest bazel
-# Despite the image having 7.4.1, it only seems to find 6.5.0
+# Install latest bazel, since ubuntu2004 image used has 6.5.0
 BAZEL_VERSION=bazel-7.4.1-linux-x86_64
 BAZEL_TMP_DIR=/tmpfs/tmp/bazel-release
 mkdir -p "${BAZEL_TMP_DIR}"
-echo "Bazel file: ${KOKORO_GFILE_DIR}/${BAZEL_VERSION?}"
-ls "${KOKORO_GFILE_DIR}"
 ln -fs "${KOKORO_GFILE_DIR}/${BAZEL_VERSION?}" "${BAZEL_TMP_DIR}/bazel"
 chmod 755 "${KOKORO_GFILE_DIR}/${BAZEL_VERSION?}"
 export PATH="${BAZEL_TMP_DIR}:${PATH}"
 
-# This should show /tmpfs/tmp/bazel-release/bazel
-# which bazel
-KOKORO_LOCAL_DIR="${KOKORO_ARTIFACTS_DIR}/git/cloud-hypervisor/google-internal/kokoro"
+# KOKORO_LOCAL_DIR="${KOKORO_ARTIFACTS_DIR}/git/cloud-hypervisor/google-internal/kokoro"
 
-cd "${KOKORO_CHV_DIR}/google_internal/kokoro"
+pushd "${KOKORO_CHV_DIR}/google_internal/kokoro"
 
 # TODO (b/395680242): RBE Support for CHV
 # source "${KOKORO_CHV_DIR}/google_internal/lib_build.sh"
 # lib_build::set_rbe_flags
 
 args=(
+  --bazelrc="${KOKORO_CHV_DIR}/google_internal/.bazelrc"
   build
+  --config=kokoro
   --verbose_failures=true
   # --test_output=all
   # --symlink_prefix=/
@@ -74,29 +73,34 @@ TESTS_LIST=(
 SWARMING_TEST_DIR=${KOKORO_ARTIFACTS_DIR}/swarming_test
 mkdir -p "${SWARMING_TEST_DIR}/tests"
 
-for TEST_NAME in ${TESTS_LIST[@]}; do
+for TEST_NAME in "${TESTS_LIST[@]}"; do
   echo "Copying test: ${TEST_NAME}"
   mkdir -p "${SWARMING_TEST_DIR}/tests/${TEST_NAME}"
   cp "${KOKORO_CHV_DIR}/bazel-bin/${TEST_NAME}/test-"*"/${TEST_NAME}_test" "${SWARMING_TEST_DIR}/tests/${TEST_NAME}/${TEST_NAME}_test"
 done
 
+popd
+
 ### Install and set up swarming/isolate
-export LUCI_ROOT="`pwd`/luci"
-mkdir -p ${LUCI_ROOT}
+LUCI_ROOT="$(pwd)/luci"
+export LUCI_ROOT
+mkdir -p "${LUCI_ROOT}"
 
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-export PATH="`pwd`/depot_tools:$PATH"
+PATH="$(pwd)/depot_tools:$PATH"
+export PATH
+#shellcheck disable=SC2016
 (
   INFRA_GIT_REVISION=9ea28c0ad6f003d65a6e559e60f5f78029c4cf85
   echo 'infra/tools/luci/isolate/${platform} git_revision:'"${INFRA_GIT_REVISION}"
   echo 'infra/tools/luci/swarming/${platform} git_revision:'"${INFRA_GIT_REVISION}"
-) > ${LUCI_ROOT}/ensure_file.txt
-cipd ensure -ensure-file ${LUCI_ROOT}/ensure_file.txt -root ${LUCI_ROOT}
+) > "${LUCI_ROOT}/ensure_file.txt"
+cipd ensure -ensure-file "${LUCI_ROOT}/ensure_file.txt" -root "${LUCI_ROOT}"
 
 KOKORO_KEY_NAME="78411_swarming-service-key"
 KOKORO_KEY_PATH="${KOKORO_KEYSTORE_DIR}/${KOKORO_KEY_NAME}"
 export SWARMING_AUTH_FLAG="--service-account-json=${KOKORO_KEY_PATH}"
-export SWARMING_TASK_PREFIX="Kokoro_PR${KOKORO_GITHUB_PULL_REQUEST_NUMBER}"
+export SWARMING_TASK_PREFIX="Kokoro_PR${KOKORO_GERRIT_CHANGE_NUMBER}"
 
 # Swarming environment
 
@@ -108,14 +112,14 @@ pushd "${SWARMING_TEST_DIR}"
 SWARMING_TRIGGER_ERROR=0
 
 for TEST_DIR in tests/*; do
-  TEST_BINARY=$(basename ${TEST_DIR})
+  TEST_BINARY="$(basename -- "${TEST_DIR}")"
   FULL_BINARY_PATH="${TEST_DIR}/${TEST_BINARY}_test"
 
   set +e
   "${CHV_SWARMING_DIR}/trigger.py" --prefix "${SWARMING_TASK_PREFIX}" "${TEST_DIR}" "${FULL_BINARY_PATH}"
   EXIT_CODE=$?
   set -e
-  if [ ${EXIT_CODE} -ne 0 ] ; then
+  if [ ${EXIT_CODE} -ne 0 ]; then
     echo "Swarming trigger error on test: ${TEST_NAME}"
     SWARMING_TRIGGER_ERROR=1
   fi
@@ -129,13 +133,13 @@ popd
 pushd "${SWARMING_TEST_DIR}"
 
 SWARMING_FAILURE=0
-for TEST_NAME in triggered/*/*.json ; do
+for TEST_NAME in triggered/*/*.json; do
   echo "Collecting for test name: ${TEST_NAME}"
   set +e
-  "${CHV_SWARMING_DIR}/collect.py" "${SWARMING_TIMESTAMP}" "${KOKORO_GIT_COMMIT}" "$(basename "${TEST_NAME}" .json)" "${TEST_NAME}"
+  "${CHV_SWARMING_DIR}/collect.py" "${SWARMING_TIMESTAMP}" "${KOKORO_GIT_COMMIT}" "$(basename -- "${TEST_NAME}" .json)" "${TEST_NAME}"
   EXIT_CODE=$?
   set -e
-  if [ ${EXIT_CODE} -eq 0 ] ; then
+  if [ ${EXIT_CODE} -eq 0 ]; then
     echo "PASS ${TEST_NAME}"
   else
     echo "FAIL ${TEST_NAME}"
@@ -144,12 +148,12 @@ for TEST_NAME in triggered/*/*.json ; do
 done
 popd
 
-if [ ${SWARMING_FAILURE} -eq 1 ] ; then
+if [ ${SWARMING_FAILURE} -eq 1 ]; then
   echo "Error: some Swarming test failed"
   exit 1
 fi
 
-if [ ${SWARMING_TRIGGER_ERROR} -eq 1 ] ; t  hen
+if [ ${SWARMING_TRIGGER_ERROR} -eq 1 ]; then
   echo "Error: could not trigger some Swarming tests"
   exit 1
 fi
