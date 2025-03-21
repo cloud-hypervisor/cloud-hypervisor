fef3108c51f573f59828f8ef539357e11bbba0b6
Alex Orozco <alexorozco@google.com>
2025-03-21 21:04:50 +0000

    Final trim of KVM + SEV-SNP changes
    I used the diff from the PR on github to make these final changes.
    
    Bug: b/397784779
    Change-Id: Iaeb7cce5546a6a6eaa3d8e69cbc85e535c44b8d9

--- vmm/src/vm.rs
+++ vmm/src/vm.rs
@@ -40,8 +40,6 @@ use gdbstub_arch::aarch64::reg::AArch64CoreRegs as CoreRegs;
 #[cfg(all(target_arch = "x86_64", feature = "guest_debug"))]
 use gdbstub_arch::x86::reg::X86_64CoreRegs as CoreRegs;
 use hypervisor::{HypervisorVmError, VmOps};
-#[cfg(feature = "sev_snp")]
-use igvm_defs::SnpPolicy;
 use libc::{termios, SIGWINCH};
 use linux_loader::cmdline::Cmdline;
 #[cfg(all(target_arch = "x86_64", feature = "guest_debug"))]
@@ -490,16 +488,6 @@ pub struct Vm {
 impl Vm {
     pub const HANDLED_SIGNALS: [i32; 1] = [SIGWINCH];
 
-    #[cfg(feature = "sev_snp")]
-    pub fn get_default_sev_snp_guest_policy() -> igvm_defs::SnpPolicy {
-        SnpPolicy::new()
-            .with_abi_minor(0)
-            .with_abi_major(0)
-            .with_smt(1)
-            .with_reserved_must_be_one(1)
-            .with_migrate_ma(0)
-    }
-
     #[allow(clippy::too_many_arguments)]
     pub fn new_from_memory_manager(
         config: Arc<Mutex<VmConfig>>,
@@ -634,10 +622,9 @@ impl Vm {
         // This initial SEV-SNP configuration must be done immediately after
         // vCPUs are created. As part of this initialization we are
         // transitioning the guest into secure state.
-        #[cfg(all(feature = "sev_snp", feature = "igvm"))]
+        #[cfg(feature = "sev_snp")]
         if sev_snp_enabled {
-            vm.sev_snp_init(Vm::get_default_sev_snp_guest_policy())
-                .map_err(Error::InitializeSevSnpVm)?;
+            vm.sev_snp_init().map_err(Error::InitializeSevSnpVm)?;
         }
 
         #[cfg(feature = "tdx")]
@@ -959,18 +946,6 @@ impl Vm {
             .map_err(Error::MemoryManager)?
         };
 
-        #[cfg(target_arch = "x86_64")]
-        // Note: For x86, always call this function before invoking start boot vcpus.
-        // Otherwise guest would fail to boot because we haven't created the
-        // userspace mappings to update the hypervisor about the memory mappings.
-        // These mappings must be created before we start the vCPU threads for
-        // the very first time.
-        memory_manager
-            .lock()
-            .unwrap()
-            .allocate_address_space()
-            .map_err(Error::MemoryManager)?;
-
         Vm::new_from_memory_manager(
             vm_config,
             memory_manager,
@@ -1165,7 +1140,6 @@ impl Vm {
         cpu_manager: Arc<Mutex<cpu::CpuManager>>,
         #[cfg(feature = "sev_snp")] host_data: &Option<String>,
     ) -> Result<EntryPoint> {
-
         let res = igvm_loader::load_igvm(
             &igvm,
             memory_manager,
@@ -1253,20 +1227,19 @@ impl Vm {
         payload: &PayloadConfig,
         memory_manager: Arc<Mutex<MemoryManager>>,
         #[cfg(feature = "igvm")] cpu_manager: Arc<Mutex<cpu::CpuManager>>,
-        #[cfg(feature = "sev_snp")] _sev_snp_enabled: bool,
+        #[cfg(feature = "sev_snp")] sev_snp_enabled: bool,
     ) -> Result<EntryPoint> {
         trace_scoped!("load_payload");
         #[cfg(feature = "igvm")]
         {
             if let Some(_igvm_file) = &payload.igvm {
                 let igvm = File::open(_igvm_file).map_err(Error::IgvmFile)?;
-                return Self::load_igvm(
-                    igvm,
-                    memory_manager,
-                    cpu_manager,
-                    #[cfg(feature = "sev_snp")]
-                    &payload.host_data,
-                );
+                #[cfg(feature = "sev_snp")]
+                if sev_snp_enabled {
+                    return Self::load_igvm(igvm, memory_manager, cpu_manager, &payload.host_data);
+                }
+                #[cfg(not(feature = "sev_snp"))]
+                return Self::load_igvm(igvm, memory_manager, cpu_manager);
             }
         }
         match (
@@ -2299,7 +2272,7 @@ impl Vm {
                     // In case of SEV-SNP guest ACPI tables are provided via
                     // IGVM. So skip the creation of ACPI tables and set the
                     // rsdp addr to None.
-                    self.create_acpi_tables()
+                    None
                 } else {
                     self.create_acpi_tables()
                 };
@@ -2360,6 +2333,18 @@ impl Vm {
 
         #[cfg(target_arch = "riscv64")]
         self.configure_system().unwrap();
+
+        #[cfg(target_arch = "x86_64")]
+        // Note: For x86, always call this function before invoking start boot vcpus.
+        // Otherwise guest would fail to boot because we haven't created the
+        // userspace mappings to update the hypervisor about the memory mappings.
+        // These mappings must be created before we start the vCPU threads for
+        // the very first time.
+        self.memory_manager
+            .lock()
+            .unwrap()
+            .allocate_address_space()
+            .map_err(Error::MemoryManager)?;
 
         #[cfg(feature = "tdx")]
         if let Some(hob_address) = hob_address {
