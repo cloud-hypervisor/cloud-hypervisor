cloud-hypervisor/cloud-hypervisor

commit f0c1f8d079d90d2390b2c01a88d81cd00dd08209

pci: vfio: VFIO_IOMMU_MAP_DMA mmio regions
For all VFIO devices, map all non-emulated MMIO regions to
the vfio container to allow PCIe P2P between all VFIO devices
on the virtual machine. This is required for a wide variety of
advanced GPU workloads such as GPUDirect P2P (DMA between two
GPUs), GPUDirect RDMA (DMA between a GPU and an IB device).

Signed-off-by: Thomas Barrett <tbarrett@crusoeenergy.com>

pci/src/vfio.rs
@@ -1568,6 +1568,16 @@ impl VfioPciDevice {
                     self.vm
                         .create_user_memory_region(mem_region)
                         .map_err(VfioPciError::CreateUserMemoryRegion)?;
+
+                    if !self.iommu_attached {
+                        self.container
+                            .vfio_dma_map(
+                                user_memory_region.start,
+                                user_memory_region.size,
+                                user_memory_region.host_addr,
+                            )
+                            .map_err(VfioPciError::DmaMap)?;
+                    }
                 }
             }
         }
@@ -1578,6 +1588,16 @@ impl VfioPciDevice {
     pub fn unmap_mmio_regions(&mut self) {
         for region in self.common.mmio_regions.iter() {
             for user_memory_region in region.user_memory_regions.iter() {
+                // Unmap from vfio container
+                if !self.iommu_attached {
+                    if let Err(e) = self
+                        .container
+                        .vfio_dma_unmap(user_memory_region.start, user_memory_region.size)
+                    {
+                        error!("Could not unmap mmio region from vfio container: {}", e);
+                    }
+                }
+
                 // Remove region
                 let r = self.vm.make_user_memory_region(
                     user_memory_region.slot,

commit b7d8e8b1be874fe218a7ee5d4c2a5f22231a83cf

pci: vfio: make DMA mapping errors non-fatal
We saw a regression with a VFIO integration test based on
cloud-hypervisor nested in QEMU with a virtual XHCI controller, when the
DMA mapping failed. The DMA mapping for p2p operation is not strictly
mandatory for a working single device pass through.

The root cause of the mapping failure is that the default QEMU iommu
only exposes a 39 bit address space, and the MMIO region for the
virtual device BAR0 is allocated outside this range. The test was fixed
by explicitly providing a full-size IOMMU for the QEMU invocation
(aw_bits=48), but arguably the current handling is not optimal.
Cloud-hypervisor should respect platform iommu limitations when
allocating the BAR in the first place.

For now, just emit a loud error message, which corresponds to the
expected unmap error message.

Fixes: f0c1f8d07 ("pci: vfio: VFIO_IOMMU_MAP_DMA mmio regions")

Signed-off-by: Stefan Nürnberger <stefan.nuernberger@cyberus-technology.de>

pci/src/vfio.rs
@@ -1662,8 +1662,12 @@ impl VfioPciDevice {
                                 user_memory_region.host_addr,
                             )
                             .map_err(|e| {
-                                VfioPciError::DmaMap(e, self.device_path.clone(), self.bdf)
-                            })?;
+                                error!(
+                                    "{}: p2p operation between pass-through devices may not work",
+                                    VfioPciError::DmaMap(e, self.device_path.clone(), self.bdf)
+                                );
+                            })
+                            .unwrap_or_default();
                     }
                 }
             }