1307d31ede3f76d9bf83ef40b0ae4784b63e7e5e
Bo Chen <bchen@crusoe.ai>
2025-05-05 15:20:24 -0700

    pci: vfio: Report more information with failed `vfio_dma_unmap`
    Signed-off-by: Bo Chen <bchen@crusoe.ai>

--- pci/src/vfio.rs
+++ pci/src/vfio.rs
@@ -1718,8 +1718,13 @@ impl VfioPciDevice {
                     if let Err(e) = self
                         .container
                         .vfio_dma_unmap(user_memory_region.start, user_memory_region.size)
+                        .map_err(|e| VfioPciError::DmaUnmap(e, self.device_path.clone(), self.bdf))
                     {
-                        error!("Could not unmap mmio region from vfio container: {}", e);
+                        error!(
+                            "Could not unmap mmio region from vfio container: \
+                            iova 0x{:x}, size 0x{:x}: {}, ",
+                            user_memory_region.start, user_memory_region.size, e
+                        );
                     }
                 }
 
