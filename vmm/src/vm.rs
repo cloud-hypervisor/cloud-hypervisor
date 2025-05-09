6c6c64fa23d4449c5bd03942cb8f3eddc0b1514d
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2025-05-09 14:13:26 +0200

    block: introduce advisory locks for disk image files
    # What
    
    This commit introduces file-based advisory locking for the files
    backing up the block devices by using the fcntl() syscall with
    OFD locks. The per-open-file-descriptor (OFD) locks are more robust
    than traditional POSIX locks (F_SETLK) as they are not tied to
    process IDs and avoid common issues in multithreaded or multi-fd
    scenarios [1].
    
    The locking mechanism is aware of the `readonly` property and allows
    `n` readers or `1` writer (exclusive mode).
    
    As the locks are advisory, multiple cloud-hypervisor processes can
    prevent themselves from writing to the same file. However, this is not
    a system-wide file-system level locking mechanism preventing to open()
    a file.
    
    # Why
    
    To prevent misconfiguration and improve safety, it is good practice to
    protect disk image files with a locking mechanism. Experience and common
    best practices suggest that advisory locks are preferable over mandatory
    locks due to better compatibility and fewer pitfalls (in fs space).
    
    The introduced functionality is aligned with the approach taken by
    QEMU [0], and is also recommended in [1].
    
    # Implementation Details
    
    We need to ensure that not only normal operation works but also
    save-state-resume and live-migration. Especially, for live migration
    it is crucial that the receiver VMM releases the locks and the sender
    VMM acquires them right after.
    
    Therefore, the locking and releasing happens directly on the block
    device struct. The device manager knows all block devices and can
    forward requests to these types.
    
    When the VM shuts down and all FDs are closed, locks are also
    released automatically.
    
    # Testing
    
    I tested normal operation, save state/resume, and live-migration
    with read/shared and write/exclusive locks. One can use the
    fcntl-tool CLI [2] to monitor the locks.
    
    # Links
    
    [0] https://github.com/qemu/qemu/blob/825b96dbcee23d134b691fc75618b59c5f53da32/util/osdep.c#L266
    [1] https://apenwarr.ca/log/20101213
    [2] https://crates.io/crates/fcntl-tool
    
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>
    Co-authored-by: Thomas Prescher <thomas.prescher@cyberus-technology.de>
    
    On-behalf-of: SAP <philipp.schuster@sap.com>

--- vmm/src/vm.rs
+++ vmm/src/vm.rs
@@ -330,6 +330,9 @@ pub enum Error {
 
     #[error("Error creating console devices")]
     CreateConsoleDevices(ConsoleDeviceError),
+
+    #[error("Error locking the disk images")]
+    LockingError(DeviceManagerError),
 }
 pub type Result<T> = result::Result<T, Error>;
 
@@ -2176,6 +2179,14 @@ impl Vm {
             return self.resume().map_err(Error::Resume);
         }
 
+        // We acquire all advisory disk image locks here and not on device creation
+        // to enable live-migration without locking issues.
+        self.device_manager
+            .lock()
+            .unwrap()
+            .try_lock_disks()
+            .map_err(Error::LockingError)?;
+
         let new_state = if self.stop_on_boot {
             VmState::BreakPoint
         } else {
@@ -2323,6 +2334,13 @@ impl Vm {
     pub fn restore(&mut self) -> Result<()> {
         event!("vm", "restoring");
 
+        // We acquire all advisory disk image locks again.
+        self.device_manager
+            .lock()
+            .unwrap()
+            .try_lock_disks()
+            .map_err(Error::LockingError)?;
+
         #[cfg(target_arch = "x86_64")]
         // Note: For x86, always call this function before invoking start boot vcpus.
         // Otherwise guest would fail to boot because we haven't created the
@@ -2446,6 +2464,21 @@ impl Vm {
 
     pub fn device_tree(&self) -> Arc<Mutex<DeviceTree>> {
         self.device_manager.lock().unwrap().device_tree()
+    }
+
+    /// Release all advisory locks held for the disk images.
+    ///
+    /// This should only be called when the VM is stopped and the VMM supposed
+    /// to shut down. A new VMM, either after a live migration or a
+    /// state save/resume cycle, should then acquire all locks before the VM
+    /// starts to run.
+    pub fn release_disk_locks(&self) -> Result<()> {
+        self.device_manager
+            .lock()
+            .unwrap()
+            .release_disk_locks()
+            .map_err(Error::LockingError)?;
+        Ok(())
     }
 
     pub fn activate_virtio_devices(&self) -> Result<()> {
