e69a89f9afae592dc75f2ee12e4cfb53e533cbda
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2025-06-17 14:50:46 +0200

    vmm: add support for live-migration of virtio-net devices & network FDs
    This adds support for the live migration of virtio-net devices backed by
    network FDs.
    
    So far, this was tested successfully with a patched libvirt setup.
    `receive-migration` via ch-remote is not supported as transferring any
    FDs from there to Cloud Hypervisor isn't really sensible in typical
    workflows.
    
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>
    On-behalf-of: SAP philipp.schuster@sap.com

--- vmm/src/api/http/http_endpoint.rs
+++ vmm/src/api/http/http_endpoint.rs
@@ -43,8 +43,8 @@ use crate::api::VmCoredump;
 use crate::api::{
     AddDisk, ApiAction, ApiError, ApiRequest, NetConfig, VmAddDevice, VmAddFs, VmAddNet, VmAddPmem,
     VmAddUserDevice, VmAddVdpa, VmAddVsock, VmBoot, VmConfig, VmCounters, VmDelete, VmNmi, VmPause,
-    VmPowerButton, VmReboot, VmReceiveMigration, VmRemoveDevice, VmResize, VmResizeZone, VmRestore,
-    VmResume, VmSendMigration, VmShutdown, VmSnapshot,
+    VmPowerButton, VmReboot, VmReceiveMigration, VmReceiveMigrationData, VmRemoveDevice, VmResize,
+    VmResizeZone, VmRestore, VmResume, VmSendMigration, VmShutdown, VmSnapshot,
 };
 use crate::config::{RestoreConfig, RestoredNetConfig};
 use crate::cpu::Error as CpuError;
@@ -229,7 +229,6 @@ vm_action_put_handler_body!(VmAddUserDevice);
 vm_action_put_handler_body!(VmRemoveDevice);
 vm_action_put_handler_body!(VmResizeZone);
 vm_action_put_handler_body!(VmSnapshot);
-vm_action_put_handler_body!(VmReceiveMigration);
 vm_action_put_handler_body!(VmSendMigration);
 
 #[cfg(all(target_arch = "x86_64", feature = "guest_debug"))]
@@ -340,6 +339,44 @@ impl PutHandler for VmAddNet {
 }
 
 impl GetHandler for VmAddNet {}
+
+// Special Handling for virtio-net Devices Backed by Network File Descriptors
+//
+// See above.
+impl PutHandler for VmReceiveMigration {
+    fn handle_request(
+        &'static self,
+        api_notifier: EventFd,
+        api_sender: Sender<ApiRequest>,
+        body: &Option<Body>,
+        files: Vec<File>,
+    ) -> std::result::Result<Option<Body>, HttpError> {
+        if let Some(body) = body {
+            let mut net_cfg: VmReceiveMigrationData = serde_json::from_slice(body.raw())?;
+            if let Some(cfgs) = &mut net_cfg.net_fds {
+                let mut cfgs = cfgs.iter_mut().collect::<Vec<&mut _>>();
+                let cfgs = cfgs.as_mut_slice();
+                apply_new_fds_to_cfg::<RestoredNetConfig>(
+                    files,
+                    cfgs,
+                    &|cfg| Some(&cfg.id),
+                    &|cfg| cfg.num_fds,
+                    &|cfg| cfg.fds.as_deref(),
+                    &|cfg, value| {
+                        cfg.fds = value;
+                    },
+                )?;
+            }
+
+            self.send(api_notifier, api_sender, net_cfg)
+                .map_err(HttpError::ApiError)
+        } else {
+            Err(HttpError::BadRequest)
+        }
+    }
+}
+
+impl GetHandler for VmReceiveMigration {}
 
 impl PutHandler for VmResize {
     fn handle_request(
