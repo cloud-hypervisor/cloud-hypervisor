17e23f0da6b35f00c79c1440311051712737d269
Muminul Islam <muislam@microsoft.com>
2025-08-13 00:21:50 +0000

    block: virtio-devices: block: Clarify the return of execute_async()
    Instead of returning boolean return an struct of completion status
    so that it can be cached for batch submission.
    
    Signed-off-by: Bo Chen <bchen@crusoe.ai>
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

--- virtio-devices/src/block.rs
+++ virtio-devices/src/block.rs
@@ -20,7 +20,7 @@ use std::{io, result};
 use anyhow::anyhow;
 use block::async_io::{AsyncIo, AsyncIoError, DiskFile};
 use block::fcntl::{get_lock_state, LockError, LockType};
-use block::{build_serial, fcntl, Request, RequestType, VirtioBlockConfig};
+use block::{build_serial, fcntl, ExecuteAsync, Request, RequestType, VirtioBlockConfig};
 use rate_limiter::group::{RateLimiterGroup, RateLimiterGroupHandle};
 use rate_limiter::TokenType;
 use seccompiler::SeccompAction;
@@ -220,7 +220,11 @@ impl BlockEpollHandler {
                 desc_chain.head_index() as u64,
             );
 
-            if let Ok(true) = result {
+            if let Ok(ExecuteAsync {
+                async_complete: true,
+                ..
+            }) = result
+            {
                 self.inflight_requests
                     .push_back((desc_chain.head_index(), request));
             } else {
