3734a13cbf306ed098f94bbe3641c9a155cef6fe
Rob Bradford <rbradford@rivosinc.com>
2025-11-14 15:04:59 +0000

    tests: Use cargo nextest for integration tests
    This alternative test runner supports retries and also reports how long
    each test takes to run.
    
    Signed-off-by: Rob Bradford <rbradford@rivosinc.com>

--- scripts/run_integration_tests_windows_aarch64.sh
+++ scripts/run_integration_tests_windows_aarch64.sh
@@ -42,7 +42,7 @@ cargo build --all --release --target "$BUILD_TARGET"
 
 # Only run with 1 thread to avoid tests interfering with one another because
 # Windows has a static IP configured
-time cargo test "windows::$test_filter" --target "$BUILD_TARGET" -- ${test_binary_args[*]}
+time cargo nextest run "windows::$test_filter" --target "$BUILD_TARGET" -- ${test_binary_args[*]}
 RES=$?
 
 dmsetup remove_all -f
