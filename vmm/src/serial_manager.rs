cloud-hypervisor/cloud-hypervisor

commit 287887c99c98e47819b3ac999cf4fa94c98407d5

vmm: fix console IO safety
Rebooting a VM fails with the following error when debug assertions
are enabled:

	fatal runtime error: IO Safety violation: owned file descriptor already closed

This happens because FromRawFd::from_raw_fd is used on RawFds stored
in ConsoleInfo every time a VM begins to boot, so the second
time (after a reboot, or if the first attempt to boot via the API
failed), the fd will be closed.  Until this assertion is hit, the code
is operating on either closed file descriptors, or new file
descriptors for something completely different.  If debug assertions
are disabled, it will just continue doing this with unpredictable
results.

To fix this, and prevent the problem reocurring, ownership of the
console file descriptors needs to be properly tracked, using Rust's
type system, so this commit refactors the console code to do that.
The file descriptors are now passed around with reference counts, so
they won't be closed prematurely.  The obvious way to do this would be
to just have each member of ConsoleInfo be an Arc<File>, but we need
to accomodate that serial console file descriptors can also be
sockets.  We can't just store an OwnedFd and convert it when it's
used, because we only get a reference from the Arc, so we need to
store the descriptors as their concrete types in an enum.  Since this
basically duplicates the ConsoleOutputMode enum from the config, the
ConsoleOutputMode enum is now not used past constructing the
ConsoleInfo.

So that ownership can be represented consistently, the debug console's
tty mode now uses its own stdout descriptor.

I'm still using .try_clone().unwrap() (i.e. dup()) to clone file
descriptors for Endpoint::FilePair and Endpoint::TtyPair, because I
assume there's a reason for them not just to hold a single file
descriptor.

I've also retained the existing behaviour of having serial manager
ignore the tty file descriptor passed to it (which is stdout), and

vmm/src/serial_manager.rs
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-use crate::config::ConsoleOutputMode;
+use crate::console_devices::ConsoleOutput;
 #[cfg(target_arch = "aarch64")]
 use devices::legacy::Pl011;
 #[cfg(target_arch = "x86_64")]
@@ -13,9 +13,8 @@ use serial_buffer::SerialBuffer;
 use std::fs::File;
 use std::io::Read;
 use std::net::Shutdown;
-use std::os::fd::RawFd;
 use std::os::unix::io::{AsRawFd, FromRawFd, IntoRawFd};
-use std::os::unix::net::{UnixListener, UnixStream};
+use std::os::unix::net::UnixStream;
 use std::panic::AssertUnwindSafe;
 use std::path::PathBuf;
 use std::sync::atomic::{AtomicBool, Ordering};
@@ -112,11 +111,10 @@ pub struct SerialManager {
     #[cfg(target_arch = "aarch64")]
     serial: Arc<Mutex<Pl011>>,
     epoll_file: File,
-    in_file: File,
+    in_file: ConsoleOutput,
     kill_evt: EventFd,
     handle: Option<thread::JoinHandle<()>>,
     pty_write_out: Option<Arc<AtomicBool>>,
-    mode: ConsoleOutputMode,
     socket_path: Option<PathBuf>,
 }
 
@@ -124,23 +122,14 @@ impl SerialManager {
     pub fn new(
         #[cfg(target_arch = "x86_64")] serial: Arc<Mutex<Serial>>,
         #[cfg(target_arch = "aarch64")] serial: Arc<Mutex<Pl011>>,
-        main_fd: Option<RawFd>,
-        mode: ConsoleOutputMode,
+        mut output: ConsoleOutput,
         socket: Option<PathBuf>,
     ) -> Result<Option<Self>> {
         let mut socket_path: Option<PathBuf> = None;
 
-        let in_file = match mode {
-            ConsoleOutputMode::Pty => {
-                if let Some(pty_main) = main_fd {
-                    // SAFETY: pty_main is guaranteed to be a valid fd from
-                    // pre_create_console_devices() in vmm/src/console_devices.rs
-                    unsafe { File::from_raw_fd(pty_main) }
-                } else {
-                    return Ok(None);
-                }
-            }
-            ConsoleOutputMode::Tty => {
+        let in_fd = match output {
+            ConsoleOutput::Pty(ref fd) => fd.as_raw_fd(),
+            ConsoleOutput::Tty(_) => {
                 // If running on an interactive TTY then accept input
                 // SAFETY: trivially safe
                 if unsafe { libc::isatty(libc::STDIN_FILENO) == 1 } {
@@ -162,22 +151,17 @@ impl SerialManager {
                         return Err(Error::SetNonBlocking(std::io::Error::last_os_error()));
                     }
 
-                    stdin_clone
+                    output = ConsoleOutput::Tty(Arc::new(stdin_clone));
+                    fd
                 } else {
                     return Ok(None);
                 }
             }
-            ConsoleOutputMode::Socket => {
-                if let Some(socket_fd) = main_fd {
-                    if let Some(path_in_socket) = socket {
-                        socket_path = Some(path_in_socket.clone());
-                    }
-                    // SAFETY: socke_fd is guaranteed to be a valid fd from
-                    // pre_create_console_devices() in vmm/src/console_devices.rs
-                    unsafe { File::from_raw_fd(socket_fd) }
-                } else {
-                    return Ok(None);
+            ConsoleOutput::Socket(ref fd) => {
+                if let Some(path_in_socket) = socket {
+                    socket_path = Some(path_in_socket.clone());
                 }
+                fd.as_raw_fd()
             }
             _ => return Ok(None),
         };
@@ -193,7 +177,7 @@ impl SerialManager {
         )
         .map_err(Error::Epoll)?;
 
-        let epoll_fd_data = if mode == ConsoleOutputMode::Socket {
+        let epoll_fd_data = if let ConsoleOutput::Socket(_) = output {
             EpollDispatch::Socket
         } else {
             EpollDispatch::File
@@ -202,16 +186,16 @@ impl SerialManager {
         epoll::ctl(
             epoll_fd,
             epoll::ControlOptions::EPOLL_CTL_ADD,
-            in_file.as_raw_fd(),
+            in_fd,
             epoll::Event::new(epoll::Events::EPOLLIN, epoll_fd_data as u64),
         )
         .map_err(Error::Epoll)?;
 
         let mut pty_write_out = None;
-        if mode == ConsoleOutputMode::Pty {
+        if let ConsoleOutput::Pty(ref file) = output {
             let write_out = Arc::new(AtomicBool::new(false));
             pty_write_out = Some(write_out.clone());
-            let writer = in_file.try_clone().map_err(Error::FileClone)?;
+            let writer = file.try_clone().map_err(Error::FileClone)?;
             let buffer = SerialBuffer::new(Box::new(writer), write_out);
             serial
                 .as_ref()
@@ -227,11 +211,10 @@ impl SerialManager {
         Ok(Some(SerialManager {
             serial,
             epoll_file,
-            in_file,
+            in_file: output,
             kill_evt,
             handle: None,
             pty_write_out,
-            mode,
             socket_path,
         }))
     }
@@ -270,15 +253,10 @@ impl SerialManager {
         }
 
         let epoll_fd = self.epoll_file.as_raw_fd();
-        let mut in_file = self.in_file.try_clone().map_err(Error::FileClone)?;
+        let in_file = self.in_file.clone();
         let serial = self.serial.clone();
         let pty_write_out = self.pty_write_out.clone();
-        // SAFETY: from_raw_fd is always called with a valid fd
-        let listener = unsafe {
-            UnixListener::from_raw_fd(in_file.try_clone().map_err(Error::FileClone)?.into_raw_fd())
-        };
         let mut reader: Option<UnixStream> = None;
-        let mode = self.mode.clone();
 
         // In case of PTY, we want to be able to detect a connection on the
         // other end of the PTY. This is done by detecting there's no event
@@ -314,7 +292,7 @@ impl SerialManager {
                             }
                         };
 
-                        if mode != ConsoleOutputMode::Socket && num_events == 0 {
+                        if matches!(in_file, ConsoleOutput::Socket(_)) && num_events == 0 {
                             // This very specific case happens when the serial is connected
                             // to a PTY. We know EPOLLHUP is always present when there's nothing
                             // connected at the other end of the PTY. That's why getting no event
@@ -338,6 +316,11 @@ impl SerialManager {
                                             .shutdown(Shutdown::Both)
                                             .map_err(Error::AcceptConnection)?;
                                     }
+
+                                    let ConsoleOutput::Socket(ref listener) = in_file else {
+                                        unreachable!();
+                                    };
+
                                     // Events on the listening socket will be connection requests.
                                     // Accept them, create a reader and a writer.
                                     let (unix_stream, _) =
@@ -363,8 +346,8 @@ impl SerialManager {
                                 EpollDispatch::File => {
                                     if event.events & libc::EPOLLIN as u32 != 0 {
                                         let mut input = [0u8; 64];
-                                        let count = match mode {
-                                            ConsoleOutputMode::Socket => {
+                                        let count = match &in_file {
+                                            ConsoleOutput::Socket(_) => {
                                                 if let Some(mut serial_reader) = reader.as_ref() {
                                                     let count = serial_reader
                                                         .read(&mut input)
@@ -386,9 +369,12 @@ impl SerialManager {
                                                     0
                                                 }
                                             }
-                                            _ => in_file
-                                                .read(&mut input)
-                                                .map_err(Error::ReadInput)?,
+                                            ConsoleOutput::Pty(file) | ConsoleOutput::Tty(file) => {
+                                                (&**file)
+                                                    .read(&mut input)
+                                                    .map_err(Error::ReadInput)?
+                                            }
+                                            _ => unreachable!(),
                                         };
 
                                         // Replace "\n" with "\r" to deal with Windows SAC (#1170)
@@ -444,7 +430,7 @@ impl Drop for SerialManager {
         if let Some(handle) = self.handle.take() {
             handle.join().ok();
         }
-        if self.mode == ConsoleOutputMode::Socket {
+        if let ConsoleOutput::Socket(_) = self.in_file {
             if let Some(socket_path) = self.socket_path.as_ref() {
                 std::fs::remove_file(socket_path.as_os_str())
                     .map_err(Error::RemoveUnixSocket)

virtio-devices/src/console.rs
@@ -108,10 +108,11 @@ struct ConsoleEpollHandler {
     file_event_registered: bool,
 }
 
+#[derive(Clone)]
 pub enum Endpoint {
-    File(File),
-    FilePair(File, File),
-    PtyPair(File, File),
+    File(Arc<File>),
+    FilePair(Arc<File>, Arc<File>),
+    PtyPair(Arc<File>, Arc<File>),
     Null,
 }
 
@@ -136,21 +137,6 @@ impl Endpoint {
 
     fn is_pty(&self) -> bool {
         matches!(self, Self::PtyPair(_, _))
-    }
-}
-
-impl Clone for Endpoint {
-    fn clone(&self) -> Self {
-        match self {
-            Self::File(f) => Self::File(f.try_clone().unwrap()),
-            Self::FilePair(f_out, f_in) => {
-                Self::FilePair(f_out.try_clone().unwrap(), f_in.try_clone().unwrap())
-            }
-            Self::PtyPair(f_out, f_in) => {
-                Self::PtyPair(f_out.try_clone().unwrap(), f_in.try_clone().unwrap())
-            }
-            Self::Null => Self::Null,
-        }
     }
 }
 

vmm/src/console_devices.rs
@@ -23,12 +23,9 @@ use std::fs::read_link;
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::io;
-#[cfg(target_arch = "x86_64")]
-use std::io::stdout;
 use std::mem::zeroed;
 use std::os::fd::AsRawFd;
 use std::os::fd::FromRawFd;
-use std::os::fd::IntoRawFd;
 use std::os::fd::RawFd;
 use std::os::unix::fs::OpenOptionsExt;
 use std::os::unix::net::UnixListener;
@@ -48,6 +45,10 @@ pub enum ConsoleDeviceError {
     #[error("Error creating console device: {0}")]
     CreateConsoleDevice(#[source] io::Error),
 
+    /// No socket option support for console device
+    #[error("No socket option support for console device")]
+    NoSocketOptionSupportForConsoleDevice,
+
     /// Error setting pty raw mode
     #[error("Error setting pty raw mode: {0}")]
     SetPtyRaw(#[source] vmm_sys_util::errno::Error),
@@ -63,14 +64,22 @@ pub enum ConsoleDeviceError {
 
 type ConsoleDeviceResult<T> = result::Result<T, ConsoleDeviceError>;
 
-#[derive(Default, Clone)]
+#[derive(Clone)]
+pub enum ConsoleOutput {
+    File(Arc<File>),
+    Pty(Arc<File>),
+    Tty(Arc<File>),
+    Null,
+    Socket(Arc<UnixListener>),
+    Off,
+}
+
+#[derive(Clone)]
 pub struct ConsoleInfo {
-    // For each of File, Pty, Tty and Socket modes, below fields  hold the FD
-    // of console, serial and debug devices.
-    pub console_main_fd: Option<RawFd>,
-    pub serial_main_fd: Option<RawFd>,
+    pub console_main_fd: ConsoleOutput,
+    pub serial_main_fd: ConsoleOutput,
     #[cfg(target_arch = "x86_64")]
-    pub debug_main_fd: Option<RawFd>,
+    pub debug_main_fd: ConsoleOutput,
 }
 
 fn modify_mode<F: FnOnce(&mut termios)>(
@@ -179,123 +188,133 @@ fn dup_stdout() -> vmm_sys_util::errno::Result<File> {
 pub(crate) fn pre_create_console_devices(vmm: &mut Vmm) -> ConsoleDeviceResult<ConsoleInfo> {
     let vm_config = vmm.vm_config.as_mut().unwrap().clone();
     let mut vmconfig = vm_config.lock().unwrap();
-    let mut console_info = ConsoleInfo::default();
 
-    match vmconfig.console.mode {
-        ConsoleOutputMode::File => {
-            let file = File::create(vmconfig.console.file.as_ref().unwrap())
-                .map_err(ConsoleDeviceError::CreateConsoleDevice)?;
-            console_info.console_main_fd = Some(file.into_raw_fd());
-        }
-        ConsoleOutputMode::Pty => {
-            let (main_fd, sub_fd, path) =
-                create_pty().map_err(ConsoleDeviceError::CreateConsoleDevice)?;
-            console_info.console_main_fd = Some(main_fd.into_raw_fd());
-            set_raw_mode(&sub_fd.as_raw_fd(), vmm.original_termios_opt.clone())?;
-            vmconfig.console.file = Some(path.clone());
-            vmm.console_resize_pipe = Some(Arc::new(
-                listen_for_sigwinch_on_tty(
-                    sub_fd,
-                    &vmm.seccomp_action,
-                    vmm.hypervisor.hypervisor_type(),
-                )
-                .map_err(ConsoleDeviceError::StartSigwinchListener)?,
-            ));
-        }
-        ConsoleOutputMode::Tty => {
-            // Duplicating the file descriptors like this is needed as otherwise
-            // they will be closed on a reboot and the numbers reused
-
-            let stdout = dup_stdout().map_err(ConsoleDeviceError::DupFd)?;
-
-            // SAFETY: FFI call. Trivially safe.
-            if unsafe { libc::isatty(stdout.as_raw_fd()) } == 1 {
+    let console_info = ConsoleInfo {
+        console_main_fd: match vmconfig.console.mode {
+            ConsoleOutputMode::File => {
+                let file = File::create(vmconfig.console.file.as_ref().unwrap())
+                    .map_err(ConsoleDeviceError::CreateConsoleDevice)?;
+                ConsoleOutput::File(Arc::new(file))
+            }
+            ConsoleOutputMode::Pty => {
+                let (main_fd, sub_fd, path) =
+                    create_pty().map_err(ConsoleDeviceError::CreateConsoleDevice)?;
+                set_raw_mode(&sub_fd.as_raw_fd(), vmm.original_termios_opt.clone())?;
+                vmconfig.console.file = Some(path.clone());
                 vmm.console_resize_pipe = Some(Arc::new(
                     listen_for_sigwinch_on_tty(
-                        stdout.try_clone().unwrap(),
+                        sub_fd,
                         &vmm.seccomp_action,
                         vmm.hypervisor.hypervisor_type(),
                     )
                     .map_err(ConsoleDeviceError::StartSigwinchListener)?,
                 ));
+                ConsoleOutput::Pty(Arc::new(main_fd))
             }
+            ConsoleOutputMode::Tty => {
+                // Duplicating the file descriptors like this is needed as otherwise
+                // they will be closed on a reboot and the numbers reused
 
-            // Make sure stdout is in raw mode, if it's a terminal.
-            set_raw_mode(&stdout, vmm.original_termios_opt.clone())?;
-            console_info.console_main_fd = Some(stdout.into_raw_fd());
-        }
-        ConsoleOutputMode::Null | ConsoleOutputMode::Socket | ConsoleOutputMode::Off => {}
-    }
+                let stdout = dup_stdout().map_err(ConsoleDeviceError::DupFd)?;
 
-    match vmconfig.serial.mode {
-        ConsoleOutputMode::File => {
-            let file = File::create(vmconfig.serial.file.as_ref().unwrap())
-                .map_err(ConsoleDeviceError::CreateConsoleDevice)?;
-            console_info.serial_main_fd = Some(file.into_raw_fd());
-        }
-        ConsoleOutputMode::Pty => {
-            let (main_fd, sub_fd, path) =
-                create_pty().map_err(ConsoleDeviceError::CreateConsoleDevice)?;
-            console_info.serial_main_fd = Some(main_fd.into_raw_fd());
-            set_raw_mode(&sub_fd.as_raw_fd(), vmm.original_termios_opt.clone())?;
-            vmconfig.serial.file = Some(path.clone());
-        }
-        ConsoleOutputMode::Tty => {
-            // During vm_shutdown, when serial device is closed, FD#2(STDOUT)
-            // will be closed and FD#2 could be reused in a future boot of the
-            // guest by a different file.
-            //
-            // To ensure FD#2 always points to STANDARD OUT, a `dup` of STDOUT
-            // is passed to serial device. Doing so, even if the serial device
-            // were to be closed, FD#2 will continue to point to STANDARD OUT.
+                // SAFETY: FFI call. Trivially safe.
+                if unsafe { libc::isatty(stdout.as_raw_fd()) } == 1 {
+                    vmm.console_resize_pipe = Some(Arc::new(
+                        listen_for_sigwinch_on_tty(
+                            stdout.try_clone().unwrap(),
+                            &vmm.seccomp_action,
+                            vmm.hypervisor.hypervisor_type(),
+                        )
+                        .map_err(ConsoleDeviceError::StartSigwinchListener)?,
+                    ));
+                }
 
-            let stdout = dup_stdout().map_err(ConsoleDeviceError::DupFd)?;
-
-            // SAFETY: FFI call. Trivially safe.
-            if unsafe { libc::isatty(stdout.as_raw_fd()) } == 1 {
-                vmm.console_resize_pipe = Some(Arc::new(
-                    listen_for_sigwinch_on_tty(
-                        stdout.try_clone().unwrap(),
-                        &vmm.seccomp_action,
-                        vmm.hypervisor.hypervisor_type(),
-                    )
-                    .map_err(ConsoleDeviceError::StartSigwinchListener)?,
-                ));
+                // Make sure stdout is in raw mode, if it's a terminal.
+                set_raw_mode(&stdout, vmm.original_termios_opt.clone())?;
+                ConsoleOutput::Tty(Arc::new(stdout))
             }
+            ConsoleOutputMode::Socket => {
+                return Err(ConsoleDeviceError::NoSocketOptionSupportForConsoleDevice)
+            }
+            ConsoleOutputMode::Null => ConsoleOutput::Null,
+            ConsoleOutputMode::Off => ConsoleOutput::Off,
+        },
+        serial_main_fd: match vmconfig.serial.mode {
+            ConsoleOutputMode::File => {
+                let file = File::create(vmconfig.serial.file.as_ref().unwrap())
+                    .map_err(ConsoleDeviceError::CreateConsoleDevice)?;
+                ConsoleOutput::File(Arc::new(file))
+            }
+            ConsoleOutputMode::Pty => {
+                let (main_fd, sub_fd, path) =
+                    create_pty().map_err(ConsoleDeviceError::CreateConsoleDevice)?;
+                set_raw_mode(&sub_fd.as_raw_fd(), vmm.original_termios_opt.clone())?;
+                vmconfig.serial.file = Some(path.clone());
+                ConsoleOutput::Pty(Arc::new(main_fd))
+            }
+            ConsoleOutputMode::Tty => {
+                // During vm_shutdown, when serial device is closed, FD#2(STDOUT)
+                // will be closed and FD#2 could be reused in a future boot of the
+                // guest by a different file.
+                //
+                // To ensure FD#2 always points to STANDARD OUT, a `dup` of STDOUT
+                // is passed to serial device. Doing so, even if the serial device
+                // were to be closed, FD#2 will continue to point to STANDARD OUT.
 
-            // Make sure stdout is in raw mode, if it's a terminal.
-            set_raw_mode(&stdout, vmm.original_termios_opt.clone())?;
-            console_info.serial_main_fd = Some(stdout.into_raw_fd());
-        }
-        ConsoleOutputMode::Socket => {
-            let listener = UnixListener::bind(vmconfig.serial.socket.as_ref().unwrap())
-                .map_err(ConsoleDeviceError::CreateConsoleDevice)?;
-            console_info.serial_main_fd = Some(listener.into_raw_fd());
-        }
-        ConsoleOutputMode::Null | ConsoleOutputMode::Off => {}
-    }
+                let stdout = dup_stdout().map_err(ConsoleDeviceError::DupFd)?;
 
-    #[cfg(target_arch = "x86_64")]
-    match vmconfig.debug_console.mode {
-        ConsoleOutputMode::File => {
-            let file = File::create(vmconfig.debug_console.file.as_ref().unwrap())
-                .map_err(ConsoleDeviceError::CreateConsoleDevice)?;
-            console_info.debug_main_fd = Some(file.into_raw_fd());
-        }
-        ConsoleOutputMode::Pty => {
-            let (main_fd, sub_fd, path) =
-                create_pty().map_err(ConsoleDeviceError::CreateConsoleDevice)?;
-            console_info.debug_main_fd = Some(main_fd.into_raw_fd());
-            set_raw_mode(&sub_fd.as_raw_fd(), vmm.original_termios_opt.clone())?;
-            vmconfig.debug_console.file = Some(path.clone());
-        }
-        ConsoleOutputMode::Tty => {
-            let out = stdout();
-            console_info.debug_main_fd = Some(out.as_raw_fd());
-            set_raw_mode(&out, vmm.original_termios_opt.clone())?;
-        }
-        ConsoleOutputMode::Null | ConsoleOutputMode::Socket | ConsoleOutputMode::Off => {}
-    }
+                // SAFETY: FFI call. Trivially safe.
+                if unsafe { libc::isatty(stdout.as_raw_fd()) } == 1 {
+                    vmm.console_resize_pipe = Some(Arc::new(
+                        listen_for_sigwinch_on_tty(
+                            stdout.try_clone().unwrap(),
+                            &vmm.seccomp_action,
+                            vmm.hypervisor.hypervisor_type(),
+                        )
+                        .map_err(ConsoleDeviceError::StartSigwinchListener)?,
+                    ));
+                }
+
+                // Make sure stdout is in raw mode, if it's a terminal.
+                set_raw_mode(&stdout, vmm.original_termios_opt.clone())?;
+
+                ConsoleOutput::Tty(Arc::new(stdout))
+            }
+            ConsoleOutputMode::Socket => {
+                let listener = UnixListener::bind(vmconfig.serial.socket.as_ref().unwrap())
+                    .map_err(ConsoleDeviceError::CreateConsoleDevice)?;
+                ConsoleOutput::Socket(Arc::new(listener))
+            }
+            ConsoleOutputMode::Null => ConsoleOutput::Null,
+            ConsoleOutputMode::Off => ConsoleOutput::Off,
+        },
+        #[cfg(target_arch = "x86_64")]
+        debug_main_fd: match vmconfig.debug_console.mode {
+            ConsoleOutputMode::File => {
+                let file = File::create(vmconfig.debug_console.file.as_ref().unwrap())
+                    .map_err(ConsoleDeviceError::CreateConsoleDevice)?;
+                ConsoleOutput::File(Arc::new(file))
+            }
+            ConsoleOutputMode::Pty => {
+                let (main_fd, sub_fd, path) =
+                    create_pty().map_err(ConsoleDeviceError::CreateConsoleDevice)?;
+                set_raw_mode(&sub_fd.as_raw_fd(), vmm.original_termios_opt.clone())?;
+                vmconfig.debug_console.file = Some(path.clone());
+                ConsoleOutput::Pty(Arc::new(main_fd))
+            }
+            ConsoleOutputMode::Tty => {
+                let out =
+                    dup_stdout().map_err(|e| ConsoleDeviceError::CreateConsoleDevice(e.into()))?;
+                set_raw_mode(&out, vmm.original_termios_opt.clone())?;
+                ConsoleOutput::Tty(Arc::new(out))
+            }
+            ConsoleOutputMode::Socket => {
+                return Err(ConsoleDeviceError::NoSocketOptionSupportForConsoleDevice)
+            }
+            ConsoleOutputMode::Null => ConsoleOutput::Null,
+            ConsoleOutputMode::Off => ConsoleOutput::Off,
+        },
+    };
 
     Ok(console_info)
 }

commit 1e8975940d70bb1d7b0c81bf1b437d09a96e8c87

vmm: fix the initial flush while using PTY
Fix the condition to correctly identify a new PTY connection.

Fixes: 287887c99 (vmm: fix console IO safety)
Signed-off-by: Praveen K Paladugu <prapal@linux.microsoft.com>

vmm/src/serial_manager.rs
@@ -294,7 +294,7 @@ impl SerialManager {
                             }
                         };
 
-                        if matches!(in_file, ConsoleOutput::Socket(_)) && num_events == 0 {
+                        if matches!(in_file, ConsoleOutput::Pty(_)) && num_events == 0 {
                             // This very specific case happens when the serial is connected
                             // to a PTY. We know EPOLLHUP is always present when there's nothing
                             // connected at the other end of the PTY. That's why getting no event