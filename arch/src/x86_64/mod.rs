dae1fa16cb8eed2992bdc7bff9e2a00f42706bf3
Oliver Anderson <oliver.anderson@cyberus-technology.de>
2025-12-10 16:15:45 +0100

    cpu-profile-generation: CPU profile generation CLI
    This commit introduces a CLI for generating a CPU profile closely
    matching the CPU of the machine the CLI is executed on. The idea is to
    have a simple way to add more CPU profiles corresponding to physical
    CPUs.
    
    Note however that with the current setup one still needs a little bit
    of manual work to integrate the generated CPU profile data into cloud
    hypervisor itself.
    
    Signed-off-by: Oliver Anderson <oliver.anderson@cyberus-technology.de>
    On-behalf-of: SAP oliver.anderson@sap.com

--- arch/src/x86_64/mod.rs
+++ arch/src/x86_64/mod.rs
@@ -7,6 +7,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE-BSD-3-Clause file.
 pub mod cpu_profile;
+#[cfg(feature = "cpu_profile_generation")]
+pub mod cpu_profile_generation;
 pub mod cpuid_definitions;
 pub mod interrupts;
 pub mod layout;
