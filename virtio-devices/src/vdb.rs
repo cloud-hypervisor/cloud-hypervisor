4e965879a9d5a031e39c04ee1ab0030cb0746397
Demi Marie Obenour <demiobenour@gmail.com>
2026-02-15 16:56:32 -0500

    virtio-devices: vdb: Support config space access
    This allows drivers to access the configuration space.
    
    Signed-off-by: Demi Marie Obenour <demiobenour@gmail.com>

--- virtio-devices/src/vdb.rs
+++ virtio-devices/src/vdb.rs
@@ -15,6 +15,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// This implements a vhost-user device backend.  Documentation can be found at:
+// https://stefanha.github.io/virtio/vhost-user-slave.html
+
 use std::io::{self};
 use std::os::unix::io::AsRawFd;
 use std::result;
@@ -23,12 +26,11 @@ use std::sync::{Arc, Barrier};
 
 use anyhow::anyhow;
 use event_monitor::event;
-use log::{error, info};
+use log::{error, info, warn};
 use seccompiler::SeccompAction;
-use serde::{Deserialize, Serialize};
 use thiserror::Error;
 use virtio_queue::{Queue, QueueT};
-use vm_memory::{GuestAddressSpace, GuestMemoryAtomic, GuestMemoryError};
+use vm_memory::{ByteValued, GuestAddressSpace, GuestMemoryAtomic, GuestMemoryError, Le32};
 use vm_migration::{Migratable, MigratableError, Pausable, Snapshot, Snapshottable, Transportable};
 use vmm_sys_util::eventfd::EventFd;
 
@@ -40,8 +42,71 @@ use crate::{
     VirtioInterrupt, VirtioInterruptType,
 };
 
+#[allow(unused)]
+/// Not a valid device backend type.
+const VIRTIO_DEVICE_BACKEND_TYPE_INVALID: u32 = 0;
+
+/// vhost-user device backend
+const VIRTIO_DEVICE_BACKEND_TYPE_VHOST_USER: u32 = 1;
+
+/// Backend is not yet ready.
+const VIRTIO_DEVICE_BACKEND_STATUS_DOWN: u32 = 0;
+
+/// Backend is ready.
+const VIRTIO_DEVICE_BACKEND_STATUS_UP: u32 = 1;
+
+/// Common virtio-device-backend configuration space fields.
+#[derive(Copy, Clone)]
+#[repr(C, packed)]
+pub struct VirtioDeviceBackendConfigCommon {
+    /// The type of the device.  Must be 1 for a vhost-user device.
+    pub device_type: Le32,
+    /// The status of the device.  Always 0 at startup.  Set to 1 when ready.
+    pub status: Le32,
+    /// A UUID for the backend.
+    pub uuid: [u8; 16],
+}
+
+// Le32 should be #[repr(transparent)] but isn't.  However,
+// it has a single field, and that field is a u32.  Furthermore,
+// it has exactly 2^32 valid values.  Therefore, there is no way
+// that this can be represented as anything other than a u32 by the
+// Pidgeonhole Principle.  Otherwise field access won't work.
+const _: () = assert!(size_of::<Le32>() == size_of::<u32>());
+const _: () = assert!(
+    size_of::<VirtioDeviceBackendConfigCommon>() == size_of::<Le32>() * 2 + size_of::<[u8; 16]>()
+);
+
+/// Configuration space fields that are specific to vhost-user device backends.
+#[derive(Copy, Clone)]
+#[repr(transparent)]
+pub struct VirtioDeviceBackendConfigVhostUser {
+    /// The maximum number of vhost-user queues that are supported.
+    pub max_queues: Le32,
+}
+
+const _: () = assert!(size_of::<VirtioDeviceBackendConfigVhostUser>() == size_of::<Le32>());
+
+#[derive(Copy, Clone)]
+#[repr(C, packed)]
+pub struct VirtioDeviceBackendConfig {
+    pub common: VirtioDeviceBackendConfigCommon,
+    pub vhost_user: VirtioDeviceBackendConfigVhostUser,
+}
+
+const _: () = assert!(
+    size_of::<VirtioDeviceBackendConfig>()
+        == size_of::<VirtioDeviceBackendConfigCommon>()
+            + size_of::<VirtioDeviceBackendConfigVhostUser>()
+);
+
+// SAFETY: The above static assertions check that the size
+// is exactly the minimum needed to hold all possible values.
+// Therefore, there cannot be any padding or invalid values.
+unsafe impl ByteValued for VirtioDeviceBackendConfig {}
+
 const QUEUE_SIZE: u16 = 128;
-const MIN_NUM_QUEUES: usize = 2;
+const NUM_QUEUES: usize = 2;
 
 // Inflate virtio queue event.
 const FRONT2BACK_QUEUE_EVENT: u16 = EPOLL_HELPER_EVENT_LAST + 1;
@@ -108,19 +173,18 @@ impl VdbEpollHandler {
         }
     }
 
-    #[allow(unused)]
     fn process_back2front_queue(&mut self) -> result::Result<(), Error> {
-        let mut used_descs = false;
+        let mut _used_descs = false;
         while let Some(mut desc_chain) = self
             .back2front_queue
             .pop_descriptor_chain(self.mem.memory())
         {
-            let desc = desc_chain.next().ok_or(Error::DescriptorChainTooShort)?;
-            used_descs = true;
+            let _desc = desc_chain.next().ok_or(Error::DescriptorChainTooShort)?;
+            _used_descs = true;
             todo!();
         }
 
-        if used_descs {
+        if _used_descs {
             self.signal(VirtioInterruptType::Queue(1))
         } else {
             Ok(())
@@ -189,21 +253,25 @@ impl EpollHelperHandler for VdbEpollHandler {
     }
 }
 
-#[derive(Serialize, Deserialize, Copy, Clone)]
-pub struct VirtioVdbConfig;
-
-#[derive(Serialize, Deserialize)]
+#[derive(Copy, Clone)]
+#[repr(packed, C)]
 pub struct VdbState {
     pub avail_features: u64,
     pub acked_features: u64,
-    pub config: VirtioVdbConfig,
+    pub config: VirtioDeviceBackendConfig,
 }
 
-// Virtio device for exposing entropy to the guest OS through virtio.
+const _: () =
+    assert!(size_of::<VdbState>() == size_of::<u64>() * 2 + size_of::<VirtioDeviceBackendConfig>());
+
+// SAFETY: VdbState has no padding and all values are valid.
+unsafe impl ByteValued for VdbState {}
+
+// Virtio device backend
 pub struct Vdb {
     common: VirtioCommon,
     id: String,
-    config: VirtioVdbConfig,
+    config: VirtioDeviceBackendConfig,
     seccomp_action: SeccompAction,
     exit_evt: EventFd,
     interrupt_cb: Option<Arc<dyn VirtioInterrupt>>,
@@ -216,8 +284,10 @@ impl Vdb {
         seccomp_action: SeccompAction,
         exit_evt: EventFd,
         state: Option<VdbState>,
+        max_queues: u32,
+        uuid: [u8; 16],
     ) -> io::Result<Self> {
-        let queue_sizes = vec![QUEUE_SIZE; MIN_NUM_QUEUES];
+        let queue_sizes = vec![QUEUE_SIZE; NUM_QUEUES];
 
         let (avail_features, acked_features, config, paused) = if let Some(state) = state {
             info!("Restoring virtio-balloon {id}");
@@ -230,9 +300,21 @@ impl Vdb {
         } else {
             let avail_features = 1u64 << VIRTIO_F_VERSION_1;
 
-            let config = VirtioVdbConfig;
-
-            (avail_features, 0, config, false)
+            (
+                avail_features,
+                0,
+                VirtioDeviceBackendConfig {
+                    common: VirtioDeviceBackendConfigCommon {
+                        device_type: Le32::from(VIRTIO_DEVICE_BACKEND_TYPE_VHOST_USER),
+                        status: Le32::from(VIRTIO_DEVICE_BACKEND_STATUS_DOWN),
+                        uuid,
+                    },
+                    vhost_user: VirtioDeviceBackendConfigVhostUser {
+                        max_queues: Le32::from(max_queues),
+                    },
+                },
+                false,
+            )
         };
 
         Ok(Vdb {
@@ -242,7 +324,7 @@ impl Vdb {
                 acked_features,
                 paused_sync: Some(Arc::new(Barrier::new(2))),
                 queue_sizes,
-                min_queues: MIN_NUM_QUEUES as u16,
+                min_queues: NUM_QUEUES as u16,
                 paused: Arc::new(AtomicBool::new(paused)),
                 ..Default::default()
             },
@@ -295,12 +377,22 @@ impl VirtioDevice for Vdb {
         self.common.ack_features(value);
     }
 
-    fn read_config(&self, _offset: u64, _data: &mut [u8]) {
-        todo!()
-    }
+    fn read_config(&self, _offset: u64, _data: &mut [u8]) {}
 
-    fn write_config(&mut self, _offset: u64, _data: &[u8]) {
-        todo!()
+    fn write_config(&mut self, offset: u64, data: &[u8]) {
+        if offset != 4 {
+            warn!("Driver attempted to write to invalid field");
+            return;
+        }
+        match *data {
+            [1, 0, 0, 0] | [1, 0] | [1] => {
+                self.config.common.status = Le32::from(VIRTIO_DEVICE_BACKEND_STATUS_UP);
+            }
+            [0, 0, 0, 0] | [0, 0] | [0] => {
+                self.config.common.status = Le32::from(VIRTIO_DEVICE_BACKEND_STATUS_DOWN);
+            }
+            _ => warn!("Invalid config space write"),
+        }
     }
 
     fn activate(
@@ -369,7 +461,7 @@ impl Snapshottable for Vdb {
     }
 
     fn snapshot(&mut self) -> std::result::Result<Snapshot, MigratableError> {
-        Snapshot::new_from_state(&self.state())
+        Snapshot::new_from_state(&self.state().as_slice())
     }
 }
 impl Transportable for Vdb {}
