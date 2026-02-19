5520fcbe0f667d586946cdf50e3249c24b11ec57
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-02-19 15:37:21 +0100

    vmm: api/http: add VmCancelMigration action
    The new endpoint will enable management software to cancel migrations.
    This is typically the case for:
    - long-running migrations that do not converge
    - long-running migrations where the destination is no longer the best
      new location
    - possibly to clean up a stuck (buggy) migration
    
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- vmm/src/api/http/http_endpoint.rs
+++ vmm/src/api/http/http_endpoint.rs
@@ -51,7 +51,7 @@ use crate::api::http::{EndpointHandler, HttpError, error_response};
 use crate::api::{
     AddDisk, ApiAction, ApiError, ApiRequest, DeviceConfig, NetConfig, VmAddDevice, VmAddFs,
     VmAddGenericVhostUser, VmAddNet, VmAddPmem, VmAddUserDevice, VmAddVdpa, VmAddVsock, VmBoot,
-    VmConfig, VmCounters, VmDelete, VmNmi, VmPause, VmPowerButton, VmReboot, VmReceiveMigration,
+    VmCancelMigration, VmConfig, VmCounters, VmDelete, VmNmi, VmPause, VmPowerButton, VmReboot, VmReceiveMigration,
     VmReceiveMigrationData, VmRemoveDevice, VmResize, VmResizeDisk, VmResizeZone, VmRestore,
     VmResume, VmSendMigration, VmShutdown, VmSnapshot,
 };
@@ -472,6 +472,7 @@ vm_action_put_handler!(VmPause);
 vm_action_put_handler!(VmResume);
 vm_action_put_handler!(VmPowerButton);
 vm_action_put_handler!(VmNmi);
+vm_action_put_handler!(VmCancelMigration);
 
 vm_action_put_handler_body!(AddDisk);
 vm_action_put_handler_body!(VmAddFs);
