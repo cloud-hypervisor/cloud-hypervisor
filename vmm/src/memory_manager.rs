5aed8346b7ef7152182d5b675531d2ef7c74c3e5
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-03-06 13:28:59 +0100

    vmm: silence some spammy warnings
    They will also have little value with the upcoming changes + logging
    per iteration.
    
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>
    On-behalf-of: SAP philipp.schuster@sap.com

--- vmm/src/memory_manager.rs
+++ vmm/src/memory_manager.rs
@@ -25,7 +25,7 @@ use devices::ioapic;
 #[cfg(target_arch = "aarch64")]
 use hypervisor::HypervisorVmError;
 use libc::_SC_NPROCESSORS_ONLN;
-use log::{debug, error, info, warn};
+use log::{debug, error, info, trace, warn};
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 use tracer::trace_scoped;
@@ -2673,11 +2673,11 @@ impl Migratable for MemoryManager {
             let sub_table = MemoryRangeTable::from_dirty_bitmap(dirty_bitmap, r.gpa, 4096);
 
             if sub_table.regions().is_empty() {
-                info!("Dirty Memory Range Table is empty");
+                trace!("Dirty Memory Range Table is empty");
             } else {
-                info!("Dirty Memory Range Table:");
+                trace!("Dirty Memory Range Table:");
                 for range in sub_table.regions() {
-                    info!("GPA: {:x} size: {} (KiB)", range.gpa, range.length / 1024);
+                    trace!("GPA: {:x} size: {} (KiB)", range.gpa, range.length / 1024);
                 }
             }
 
