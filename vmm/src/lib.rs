4c59507ba7f2b359547bda8ddc823de6a8b664cb
Sebastian Eydam <sebastian.eydam@cyberus-technology.de>
2026-03-09 16:19:41 +0100

    vmm: extract send_memory_regions from vm
    On-behalf-of: SAP sebastian.eydam@sap.com
    Signed-off-by: Sebastian Eydam <sebastian.eydam@cyberus-technology.de>

--- vmm/src/lib.rs
+++ vmm/src/lib.rs
@@ -1184,7 +1184,7 @@ impl Vmm {
 
             // Send the current dirty pages
             let transfer_begin = Instant::now();
-            migration_transport::vm_send_dirty_pages(vm, socket, &iteration_table)?;
+            migration_transport::send_memory_regions(&vm.guest_memory(), &iteration_table, socket)?;
             let transfer_duration = transfer_begin.elapsed();
             ctx.update_metrics_after_transfer(transfer_begin, transfer_duration);
 
@@ -1236,7 +1236,7 @@ impl Vmm {
 
             ctx.update_metrics_before_transfer(iteration_begin, &final_table);
             let transfer_begin = Instant::now();
-            migration_transport::vm_send_dirty_pages(vm, socket, &final_table)?;
+            migration_transport::send_memory_regions(&vm.guest_memory(), &final_table, socket)?;
             let transfer_duration = transfer_begin.elapsed();
             ctx.update_metrics_after_transfer(transfer_begin, transfer_duration);
             ctx.iteration += 1;
