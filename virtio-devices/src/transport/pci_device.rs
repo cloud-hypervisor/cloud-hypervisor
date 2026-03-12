7f76c50bf5ac5ead0397b8cdc7c835e98016477c
Demi Marie Obenour <demiobenour@gmail.com>
2026-03-11 20:59:38 -0400

    virtio-devices: Inform PCI devices that they have been moved
    Signed-off-by: Demi Marie Obenour <demiobenour@gmail.com>

--- virtio-devices/src/transport/pci_device.rs
+++ virtio-devices/src/transport/pci_device.rs
@@ -642,6 +642,12 @@ impl VirtioPciDevice {
             )));
         }
 
+        virtio_pci_device
+            .device
+            .lock()
+            .unwrap()
+            .set_config_address_base(virtio_pci_device.config_bar_addr());
+
         // In case of a restore, we can activate the device, as we know at
         // this point the virtqueues are in the right state and the device is
         // ready to be activated, which will spawn each virtio worker thread.
@@ -1146,12 +1152,20 @@ impl PciDevice for VirtioPciDevice {
         old_base: u64,
         new_base: u64,
     ) -> std::result::Result<(), std::io::Error> {
+        let move_config_base = old_base == self.config_bar_addr();
         // We only update our idea of the bar in order to support free_bars() above.
         // The majority of the reallocation is done inside DeviceManager.
         for bar in self.bar_regions.iter_mut() {
             if bar.addr() == old_base {
                 *bar = bar.set_address(new_base);
             }
+        }
+
+        if move_config_base {
+            self.device
+                .lock()
+                .unwrap()
+                .set_config_address_base(new_base);
         }
 
         Ok(())
