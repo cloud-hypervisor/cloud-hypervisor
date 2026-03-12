63f5e6e97f781563679b271df2da6f743f1b7794
Anatol Belski <anbelski@linux.microsoft.com>
2026-03-12 20:59:46 +0100

    block: qcow_sync: impl Geometry for QcowDiskSync
    Uses the default DiskTopology (512B logical/physical) since
    QCOW2 does not probe the underlying device geometry.
    
    Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>

--- block/src/qcow_sync.rs
+++ block/src/qcow_sync.rs
@@ -296,6 +296,8 @@ impl disk_file::DiskFd for QcowDiskSync {
     }
 }
 
+impl disk_file::Geometry for QcowDiskSync {}
+
 pub struct QcowSync {
     metadata: Arc<QcowMetadata>,
     data_file: QcowRawFile,
