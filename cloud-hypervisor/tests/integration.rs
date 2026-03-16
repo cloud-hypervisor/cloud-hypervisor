e257d53dabcfe874f49d6e2e36cffe752db68877
Pascal Scholz <pascal.scholz@cyberus-technology.de>
2026-03-16 10:32:56 +0100

    tests: Add an integration test to verify errors for an invalid BDF request
    This integration test verifies that a requested BDF must be valid (i.e.,
    less than 32) and that requesting a BDF with a function ID results in an
    error. In both cases, we check for the correct error log.
    
    Signed-off-by: Pascal Scholz <pascal.scholz@cyberus-technology.de>
    On-behalf-of: SAP pascal.scholz@sap.com

--- cloud-hypervisor/tests/integration.rs
+++ cloud-hypervisor/tests/integration.rs
@@ -10370,6 +10370,87 @@ mod common_parallel {
 
         handle_child_output(r, &output);
     }
+
+    #[test]
+    // Test that requesting an invalid device ID fails.
+    fn test_bdf_request_invalid() {
+        let disk_config = UbuntuDiskConfig::new(JAMMY_IMAGE_NAME.to_string());
+        let guest = Guest::new(Box::new(disk_config));
+
+        #[cfg(target_arch = "x86_64")]
+        let kernel_path = direct_kernel_boot_path();
+        #[cfg(target_arch = "aarch64")]
+        let kernel_path = edk2_path();
+
+        let api_socket = temp_api_path(&guest.tmp_dir);
+
+        // Boot without network
+        let mut cmd = GuestCommand::new(&guest);
+
+        cmd.args(["--api-socket", &api_socket])
+            .default_cpus()
+            .default_memory()
+            .args(["--kernel", kernel_path.to_str().unwrap()])
+            .args(["--cmdline", DIRECT_KERNEL_BOOT_CMDLINE])
+            .default_net()
+            .default_disks()
+            .capture_output();
+
+        let mut child = cmd.spawn().unwrap();
+
+        guest.wait_vm_boot().unwrap();
+
+        // Add a network device with non-static BDF request
+        let r = std::panic::catch_unwind(|| {
+            // Invalid API call because the BDF is out of range
+            let (cmd_success, _, cmd_stderr) = remote_command_w_output(
+                &api_socket,
+                "add-net",
+                Some(
+                    format!(
+                        "id=test0,tap=,mac={},ip={},mask=255.255.255.128,addr={:02x}.0",
+                        guest.network.guest_mac1, guest.network.host_ip1, 0xBC,
+                    )
+                    .as_str(),
+                ),
+            );
+            // Check for fail
+            assert!(!cmd_success);
+            // Check that the error message contains the expected error
+            let std_err_str = String::from_utf8(cmd_stderr).unwrap();
+            assert!(
+                std_err_str.contains("invalid value: expected a device ID in the inclusive range of 1 to 31, found 188"),
+                "Command return was: {std_err_str}",
+            );
+
+            // Use a valid device ID but a not supported function ID
+            let (cmd_success, _, cmd_stderr) = remote_command_w_output(
+                &api_socket,
+                "add-net",
+                Some(
+                    format!(
+                        "id=test0,tap=,mac={},ip={},mask=255.255.255.128,addr={:02x}.6",
+                        guest.network.guest_mac1, guest.network.host_ip1, 0x10,
+                    )
+                    .as_str(),
+                ),
+            );
+            // Check for fail
+            assert!(!cmd_success);
+            // Check that the error message contains the expected error
+            let std_err_str = String::from_utf8(cmd_stderr).unwrap();
+            assert!(
+                std_err_str
+                    .contains("multi-function devices currently not supported; expected 0 got 6"),
+                "Command return was: {std_err_str}"
+            );
+        });
+
+        kill_child(&mut child);
+        let output = child.wait_with_output().unwrap();
+
+        handle_child_output(r, &output);
+    }
 }
 
 mod dbus_api {
