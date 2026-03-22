b9c9e7265d1ad19ab72ef1269d353094084a539d
Muminul Islam <muislam@microsoft.com>
2026-03-21 17:35:53 -0700

    tests: use basic_regular_guest macro in test_virtio_vsock
    Replace manual UbuntuDiskConfig and GuestFactory guest
    creation with the basic_regular_guest! macro in
    test_virtio_vsock for consistency and reduced boilerplate.
    
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

--- cloud-hypervisor/tests/integration.rs
+++ cloud-hypervisor/tests/integration.rs
@@ -2724,8 +2724,7 @@ mod common_parallel {
 
     #[test]
     fn test_virtio_vsock() {
-        let disk_config = UbuntuDiskConfig::new(JAMMY_IMAGE_NAME.to_string());
-        let guest = GuestFactory::new_regular_guest_factory().create_guest(Box::new(disk_config));
+        let guest = basic_regular_guest!(JAMMY_IMAGE_NAME);
         _test_virtio_vsock(&guest, false);
     }
 
