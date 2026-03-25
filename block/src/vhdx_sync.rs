d23c128427f787bcddb988802ecd5b836cb62578
Anatol Belski <anbelski@linux.microsoft.com>
2026-03-25 16:41:01 +0100

    block: vhdx: impl Geometry for VhdxDiskSync
    Uses default topology.
    
    Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>

--- block/src/vhdx_sync.rs
+++ block/src/vhdx_sync.rs
@@ -66,6 +66,8 @@ impl disk_file::DiskFd for VhdxDiskSync {
     }
 }
 
+impl disk_file::Geometry for VhdxDiskSync {}
+
 impl DiskFile for VhdxDiskSync {
     fn logical_size(&mut self) -> DiskFileResult<u64> {
         Ok(self.vhdx_file.lock().unwrap().virtual_disk_size())
