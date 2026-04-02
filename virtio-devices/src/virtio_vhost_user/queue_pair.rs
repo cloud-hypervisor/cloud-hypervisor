0dc144ea5ed7f0e9cd9b6f47ff88b5dc12941201
Demi Marie Obenour <demiobenour@gmail.com>
2026-04-02 15:37:08 -0400

    virtio-vhost-user: Add queue pair for message handling
    Virtio-vhost-user uses four virtqueues.  These are grouped into two
    pairs of two queues each.  In each pair, one pair is used for requests
    and the other is used for replies.  Each pair is also associated with an
    AF_UNIX stream socket used for communication with the frontend.
    
    Implement a queue pair abstraction that handles message framing and
    communication with the VM.
    
    Signed-off-by: Demi Marie Obenour <demiobenour@gmail.com>

---
+++ virtio-devices/src/virtio_vhost_user/queue_pair.rs
@@ -0,0 +1,422 @@
+// Copyright (C) 2019 Alibaba Cloud Computing. All rights reserved.
+// Copyright (c) 2020 Ant Financial
+// Copyright (c) 2026 Demi Marie Obenour
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This includes lots of code from the vhost-user crate, but generalized.
+
+use std::ffi::c_void;
+use std::fs::File;
+use std::io::ErrorKind;
+use std::os::fd::{AsRawFd as _, FromRawFd as _, OwnedFd};
+use std::os::unix::net::UnixStream;
+use std::process;
+
+use libc::iovec;
+use log::error;
+use vhost::vhost_user::Error;
+use vhost::vhost_user::message::{MAX_ATTACHED_FD_ENTRIES, MAX_MSG_SIZE};
+use virtio_queue::{Queue, QueueT as _};
+use vm_memory::bitmap::AtomicBitmap;
+use vm_memory::{ByteValued, Bytes as _, GuestAddressSpace, GuestMemoryAtomic, GuestMemoryMmap};
+use vm_virtio::{AccessPlatform, Translatable as _};
+use vmm_sys_util::eventfd::EventFd;
+use vmm_sys_util::sock_ctrl_msg::ScmSocket as _;
+
+use crate::{EpollHelper, EpollHelperError};
+
+// SAFETY: is POD
+unsafe impl ByteValued for VhostUserMsgHeader {}
+#[repr(C, packed)]
+#[derive(Copy, Clone, Default)]
+pub struct VhostUserMsgHeader {
+    pub request: u32,
+    pub flags: u32,
+    pub size: u32,
+}
+
+#[repr(u16)]
+pub(super) enum Events {
+    QueueIn = 0,
+    QueueOut = 1,
+    SocketIn = 2,
+    SocketOut = 3,
+    Total = 4,
+}
+
+pub(super) enum FdRearm {
+    Neither,
+    Socket,
+    Queue,
+}
+
+pub(super) struct VirtioVhostUserQueuePair {
+    front2back_queue: Queue,
+    back2front_queue: Queue,
+    front2back_queue_evt: EventFd,
+    back2front_queue_evt: EventFd,
+    socket: Option<UnixStream>,
+    incoming_data: Vec<u8>,
+    outgoing_buf: Vec<u8>,
+    files_for_cycle: Vec<Option<OwnedFd>>,
+    offset: usize,
+    mem: GuestMemoryAtomic<GuestMemoryMmap<AtomicBitmap>>,
+}
+
+fn validate_hdr(buf: &mut [u8]) -> Result<(VhostUserMsgHeader, &mut [u8]), Error> {
+    let (hdr, body) = buf.split_at_mut(size_of::<VhostUserMsgHeader>());
+    let &hdr = VhostUserMsgHeader::from_slice(hdr).expect("length correct");
+    let version = hdr.flags & 3;
+    if version != 1 {
+        error!("virtio-vhost-user: Bad version {version}");
+        return Err(Error::InvalidMessage);
+    }
+    Ok((hdr, body))
+}
+
+impl VirtioVhostUserQueuePair {
+    pub(super) fn new(
+        front2back_queue: Queue,
+        back2front_queue: Queue,
+        front2back_queue_evt: EventFd,
+        back2front_queue_evt: EventFd,
+        socket: Option<UnixStream>,
+        mem: GuestMemoryAtomic<GuestMemoryMmap<AtomicBitmap>>,
+    ) -> Self {
+        Self {
+            front2back_queue,
+            back2front_queue,
+            front2back_queue_evt,
+            back2front_queue_evt,
+            socket,
+            incoming_data: Vec::new(),
+            outgoing_buf: Vec::new(),
+            files_for_cycle: Vec::new(),
+            offset: 0,
+            mem,
+        }
+    }
+
+    /// Sets the file descriptor to use for the socket.
+    /// Does not close the socket on success
+    /// (so socket.as_raw_fd() is still a valid fd).
+    pub(super) fn set_socket(&mut self, socket: UnixStream) -> Result<(), Error> {
+        if self.socket.is_some() {
+            return Err(Error::InvalidMessage);
+        }
+        self.socket = Some(socket);
+        Ok(())
+    }
+
+    pub(super) fn register_epoll_events(
+        &mut self,
+        helper: &mut EpollHelper,
+        base: u16,
+    ) -> Result<(), EpollHelperError> {
+        helper.add_event(
+            self.front2back_queue_evt.as_raw_fd(),
+            base + Events::QueueIn as u16,
+        )?;
+        helper.add_event(
+            self.back2front_queue_evt.as_raw_fd(),
+            base + Events::QueueOut as u16,
+        )?;
+        if let Some(socket) = &self.socket {
+            helper.add_event(socket.as_raw_fd(), base + Events::SocketIn as u16)?;
+            helper.add_event_custom(
+                socket.as_raw_fd(),
+                base + Events::SocketOut as u16,
+                epoll::Events::EPOLLOUT,
+            )?;
+        }
+        Ok(())
+    }
+
+    /// Send an outgoing message if possible.
+    ///
+    /// Returns true if a full message was successfully sent.
+    ///
+    /// If the function returns true, the buffer and outgoing FD queue
+    /// will be empty.
+    ///
+    /// # Errors
+    ///
+    /// Fails if there is an I/O error on the socket.
+    fn send_message(&mut self) -> Result<bool, Error> {
+        let buf: &[u8] = &self.outgoing_buf;
+        if buf.is_empty() {
+            return Ok(true);
+        }
+        let hdr = VhostUserMsgHeader::from_slice(&buf[..size_of::<VhostUserMsgHeader>()])
+            .expect("length correct");
+        assert_eq!(hdr.flags & 3, 1, "bad version in outgoing message");
+        assert!(hdr.size >= size_of::<VhostUserMsgHeader>() as u32);
+        assert!(hdr.size <= MAX_MSG_SIZE as u32);
+        assert_eq!(hdr.size as usize, buf.len());
+        let buf = &buf[..self.offset];
+        let Some(socket) = &self.socket else {
+            error!(
+                "No socket yet - did the backend place buffers on its request or reply queue without getting an FD from the frontend?"
+            );
+            return Err(Error::FeatureMismatch);
+        };
+        loop {
+            // SAFETY: FFI with valid parameters
+            let v = unsafe {
+                libc::send(
+                    socket.as_raw_fd(),
+                    buf.as_ptr().cast(),
+                    buf.len(),
+                    libc::MSG_NOSIGNAL | libc::MSG_DONTWAIT,
+                )
+            };
+            if v == -1 {
+                let e = std::io::Error::last_os_error();
+                let errno = e.raw_os_error().unwrap();
+                if errno == libc::EAGAIN || errno == libc::EWOULDBLOCK {
+                    break Ok(false);
+                }
+                if errno != libc::EINTR {
+                    break Err(Error::ReqHandlerError(e));
+                }
+            } else {
+                let v: usize = v.try_into().unwrap();
+                if v > buf.len() {
+                    process::abort();
+                }
+                self.offset += v;
+                if self.offset == buf.len() {
+                    self.outgoing_buf.clear();
+                    self.offset = 0;
+                    break Ok(true);
+                }
+            }
+        }
+    }
+
+    /// Send an outgoing message if possible.
+    ///
+    /// On success, the first element of the returned tuple indicates
+    /// which file descriptors need to be polled.  The second element
+    /// indicates whether the queue interrupt needs to be triggered.
+    ///
+    /// The callback will be called for each message sent.  It is allowed
+    /// to modify the message's contents but not its header.  It can
+    /// reject the message by returning an error.
+    ///
+    /// # Errors
+    ///
+    /// Fails if there is an I/O error on the socket or if the callback
+    /// returns an error.
+    #[allow(clippy::type_complexity)] // pulling this out leads to borrowck error
+    pub(super) fn process_outgoing(
+        &mut self,
+        access_platform: Option<&dyn AccessPlatform>,
+        max_messages: usize,
+        process_message: &mut dyn FnMut(VhostUserMsgHeader, &mut [u8]) -> Result<(), Error>,
+    ) -> Result<(FdRearm, bool), Error> {
+        let mut used_descs = false;
+        for _ in 0..max_messages {
+            if !self.send_message()? {
+                return Ok((FdRearm::Socket, used_descs));
+            }
+            let Some(mut desc_chain) = self
+                .back2front_queue
+                .pop_descriptor_chain(self.mem.memory())
+            else {
+                return Ok((FdRearm::Queue, used_descs));
+            };
+            used_descs = true;
+            let Some(desc) = desc_chain.next() else {
+                error!("virtio-vhost-user: descriptor chain is empty");
+                return Err(Error::InvalidMessage);
+            };
+            let mem = desc_chain.memory();
+            if desc.is_write_only() {
+                error!("virito-vhost-user: descriptor is write-only");
+                return Err(Error::InvalidMessage);
+            }
+            let desc_len = desc.len() as usize;
+            if desc_len < size_of::<VhostUserMsgHeader>() {
+                error!("virtio-vhost-user: descriptor too short");
+                return Err(Error::InvalidMessage);
+            }
+            if desc_len > MAX_MSG_SIZE {
+                error!("virtio-vhost-user: descriptor too long");
+                return Err(Error::InvalidMessage);
+            }
+            self.outgoing_buf.resize(desc_len, 0);
+            let addr = desc.addr().translate_gva(access_platform, desc_len);
+            if let Err(e) = mem.read_slice(&mut self.outgoing_buf, addr) {
+                error!("virtio-vhost-user: Problem reading guest data: {e}");
+                return Err(Error::InvalidMessage);
+            }
+
+            let (hdr, buf) = validate_hdr(&mut self.outgoing_buf)?;
+            process_message(hdr, buf)?;
+            if desc_chain.next().is_some() {
+                error!("virtio-vhost-user: guest provided chained descriptors");
+                return Err(Error::InvalidMessage);
+            }
+        }
+        Ok((FdRearm::Neither, used_descs))
+    }
+
+    fn extend_buffer(&mut self, min_size: usize) -> std::io::Result<bool> {
+        let Some(socket) = &self.socket else {
+            error!(
+                "No socket yet - did the backend place buffers on its request or reply queue without getting an FD from the frontend?"
+            );
+            return Err(std::io::Error::from(ErrorKind::InvalidData));
+        };
+        while min_size > self.incoming_data.len() {
+            let extra_space = min_size - self.incoming_data.len();
+            self.incoming_data.reserve(extra_space);
+            let ptr: *mut c_void = self.incoming_data.as_mut_ptr().cast();
+            // SAFETY: current_len points to before the end of the vec's capacity,
+            // as at least one byte was reserved after it.
+            let ptr = unsafe { ptr.add(self.incoming_data.len()) };
+            let mut iov = [iovec {
+                iov_base: ptr,
+                iov_len: extra_space,
+            }];
+
+            let mut fd_array = vec![-1; MAX_ATTACHED_FD_ENTRIES];
+
+            // SAFETY: anything can be written into unallocated capacity of a Vec<u8>
+            let recv_res = unsafe { socket.recv_with_fds(&mut iov[..], &mut fd_array) };
+            let (len, num_fds) = match recv_res {
+                Ok(e) => e,
+                Err(e) => match e.errno() {
+                    libc::EAGAIN => return Ok(false),
+                    libc::EINTR => continue,
+                    e => return Err(std::io::Error::from_raw_os_error(e)),
+                },
+            };
+
+            assert!(len <= extra_space);
+            // SAFETY: the extra space has been reserved,
+            // has been initialized by the kernel, and does
+            // not exceed the spare capacity.
+            unsafe {
+                self.incoming_data.set_len(self.incoming_data.len() + len);
+            }
+
+            for &fd in fd_array.iter().take(num_fds) {
+                assert!(fd >= 0);
+                // SAFETY: we have the ownership of `fd`.
+                let fd = unsafe { File::from_raw_fd(fd) };
+                self.files_for_cycle.push(Some(fd.into()));
+            }
+        }
+        Ok(true)
+    }
+
+    /// Process incoming data on the vhost-user socket.
+    ///
+    /// Returns true if a full message was received, or false if
+    /// all data has been consumed.  In the latter case, if
+    /// edge-triggered file descriptor watching is used, the watch
+    /// must be re-armed.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the callback returns an error or an
+    /// invalid message was received.
+    fn socket_rx(&mut self) -> Result<bool, Error> {
+        let min_size = size_of::<VhostUserMsgHeader>();
+        // TODO: better error
+        if !self.extend_buffer(min_size).map_err(Error::SocketError)? {
+            return Ok(false);
+        }
+        let msg_size = validate_hdr(&mut self.incoming_data)?.0.size;
+        if msg_size > MAX_MSG_SIZE.try_into().unwrap() {
+            error!("Bad message from frontend: size is {msg_size} (limit {MAX_MSG_SIZE})");
+            return Err(Error::InvalidMessage);
+        }
+        self.extend_buffer(msg_size as usize)
+            .map_err(Error::SocketError)
+    }
+
+    /// Process an incoming message from the frontend if possible.
+    ///
+    /// The callback will be invoked for each such message.
+    /// The file descriptor slice provided will only contain `Some`
+    /// entries, but the callback is free to consume them (replace them
+    /// with `None`).  File descriptors not consumed will be lost.
+    ///
+    /// Returns `Ok(true)` if a message was processed and `Ok(false)`
+    /// if there was no message processed.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the callback returns an error or an
+    /// invalid message was received.
+    #[allow(clippy::type_complexity)] // pulling this out leads to borrowck error
+    pub(super) fn process_incoming(
+        &mut self,
+        access_platform: Option<&(dyn AccessPlatform + 'static)>,
+        max_messages: usize,
+        process_message: &mut dyn FnMut(
+            VhostUserMsgHeader,
+            &mut [u8],
+            &mut [Option<OwnedFd>],
+        ) -> Result<(), Error>,
+    ) -> Result<(FdRearm, bool), Error> {
+        let mut used_descs = false;
+        for _ in 0..max_messages {
+            if !self.socket_rx()? {
+                return Ok((FdRearm::Socket, used_descs));
+            }
+            let Some(mut desc_chain) = self
+                .front2back_queue
+                .pop_descriptor_chain(self.mem.memory())
+            else {
+                return Ok((FdRearm::Queue, used_descs));
+            };
+
+            let desc = match desc_chain.next() {
+                Some(desc) => desc,
+                None => return Err(Error::InvalidParam),
+            };
+
+            if !desc.is_write_only() {
+                // TODO: better error
+                return Err(Error::InvalidParam);
+            }
+
+            let desc_len = usize::try_from(desc.len()).unwrap();
+            if desc_len < self.incoming_data.len() {
+                error!("Vhost-user incoming buffer too small!");
+                return Err(Error::InvalidParam);
+            }
+            let (hdr, body) = self
+                .incoming_data
+                .split_at_mut(size_of::<VhostUserMsgHeader>());
+            let &hdr = VhostUserMsgHeader::from_slice(hdr).unwrap();
+
+            used_descs = true;
+            let mem = desc_chain.memory();
+            let addr = desc.addr().translate_gva(access_platform, desc_len);
+            process_message(hdr, body, &mut self.files_for_cycle)?;
+            if let Err(e) = mem.write_slice(&self.incoming_data, addr) {
+                error!("virtio-vhost-user: Problem writing guest data: {e}");
+                return Err(Error::InvalidMessage);
+            }
+        }
+        Ok((FdRearm::Neither, used_descs))
+    }
+}
