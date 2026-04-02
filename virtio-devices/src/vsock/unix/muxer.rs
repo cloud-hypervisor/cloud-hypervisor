f56c8392ea48b43620be238c6650f3ed15184cb0
Peter Oskolkov <posk@google.com>
2026-04-02 20:52:21 +0000

    virtio-devices: vsock: RST vsocks on snapshot restore
    Otherwise guest connections just hang.
    
    Signed-off-by: Peter Oskolkov <posk@google.com>

--- virtio-devices/src/vsock/unix/muxer.rs
+++ virtio-devices/src/vsock/unix/muxer.rs
@@ -345,7 +345,23 @@ impl VsockEpollListener for VsockMuxer {
     }
 }
 
-impl VsockBackend for VsockMuxer {}
+impl VsockBackend for VsockMuxer {
+    fn connections(&self) -> Vec<(u32, u32)> {
+        self.conn_map
+            .keys()
+            .map(|k| (k.local_port, k.peer_port))
+            .collect()
+    }
+
+    fn queue_rst_for_connections(&mut self, conns: Vec<(u32, u32)>) {
+        for (local_port, peer_port) in conns {
+            self.rxq.push(MuxerRx::RstPkt {
+                local_port,
+                peer_port,
+            });
+        }
+    }
+}
 
 impl VsockMuxer {
     /// Muxer constructor.
