1fc6f1f945849432a6abffaf4297ec9c6c01f1b2
Demi Marie Obenour <demiobenour@gmail.com>
2026-04-02 15:41:31 -0400

    virtio-vhost-user: Implement backend->frontend requests
    These are significantly simpler than frontend->backend requests.
    
    Signed-off-by: Demi Marie Obenour <demiobenour@gmail.com>

---
+++ virtio-devices/src/virtio_vhost_user/backend_request.rs
@@ -0,0 +1,69 @@
+// Copyright (c) 2020 Ant Financial
+// Copyright (c) 2026 Demi Marie Obenour
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::os::unix::net::UnixStream;
+
+use vhost::vhost_user::Error;
+use vm_virtio::AccessPlatform;
+
+use super::queue_pair::{FdRearm, VirtioVhostUserQueuePair};
+use crate::EpollHelper;
+
+pub(super) struct BackendRequestQueuePair {
+    queue_pair: VirtioVhostUserQueuePair,
+}
+impl BackendRequestQueuePair {
+    pub(super) fn set_socket(&mut self, socket: UnixStream) -> Result<(), Error> {
+        self.queue_pair.set_socket(socket)
+    }
+    pub(super) fn new(queue_pair: VirtioVhostUserQueuePair) -> Self {
+        Self { queue_pair }
+    }
+
+    pub(super) fn register_epoll_events(
+        &mut self,
+        helper: &mut EpollHelper,
+        base: u16,
+    ) -> Result<(), crate::EpollHelperError> {
+        self.queue_pair.register_epoll_events(helper, base)
+    }
+    pub(super) fn process_incoming(
+        &mut self,
+        access_platform: Option<&(dyn AccessPlatform + 'static)>,
+        max_iterations: usize,
+    ) -> Result<(FdRearm, bool), Error> {
+        self.queue_pair.process_incoming(
+            access_platform,
+            max_iterations,
+            &mut |_hdr, _buf, files| {
+                if files.is_empty() {
+                    Ok(())
+                } else {
+                    Err(Error::IncorrectFds)
+                }
+            },
+        )
+    }
+    pub(super) fn process_outgoing(
+        &mut self,
+        access_platform: Option<&(dyn AccessPlatform + 'static)>,
+        max_iterations: usize,
+    ) -> Result<(FdRearm, bool), Error> {
+        self.queue_pair
+            .process_outgoing(access_platform, max_iterations, &mut |_hdr, _buf| Ok(()))
+    }
+}
