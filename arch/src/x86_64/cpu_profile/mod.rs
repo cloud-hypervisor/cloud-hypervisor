d96de000f2b9acaf50f79856082b7602bd4ae5fe
Oliver Anderson <oliver.anderson@cyberus-technology.de>
2026-04-13 11:04:18 +0200

    arch: CpuProfile enum
    Introduce a CpuProfile enum that will be deserialized from the user's
    selected CPU profile.
    
    Currently we only have a "host" variant, but in the future there will
    be a build script automatically constructing this enum based on
    pre-generated CPU profiles.
    
    Signed-off-by: Oliver Anderson <oliver.anderson@cyberus-technology.de>
    On-behalf-of: SAP oliver.anderson@sap.com

--- arch/src/x86_64/cpu_profile/mod.rs
+++ arch/src/x86_64/cpu_profile/mod.rs
@@ -4,3 +4,13 @@
 //
 
 pub mod cpuid_adjustments;
+
+// TODO: Auto generate the CpuProfile enum with a build script once we introduce user facing CPU profiles.
+
+/// A [`CpuProfile`] is a mechanism for ensuring live migration compatibility
+/// between hosts with potentially different CPU models.
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub enum CpuProfile {
+    #[default]
+    Host,
+}
