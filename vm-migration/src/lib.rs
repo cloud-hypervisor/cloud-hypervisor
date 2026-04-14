a97f11da2d476da6772cc336497704984963f99b
Sebastian Eydam <sebastian.eydam@cyberus-technology.de>
2026-04-14 10:18:56 +0200

    vm-migration: client-side of a TLS connection
    TLS connections have a TLS server (listens for incoming connections) and
    a TLS client (initiates the connection). This commit adds the code for
    the client side, which is the sender of a migration
    
    On-behalf-of: SAP sebastian.eydam@sap.com
    Signed-off-by: Sebastian Eydam <sebastian.eydam@cyberus-technology.de>

--- vm-migration/src/lib.rs
+++ vm-migration/src/lib.rs
@@ -16,6 +16,7 @@ use crate::protocol::MemoryRangeTable;
 mod bitpos_iterator;
 mod context;
 pub mod protocol;
+pub mod tls;
 
 #[derive(Error, Debug)]
 pub enum UffdError {
@@ -92,6 +93,9 @@ pub enum MigratableError {
 
     #[error("Failed to release a disk lock")]
     UnlockError(#[source] anyhow::Error),
+
+    #[error("Error setting up a TLS-encrypted connection")]
+    Tls(#[source] tls::TlsError),
 }
 
 /// A Pausable component can be paused and resumed.
