f5f74aaa294fd738c5b90a296efda2a44119d17e
Tushar Khatri <hello@tusharkhatri.in>
2026-06-12 13:34:53 +0000

    devices: reevaluate #[allow] attributes
    Convert the still-needed #[allow]s to #[expect] so they warn if the
    lints stop firing.
    
    Part of #8326.
    
    Signed-off-by: Tushar Khatri <hello@tusharkhatri.in>

--- devices/src/gic.rs
+++ devices/src/gic.rs
@@ -38,7 +38,7 @@ pub struct Gic {
 }
 
 impl Gic {
-    #[allow(clippy::needless_pass_by_value)]
+    #[expect(clippy::needless_pass_by_value)]
     pub fn new(
         vcpu_count: u32,
         interrupt_manager: Arc<dyn InterruptManager<GroupConfig = MsiIrqGroupConfig>>,
