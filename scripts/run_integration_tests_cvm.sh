5cf282dfb5364a0d91cf3ab7752a1213ee400cb6
Rob Bradford <rbradford@meta.com>
2026-06-17 11:49:33 +0100

    tests: Add missing preparatory steps needed for CVM tests
    The tests expect some generated images and assets so make sure that they
    are there (directly copied from the x86-64 test).
    
    Signed-off-by: Rob Bradford <rbradford@meta.com>

--- scripts/run_integration_tests_cvm.sh
+++ scripts/run_integration_tests_cvm.sh
@@ -34,6 +34,68 @@ if [ ! -f "$JAMMY_OS_RAW_IMAGE" ]; then
     popd || exit
 fi
 
+JAMMY_OS_QCOW_ZLIB_FILE_IMAGE_NAME="jammy-server-cloudimg-amd64-custom-20241017-0-zlib.qcow2"
+JAMMY_OS_QCOW_ZLIB_FILE_IMAGE="$WORKLOADS_DIR/$JAMMY_OS_QCOW_ZLIB_FILE_IMAGE_NAME"
+if [ ! -f "$JAMMY_OS_QCOW_ZLIB_FILE_IMAGE" ]; then
+    pushd "$WORKLOADS_DIR" || exit
+    time qemu-img convert -c -f raw -O qcow2 -o compression_type=zlib \
+        "$JAMMY_OS_RAW_IMAGE" $JAMMY_OS_QCOW_ZLIB_FILE_IMAGE_NAME
+    popd || exit
+fi
+
+JAMMY_OS_QCOW_ZSTD_FILE_IMAGE_NAME="jammy-server-cloudimg-amd64-custom-20241017-0-zstd.qcow2"
+JAMMY_OS_QCOW_ZSTD_FILE_IMAGE="$WORKLOADS_DIR/$JAMMY_OS_QCOW_ZSTD_FILE_IMAGE_NAME"
+if [ ! -f "$JAMMY_OS_QCOW_ZSTD_FILE_IMAGE" ]; then
+    pushd "$WORKLOADS_DIR" || exit
+    time qemu-img convert -c -f raw -O qcow2 -o compression_type=zstd \
+        "$JAMMY_OS_RAW_IMAGE" $JAMMY_OS_QCOW_ZSTD_FILE_IMAGE_NAME
+    popd || exit
+fi
+
+JAMMY_OS_QCOW_BACKING_ZSTD_FILE_IMAGE_NAME="jammy-server-cloudimg-amd64-custom-20241017-0-backing-zstd.qcow2"
+JAMMY_OS_QCOW_BACKING_ZSTD_FILE_IMAGE="$WORKLOADS_DIR/$JAMMY_OS_QCOW_BACKING_ZSTD_FILE_IMAGE_NAME"
+if [ ! -f "$JAMMY_OS_QCOW_BACKING_ZSTD_FILE_IMAGE" ]; then
+    pushd "$WORKLOADS_DIR" || exit
+    time qemu-img create -f qcow2 \
+        -b "$JAMMY_OS_QCOW_ZSTD_FILE_IMAGE" \
+        -F qcow2 $JAMMY_OS_QCOW_BACKING_ZSTD_FILE_IMAGE_NAME
+    popd || exit
+fi
+
+JAMMY_OS_QCOW_BACKING_UNCOMPRESSED_FILE_IMAGE_NAME="jammy-server-cloudimg-amd64-custom-20241017-0-backing-uncompressed.qcow2"
+JAMMY_OS_QCOW_BACKING_UNCOMPRESSED_FILE_IMAGE="$WORKLOADS_DIR/$JAMMY_OS_QCOW_BACKING_UNCOMPRESSED_FILE_IMAGE_NAME"
+if [ ! -f "$JAMMY_OS_QCOW_BACKING_UNCOMPRESSED_FILE_IMAGE" ]; then
+    pushd "$WORKLOADS_DIR" || exit
+    time qemu-img create -f qcow2 \
+        -b "$JAMMY_OS_IMAGE" \
+        -F qcow2 $JAMMY_OS_QCOW_BACKING_UNCOMPRESSED_FILE_IMAGE_NAME
+    popd || exit
+fi
+
+JAMMY_OS_QCOW_BACKING_RAW_FILE_IMAGE_NAME="jammy-server-cloudimg-amd64-custom-20241017-0-backing-raw.qcow2"
+JAMMY_OS_QCOW_BACKING_RAW_FILE_IMAGE="$WORKLOADS_DIR/$JAMMY_OS_QCOW_BACKING_RAW_FILE_IMAGE_NAME"
+if [ ! -f "$JAMMY_OS_QCOW_BACKING_RAW_FILE_IMAGE" ]; then
+    pushd "$WORKLOADS_DIR" || exit
+    time qemu-img create -f qcow2 \
+        -b "$JAMMY_OS_RAW_IMAGE" \
+        -F raw $JAMMY_OS_QCOW_BACKING_RAW_FILE_IMAGE_NAME
+    popd || exit
+fi
+
+BLK_IMAGE="$WORKLOADS_DIR/blk.img"
+MNT_DIR="mount_image"
+if [ ! -f "$BLK_IMAGE" ]; then
+    pushd "$WORKLOADS_DIR" || exit
+    fallocate -l 16M "$BLK_IMAGE"
+    mkfs.ext4 -j "$BLK_IMAGE"
+    mkdir $MNT_DIR
+    sudo mount -t ext4 "$BLK_IMAGE" $MNT_DIR
+    sudo bash -c "echo bar > $MNT_DIR/foo" || exit 1
+    sudo umount "$BLK_IMAGE"
+    rm -r $MNT_DIR
+    popd || exit
+fi
+
 cargo build --features $build_features --all --release --target "$BUILD_TARGET"
 
 export RUST_BACKTRACE=1
