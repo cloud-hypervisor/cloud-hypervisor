ad3790a0ea9d03a7c8f5a361f830eee1159dafb2
Pascal Scholz <pascal.scholz@cyberus-technology.de>
2026-07-20 14:28:57 +0200

    vmm: Update memory zones
    We consume `zone_updates` from `VmReceiveMigrationData` and
    `RestoreConfig` to remap already existing `MemoryZone`s to different
    host NUMA nodes. For now, we do not support further changes, such as
    altering the size of the respective `MemoryZone`s.
    
    These changes allow to migrate a VM to a host that has the capacity to
    host the same `MemoryZone`s on a different NUMA layout.
    
    Signed-off-by: Pascal Scholz <pascal.scholz@cyberus-technology.de>
    On-behalf-of: SAP pascal.scholz@sap.com

--- vmm/src/lib.rs
+++ vmm/src/lib.rs
@@ -1178,6 +1178,7 @@ impl Vmm {
         T: Read,
     {
         let mode = receive_data_migration.memory_mode;
+        let zone_updates = &receive_data_migration.zone_updates;
 
         // Read in config data along with memory manager data
         let mut data: Vec<u8> = Vec::new();
@@ -1221,6 +1222,10 @@ impl Vmm {
             &vm_migration_config.vm_config,
             &vm_migration_config.common_cpuid,
         )?;
+
+        update_memory_zones(zone_updates, &vm_migration_config.vm_config).map_err(|e| {
+            MigratableError::MigrateReceive(anyhow!("Error updating memory zones: {e:?}"))
+        })?;
 
         let config = vm_migration_config.vm_config.clone();
         self.vm_config = Some(vm_migration_config.vm_config);
@@ -2517,6 +2522,11 @@ impl RequestHandler for Vmm {
                         .expect("restore validated iommufd=on, so a platform exists")
                         .iommufd_fd = Some(iommufd_fd);
                 }
+
+                update_memory_zones(&restore_cfg.zone_updates, &vm_config).map_err(|e| {
+                    error!("VM Restore failed: {e:?}");
+                    VmError::ApplyMemoryZoneUpdate(e)
+                })?;
 
                 self.vm_restore(
                     source_url,
