193e99c23a6fa63e8d7917da1c4f6563f264cb5a
Oliver Anderson <oliver.anderson@cyberus-technology.de>
2026-07-29 15:06:03 +0200

    arch: Add compatibility check for CPUID.0x7.0x1:EDX
    Register EDX of CPUID leaf 0x7 sub-leaf 0x1 (Structured Extended Feature
    Enumeration Sub-leaf) is currently not checked, but it should be.
    
    KVM supports several of the features enumerated by this value when they
    are supported by the host, such as AVX-VNNI-INT8, AMX-COMPLEX, AVX10
    and more.
    
    The migration destination needs to support any such feature exposed to
    the guest from the migration source.
    
    Signed-off-by: Oliver Anderson <oliver.anderson@cyberus-technology.de>
    On-behalf-of: SAP oliver.anderson@sap.com

--- arch/src/x86_64/mod.rs
+++ arch/src/x86_64/mod.rs
@@ -457,6 +457,12 @@ impl CpuidFeatureEntry {
                 feature_reg: CpuidReg::ECX,
                 compatible_check: CpuidCompatibleCheck::BitwiseSubset,
             },
+            CpuidFeatureEntry {
+                function: 7,
+                index: 1,
+                feature_reg: CpuidReg::EDX,
+                compatible_check: CpuidCompatibleCheck::BitwiseSubset,
+            },
             // Leaf 0x8000_0001, ECX/EDX, CPUID features bits
             CpuidFeatureEntry {
                 function: 0x8000_0001,
