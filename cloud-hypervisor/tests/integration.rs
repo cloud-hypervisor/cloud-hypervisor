517fa1ad99adf00a6892da37c417590cd335e346
Sumedh Alok Sharma <sumsharma@microsoft.com>
2026-08-13 07:38:01 +0000

    block: vmdk: Open disk only when backing_files is set via DiskConfig
    Needs backing_files=true set via DiskConfig to open vmdk disk. This
    change adds VMDK images in exception list to mark it as trusted images
    If unset, the image format is treated as Unsupported.
    
    Fixes: #8713
    
    Signed-off-by: Sumedh Alok Sharma <sumsharma@microsoft.com>

--- cloud-hypervisor/tests/integration.rs
+++ cloud-hypervisor/tests/integration.rs
@@ -1624,6 +1624,12 @@ mod common_parallel {
         let res = run_qemu_img(&test_disk, &create_args, Some(&["64M"]));
         assert!(res.status.success(), "qemu-img create failed: {res:?}");
 
+        let vmdk_backing = if matches!(image_type, ImageType::FlatVmdk) {
+            ",backing_files=on"
+        } else {
+            ""
+        };
+
         let mut child = GuestCommand::new(&guest)
             .args(["--cpus", "boot=4"])
             .args(["--memory", "size=512M"])
@@ -1633,7 +1639,7 @@ mod common_parallel {
             .args([
                 "--disk",
                 format!(
-                    "path={},direct=on,image_type={image_type_str}",
+                    "path={},direct=on,image_type={image_type_str}{vmdk_backing}",
                     test_disk.to_str().unwrap()
                 )
                 .as_str(),
