241cbba50415212a3b330ab0b20acc67b4329e76
Oliver Anderson <oliver.anderson@cyberus-technology.de>
2026-08-13 16:31:10 +0200

    arch: Add compatibility check for CPUID 0x1d.0x0:EAX
    CPUID 0x1d.0x0:EAX corresponds to (AMX) MAX_PALETTE which is the highest
    numbered palette sub-leaf.
    
    While the value is 1 on all current Intel processors that have any kind
    of AMX support, this could potentially change in the future.
    
    AMD does currently not use this CPUID leaf for anything, so we assume
    that it is always either not reported at all or reported as 0 by the
    hypervisor when running on AMD processors.
    
    Signed-off-by: Oliver Anderson <oliver.anderson@cyberus-technology.de>
    On-behalf-of: SAP oliver.anderson@sap.com

--- arch/src/x86_64/mod.rs
+++ arch/src/x86_64/mod.rs
@@ -469,6 +469,12 @@ impl CpuidFeatureEntry {
                 feature_reg: CpuidReg::EDX,
                 compatible_check: CpuidCompatibleCheck::BitwiseSubset,
             },
+            CpuidFeatureEntry {
+                function: 0x1d,
+                index: 0,
+                feature_reg: CpuidReg::EAX,
+                compatible_check: CpuidCompatibleCheck::NumNotGreater,
+            },
             // Leaf 0x8000_0001, ECX/EDX, CPUID features bits
             CpuidFeatureEntry {
                 function: 0x8000_0001,
