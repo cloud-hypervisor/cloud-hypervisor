192123e011a453211c874cedd95291cfc6805280
Rob Bradford <rbradford@meta.com>
2026-08-20 13:44:05 +0100

    scripts: Streamline vdpa setup
    Use the prebuilt Ubuntu package for the required modules rather than
    building from source. Remove the unnecessary separate check as
    attempting to load the modules will identify if they are not installed.
    
    Signed-off-by: Rob Bradford <rbradford@meta.com>

--- scripts/prepare_vdpa.sh
+++ scripts/prepare_vdpa.sh
@@ -1,36 +1,12 @@
 #!/usr/bin/env bash
 set -x
 
-build_install_vdpa_sim_modules_ubuntu() {
-    sudo apt install -y libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf git make dpkg-dev libmnl-dev pkg-config iproute2
-    sudo sed -i -- 's/# deb-src/deb-src/g' /etc/apt/sources.list
+install_vdpa_sim_modules_ubuntu() {
     sudo apt update
-    apt-get source linux-image-unsigned-"$(uname -r)"
-    pushd linux-azure*/drivers/vdpa/vdpa_sim/ || exit
-    # REUSE-IgnoreStart
-    cat <<'EOF' >Makefile
-# SPDX-License-Identifier: GPL-2.0
-obj-m += vdpa_sim.o
-obj-m += vdpa_sim_net.o
-obj-m += vdpa_sim_blk.o
-EOF
-    # REUSE-IgnoreEnd
-    make -C /lib/modules/"$(uname -r)"/build M="$PWD"
-    sudo make -C /lib/modules/"$(uname -r)"/build M="$PWD" modules_install
-    popd || exit
-    sudo depmod -a
+    sudo apt install -y linux-modules-extra-"$(uname -r)"
 }
 
-check_vdpa_sim_modules() {
-    for module in $MODULES; do
-        modinfo "$module" || {
-            echo "Module $module is not installed. Please build and install it first."
-            exit 1
-        }
-    done
-}
-
-modproobe_modules() {
+modprobe_modules() {
     for module in $MODULES; do
         sudo modprobe "$module" || {
             echo "Failed to load module $module. Please check if it is installed correctly."
@@ -62,12 +38,8 @@ if [[ -f /etc/lsb-release ]]; then
 fi
 
 if [[ "$DISTRO_NAME" == "ubuntu" ]]; then
-    build_install_vdpa_sim_modules_ubuntu
+    install_vdpa_sim_modules_ubuntu
 fi
-# For other distros, we assume the modules are already built and installed
-# For Azure Linux, the modules are included in the kernel and should be available by default
-check_vdpa_sim_modules
 
-modproobe_modules
-
+modprobe_modules
 prepare_vdpa
