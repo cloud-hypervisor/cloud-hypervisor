c5d11a6736347ba4cf459c5a655d7137f611976d
Rob Bradford <rbradford@meta.com>
2026-08-26 11:08:26 +0100

    vmm: Add missing vmdk from DiskConfig::SYNTAX
    This image type was missing from the help text.
    
    Signed-off-by: Rob Bradford <rbradford@meta.com>

--- vmm/src/config.rs
+++ vmm/src/config.rs
@@ -1451,7 +1451,7 @@ impl DiskConfig {
          rate_limit_group=<group_id>,\
          queue_affinity=<list_of_queue_indices_with_their_associated_cpuset>,\
          serial=<serial_number>,backing_files=on|off,sparse=on|off,\
-         image_type=<raw,qcow2,vhd,vhdx>,lock_granularity=byte-range|full";
+         image_type=<raw,qcow2,vhd,vhdx,vmdk>,lock_granularity=byte-range|full";
 
     pub fn parse(disk: &str) -> Result<Self> {
         let mut parser = OptionParser::new();
