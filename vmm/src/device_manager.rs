9cd9819a425c1f562356481614b8083e8e5f68dd
Rob Bradford <rbradford@meta.com>
2026-08-26 09:20:23 +0100

    block: Remove OpenedDisk struct
    This serves no purpose now that the image type must be specified when
    the disk was opened.
    
    Signed-off-by: Rob Bradford <rbradford@meta.com>

--- vmm/src/device_manager.rs
+++ vmm/src/device_manager.rs
@@ -2729,7 +2729,7 @@ impl DeviceManager {
                 .as_ref()
                 .ok_or(DeviceManagerError::NoDiskPath)?;
 
-            let opened = open_disk(
+            let disk = open_disk(
                 &DiskOpenOptions {
                     path: disk_path,
                     readonly: disk_cfg.readonly,
@@ -2790,7 +2790,7 @@ impl DeviceManager {
 
             let mut virtio_block = virtio_devices::Block::new(
                 id.clone(),
-                opened.disk,
+                disk,
                 disk_cfg
                     .path
                     .as_ref()
