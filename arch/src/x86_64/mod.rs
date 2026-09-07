fb85716e9ad8d100736acc8cdd8fa3099449decd
Oliver Anderson <oliver.anderson@cyberus-technology.de>
2026-09-07 11:12:57 +0200

    arch: Add compatibility check for CPUID 0xd.0x0:EDX
    CPUID 0xd.0x0:EDX corresponds to the valid bit fields of the upper 32
    bits of the XFEATURE_ENABLED_MASK register.
    
    This is mainly for future compatibility concerns for when/if Intel or
    AMD introduce features requiring the upper bits of XCR0.
    
    Strictly speaking these upper bits have been used by AMD in the past,
    but not since the introduction of the Zen microarchitecture.
    
    Signed-off-by: Oliver Anderson <oliver.anderson@cyberus-technology.de>
    On-behalf-of: SAP oliver.anderson@sap.com

--- arch/src/x86_64/mod.rs
+++ arch/src/x86_64/mod.rs
@@ -475,6 +475,12 @@ impl CpuidFeatureEntry {
                 compatible_check: CpuidCompatibleCheck::BitwiseSubset,
             },
             CpuidFeatureEntry {
+                function: 0xd,
+                index: 0,
+                feature_reg: CpuidReg::EDX,
+                compatible_check: CpuidCompatibleCheck::BitwiseSubset,
+            },
+            CpuidFeatureEntry {
                 function: 0x1d,
                 index: 0,
                 feature_reg: CpuidReg::EAX,
