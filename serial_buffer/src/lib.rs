c9c7c56844b73f63f393455d941e8de0e3e4d2a0
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-09-15 17:35:32 +0200

    serial_buffer: apply clippy lint clone_on_ref_ptr
    Improves readability as it directly shows that a copy is cheap and
    creates another smart pointer instance to the same underlying data.
    
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- serial_buffer/src/lib.rs
+++ serial_buffer/src/lib.rs
@@ -155,7 +155,7 @@ mod tests {
     #[test]
     fn accumulates_while_detached_then_replays_on_connect() {
         let write_out = Arc::new(AtomicBool::new(false));
-        let mut buf = SerialBuffer::new(Box::new(io::sink()), write_out.clone());
+        let mut buf = SerialBuffer::new(Box::new(io::sink()), Arc::clone(&write_out));
 
         buf.write_all(b"boot: hello\n").unwrap();
         buf.write_all(b"login: ").unwrap();
@@ -172,7 +172,7 @@ mod tests {
     #[test]
     fn live_writes_pass_through_after_connect() {
         let write_out = Arc::new(AtomicBool::new(false));
-        let mut buf = SerialBuffer::new(Box::new(io::sink()), write_out.clone());
+        let mut buf = SerialBuffer::new(Box::new(io::sink()), Arc::clone(&write_out));
 
         let sink = TestSink::new();
         buf.set_out(Box::new(sink.clone()));
@@ -188,7 +188,7 @@ mod tests {
     #[test]
     fn output_while_detached_goes_to_next_client() {
         let write_out = Arc::new(AtomicBool::new(false));
-        let mut buf = SerialBuffer::new(Box::new(io::sink()), write_out.clone());
+        let mut buf = SerialBuffer::new(Box::new(io::sink()), Arc::clone(&write_out));
 
         // First client: connects, drains "early\n", then disconnects.
         let first = TestSink::new();
@@ -215,7 +215,7 @@ mod tests {
     #[test]
     fn drained_bytes_are_not_resent_to_a_second_client() {
         let write_out = Arc::new(AtomicBool::new(false));
-        let mut buf = SerialBuffer::new(Box::new(io::sink()), write_out.clone());
+        let mut buf = SerialBuffer::new(Box::new(io::sink()), Arc::clone(&write_out));
 
         buf.write_all(b"boot log\n").unwrap();
 
