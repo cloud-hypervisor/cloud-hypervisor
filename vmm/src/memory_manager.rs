651f32478114d2e218429b8b9cb287e1482bbec1
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-09-17 21:53:00 +0200

    vmm: memory_manager: log dirty ranges as a summary
    This is a hot code for memory iterations and saving every ms matters
    there for low precopy iteration overhead and low downtime. Also, the
    functionality is working and the logging provides zero value-add.
    
    - one single log line: less branch instructions in hot code
    - degrade to trace: provides little value (code is mature)
    
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- vmm/src/memory_manager.rs
+++ vmm/src/memory_manager.rs
@@ -26,7 +26,7 @@ use arch::{RegionType, layout};
 use devices::ioapic;
 #[cfg(any(target_arch = "aarch64", target_arch = "riscv64"))]
 use hypervisor::HypervisorVmError;
-use log::{debug, error, info, warn};
+use log::{debug, error, info, trace, warn};
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 use tracer::trace_scoped;
@@ -3635,14 +3635,12 @@ impl Migratable for MemoryManager {
 
             let sub_table = MemoryRangeTable::from_dirty_bitmap(dirty_bitmap, r.gpa, 4096);
 
-            if sub_table.regions().is_empty() {
-                debug!("Dirty Memory Range Table is empty");
-            } else {
-                debug!("Dirty Memory Range Table:");
-                for range in sub_table.regions() {
-                    debug!("GPA: {:x} size: {} (KiB)", range.gpa, range.length / 1024);
-                }
-            }
+            trace!(
+                "Dirty memory range table for slot {}: ranges = {} size = {} KiB",
+                r.slot,
+                sub_table.regions().len(),
+                sub_table.effective_size() / 1024,
+            );
 
             table.extend(sub_table);
         }
