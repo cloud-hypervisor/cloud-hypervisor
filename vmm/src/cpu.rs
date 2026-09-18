48707a43273e5d78efd70de3a23522faba656e55
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-09-18 08:15:00 +0200

    vmm: cpu: write the coredump notes completely
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- vmm/src/cpu.rs
+++ vmm/src/cpu.rs
@@ -3160,7 +3160,7 @@ impl CpuElf64Writable for CpuManager {
             buf.resize(note_size as usize, 0);
 
             coredump_file
-                .write(&buf)
+                .write_all(&buf)
                 .map_err(GuestDebuggableError::CoredumpFile)?;
         }
 
@@ -3281,7 +3281,7 @@ impl CpuElf64Writable for CpuManager {
             buf.resize(note_size as usize, 0);
 
             coredump_file
-                .write(&buf)
+                .write_all(&buf)
                 .map_err(GuestDebuggableError::CoredumpFile)?;
         }
 
