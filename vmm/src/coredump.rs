83f06e40b70245be53aa4f8d30f4211cbf9bfda3
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-09-18 08:14:46 +0200

    vmm: coredump: write the core file headers completely
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- vmm/src/coredump.rs
+++ vmm/src/coredump.rs
@@ -234,7 +234,7 @@ pub(crate) trait Elf64Writable {
         let mut coredump_file = dump_state.file.as_ref().unwrap();
         let bytes: &[u8] = elf64_ehdr.as_slice();
         coredump_file
-            .write(bytes)
+            .write_all(bytes)
             .map_err(GuestDebuggableError::CoredumpFile)?;
 
         Ok(())
@@ -256,7 +256,7 @@ pub(crate) trait Elf64Writable {
         let mut coredump_file = dump_state.file.as_ref().unwrap();
         let bytes: &[u8] = elf64_phdr.as_slice();
         coredump_file
-            .write(bytes)
+            .write_all(bytes)
             .map_err(GuestDebuggableError::CoredumpFile)?;
 
         Ok(())
@@ -284,7 +284,7 @@ pub(crate) trait Elf64Writable {
         let mut coredump_file = dump_state.file.as_ref().unwrap();
         let bytes: &[u8] = elf64_load.as_slice();
         coredump_file
-            .write(bytes)
+            .write_all(bytes)
             .map_err(GuestDebuggableError::CoredumpFile)?;
 
         Ok(())
